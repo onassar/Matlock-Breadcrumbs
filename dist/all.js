c23485bec83761b97cdb77d0b4a3bf09544ab18f
onassar <onassar@gmail.com>
2018-12-14 15:20:03 -0500

    Testing

--- dist/all.js
+++ dist/all.js
@@ -26,5 +26,21 @@
                 'window.jQuery.fn.jquery'
             ]
         }
+    },{
+    properties: {
+        link: 'https://github.com/Polymer/polymer',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            ['window.Polymer']
+        ]
+    },
+    versions: {
+        references: [
+            'window.Polymer.version'
+        ]
     }
+}
 ]

d92fa0a96833f04c9dabb08081c99e4b17052959
onassar <onassar@gmail.com>
2018-12-14 01:09:02 -0500

    Build script

---
+++ scripts/build.sh
@@ -0,0 +1,36 @@
+## 
+## build.sh
+## 
+## Usage:
+## 
+##     ./scripts/build.sh
+
+
+## 
+## Overhead
+## 
+## 
+rm -rf build/
+mkdir build/
+
+
+## 
+## 
+## 
+## 
+...
+
+
+## 
+## Overhead
+## 
+## 
+rm -rf build/
+
+
+## 
+## Done
+## 
+## 
+echo "Complete"
+exit 0

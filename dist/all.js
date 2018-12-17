fec77d9d2c9e03be8f4d715a9e0f40549c2ea193
onassar <onassar@gmail.com>
2018-12-17 04:32:26 -0500

    New

--- dist/all.js
+++ dist/all.js
@@ -1610,6 +1610,9 @@
 {
     properties: {
         link: 'https://github.com/imakewebthings/jquery-waypoints',
+        notes: [
+            "Marked the reference as dead.dead.dead since this repo does not exist anymore but is still referenced. Find a better way to mark this as dead"
+        ],
         platform: 'GitHub',
         type: 'repository'
     },

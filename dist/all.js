f2aefff2435da9a51e67e3b4f32bbded624a0978
onassar <onassar@gmail.com>
2018-12-17 03:20:29 -0500

    new ones

--- dist/all.js
+++ dist/all.js
@@ -2100,6 +2100,31 @@
     }
 }
 ,
+// src/marionettejs/backbone.marionette.js
+{
+    properties: {
+        link: 'https://github.com/marionettejs/backbone.marionette',
+        notes: [],
+        platform: 'GitHub',
+        tests: [
+            'https://secure.helpscout.net/conversation/630081877/9026/?folderId=1453481'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            ['window.Marionette'],
+            ['window.Backbone.Marionette']
+        ]
+    },
+    versions: {
+        references: [
+            'window.Marionette.VERSION',
+            'window.Backbone.Marionette.VERSION'
+        ]
+    }
+}
+,
 // src/markedjs/marked.js
 {
     properties: {
---
+++ src/marionettejs/backbone.marionette.js
@@ -0,0 +1,23 @@
+{
+    properties: {
+        link: 'https://github.com/marionettejs/backbone.marionette',
+        notes: [],
+        platform: 'GitHub',
+        tests: [
+            'https://secure.helpscout.net/conversation/630081877/9026/?folderId=1453481'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            ['window.Marionette'],
+            ['window.Backbone.Marionette']
+        ]
+    },
+    versions: {
+        references: [
+            'window.Marionette.VERSION',
+            'window.Backbone.Marionette.VERSION'
+        ]
+    }
+}

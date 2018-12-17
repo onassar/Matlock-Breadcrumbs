46a731b56ec8e7dc02cf5d5c9e058e4e603e52e6
onassar <onassar@gmail.com>
2018-12-17 04:43:44 -0500

    New

--- dist/all.js
+++ dist/all.js
@@ -1977,6 +1977,29 @@
     }
 }
 ,
+// src/julianshapiro/velocity.js
+{
+    properties: {
+        link: 'https://github.com/julianshapiro/velocity',
+        notes: [],
+        platform: 'GitHub',
+        tests: [
+            'https://appsumo.com/stencil-2018/'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            ['window.jQuery.Velocity']
+        ]
+    },
+    versions: {
+        statements: [
+            'window.jQuery.Velocity.version.major + \'.\' + window.jQuery.Velocity.version.minor + \'.\' + window.jQuery.Velocity.version.patch'
+        ]
+    }
+}
+,
 // src/jwplayer/jwplayer.js
 {
     properties: {
---
+++ src/julianshapiro/velocity.js
@@ -0,0 +1,21 @@
+{
+    properties: {
+        link: 'https://github.com/julianshapiro/velocity',
+        notes: [],
+        platform: 'GitHub',
+        tests: [
+            'https://appsumo.com/stencil-2018/'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            ['window.jQuery.Velocity']
+        ]
+    },
+    versions: {
+        statements: [
+            'window.jQuery.Velocity.version.major + \'.\' + window.jQuery.Velocity.version.minor + \'.\' + window.jQuery.Velocity.version.patch'
+        ]
+    }
+}

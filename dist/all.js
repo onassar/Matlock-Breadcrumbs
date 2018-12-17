ad7cc5765f1293364a52f18e44253ce9b388770d
onassar <onassar@gmail.com>
2018-12-17 04:39:03 -0500

    New

--- dist/all.js
+++ dist/all.js
@@ -3303,6 +3303,24 @@
     }
 }
 ,
+// src/weareoutman/clockpicker.js
+{
+    properties: {
+        link: 'https://github.com/weareoutman/clockpicker',
+        notes: [],
+        platform: 'GitHub',
+        tests: [
+            'http://weareoutman.github.io/clockpicker/'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            ['window.jQuery.fn.clockpicker']
+        ]
+    }
+}
+,
 // src/webpack/webpack.js
 {
     properties: {
---
+++ src/weareoutman/clockpicker.js
@@ -0,0 +1,16 @@
+{
+    properties: {
+        link: 'https://github.com/weareoutman/clockpicker',
+        notes: [],
+        platform: 'GitHub',
+        tests: [
+            'http://weareoutman.github.io/clockpicker/'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            ['window.jQuery.fn.clockpicker']
+        ]
+    }
+}

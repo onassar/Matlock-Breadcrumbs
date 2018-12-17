b9960318318d7d020d67bc9a4c7c65b8b955d0a5
onassar <onassar@gmail.com>
2018-12-17 03:08:52 -0500

    New one

--- dist/all.js
+++ dist/all.js
@@ -1851,6 +1851,29 @@
     }
 }
 ,
+// src/jsor/jcarousel.js
+{
+    properties: {
+        link: 'https://github.com/jsor/jcarousel',
+        notes: [],
+        platform: 'GitHub',
+        tests: [
+            'https://sorgalla.com/jcarousel/'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            ['window.jQuery.jCarousel']
+        ]
+    },
+    versions: {
+        references: [
+            'window.jQuery.jCarousel.version'
+        ]
+    }
+}
+,
 // src/jwplayer/jwplayer.js
 {
     properties: {
---
+++ src/jsor/jcarousel.js
@@ -0,0 +1,21 @@
+{
+    properties: {
+        link: 'https://github.com/jsor/jcarousel',
+        notes: [],
+        platform: 'GitHub',
+        tests: [
+            'https://sorgalla.com/jcarousel/'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            ['window.jQuery.jCarousel']
+        ]
+    },
+    versions: {
+        references: [
+            'window.jQuery.jCarousel.version'
+        ]
+    }
+}

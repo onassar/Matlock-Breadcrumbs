04f453874d5216c0f18376e6fe8b5a6c5db95242
onassar <onassar@gmail.com>
2018-12-17 13:44:19 -0500

    New

--- dist/all.js
+++ dist/all.js
@@ -181,6 +181,32 @@
     }
 }
 ,
+// src/MikeMcl/big.js.js
+{
+    properties: {
+        link: 'https://github.com/MikeMcl/big.js',
+        notes: [],
+        platform: 'GitHub',
+        tests: [
+            'http://mikemcl.github.io/big.js/'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.Big',
+                'window.Big.version'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.Big.version'
+        ]
+    }
+}
+,
 // src/Modernizr/Modernizr.js
 {
     properties: {
---
+++ src/MikeMcl/big.js.js
@@ -0,0 +1,24 @@
+{
+    properties: {
+        link: 'https://github.com/MikeMcl/big.js',
+        notes: [],
+        platform: 'GitHub',
+        tests: [
+            'http://mikemcl.github.io/big.js/'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.Big',
+                'window.Big.version'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.Big.version'
+        ]
+    }
+}

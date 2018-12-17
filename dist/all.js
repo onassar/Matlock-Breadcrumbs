bd7918ed58c15bd3ec253017fa3a0232e1795a28
onassar <onassar@gmail.com>
2018-12-17 04:28:51 -0500

    New

--- dist/all.js
+++ dist/all.js
@@ -1297,6 +1297,23 @@
     }
 }
 ,
+// src/fengyuanchen/datepicker.js
+{
+    properties: {
+        link: 'https://github.com/fengyuanchen/datepicker',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.fn.datepicker',
+                'window.jQuery.fn.datepicker.noConflict'
+            ]
+        ]
+    }
+}
+,
 // src/ftlabs/fastclick.js
 {
     properties: {
---
+++ src/fengyuanchen/datepicker.js
@@ -0,0 +1,15 @@
+{
+    properties: {
+        link: 'https://github.com/fengyuanchen/datepicker',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.fn.datepicker',
+                'window.jQuery.fn.datepicker.noConflict'
+            ]
+        ]
+    }
+}

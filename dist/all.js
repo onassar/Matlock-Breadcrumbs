3eea414b25a704e5f09e19d07ff9b51bdc38bedf
onassar <onassar@gmail.com>
2018-12-17 21:57:03 -0500

    New

--- dist/all.js
+++ dist/all.js
@@ -941,6 +941,26 @@
     }
 }
 ,
+// src/ccampbell/gator.js
+{
+    properties: {
+        donate: {
+            link: 'https://craig.is/accepting-donations?ref=gator'
+        },
+        link: 'https://github.com/ccampbell/gator',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.Gator',
+                'window.Gator.prototype.off'
+            ]
+        ]
+    },
+}
+,
 // src/ccampbell/mousetrap.js
 {
     properties: {
---
+++ src/ccampbell/gator.js
@@ -0,0 +1,18 @@
+{
+    properties: {
+        donate: {
+            link: 'https://craig.is/accepting-donations?ref=gator'
+        },
+        link: 'https://github.com/ccampbell/gator',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.Gator',
+                'window.Gator.prototype.off'
+            ]
+        ]
+    },
+}

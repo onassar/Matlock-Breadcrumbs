8010db4c14efbdf8535e8dbed87d9c7b3b714869
onassar <onassar@gmail.com>
2018-12-18 15:07:51 -0500

    New one

--- dist/all.js
+++ dist/all.js
@@ -862,6 +862,20 @@
     }
 }
 ,
+// src/bfintal/Counter-Up.js
+{
+    properties: {
+        link: 'https://github.com/dordille/bfintal/Counter-Up',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            ['window.jQuery.fn.counterUp']
+        ]
+    }
+}
+,
 // src/blueimp/jQuery-File-Upload.js
 {
     properties: {
---
+++ src/bfintal/Counter-Up.js
@@ -0,0 +1,12 @@
+{
+    properties: {
+        link: 'https://github.com/dordille/bfintal/Counter-Up',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            ['window.jQuery.fn.counterUp']
+        ]
+    }
+}

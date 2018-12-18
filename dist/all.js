d450c065981e5850290a50c4811b6c49b1de4c73
onassar <onassar@gmail.com>
2018-12-17 21:22:04 -0500

    New

--- dist/all.js
+++ dist/all.js
@@ -1525,6 +1525,33 @@
     }
 }
 ,
+// src/fullcalendar/fullcalendar.js
+{
+    properties: {
+        link: 'https://github.com/fullcalendar/fullcalendar',
+        notes: [],
+        platform: 'GitHub',
+        tags: [
+            'calendars'
+        ],
+        tests: [
+            'https://www.primefaces.org/primeng/#/',
+            'https://fullcalendar.io/'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            ['window.FullCalendar']
+        ]
+    },
+    versions: {
+        references: [
+            'window.FullCalendar.version'
+        ]
+    }
+}
+,
 // src/getsentry/sentry-javascript.js
 {
     properties: {
---
+++ src/fullcalendar/fullcalendar.js
@@ -0,0 +1,25 @@
+{
+    properties: {
+        link: 'https://github.com/fullcalendar/fullcalendar',
+        notes: [],
+        platform: 'GitHub',
+        tags: [
+            'calendars'
+        ],
+        tests: [
+            'https://www.primefaces.org/primeng/#/',
+            'https://fullcalendar.io/'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            ['window.FullCalendar']
+        ]
+    },
+    versions: {
+        references: [
+            'window.FullCalendar.version'
+        ]
+    }
+}

4117372b4a86b387f4b9f53df6749b90e15fe7fd
onassar <onassar@gmail.com>
2018-12-17 22:07:07 -0500

    New

--- dist/all.js
+++ dist/all.js
@@ -4041,10 +4041,23 @@
 {
     properties: {
         link: 'https://github.com/zenorocha/clipboard.js',
+        notes: [
+            'https://i.imgur.com/RVXNiwf.jpg'
+        ],
         platform: 'GitHub',
         type: 'repository'
     },
     tests: {
+        patterns: {
+            page: [
+                [
+                    /libs\/clipboard\.js/([^\/]+)/clipboard\.js/i
+                ],
+                [
+                    /libs\/clipboard\.js/([^\/]+)/clipboard\.min\.js/i
+                ]
+            ]
+        },
         references: [
             ['window.ClipboardJS']
         ]

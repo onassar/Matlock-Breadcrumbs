babe943dde4d4b68d29c63404f6c25656984e21f
onassar <onassar@gmail.com>
2018-12-17 21:35:03 -0500

    New

--- dist/all.js
+++ dist/all.js
@@ -3379,6 +3379,33 @@
     }
 }
 ,
+// src/systemjs/systemjs.js
+{
+    properties: {
+        link: 'https://github.com/systemjs/systemjs',
+        notes: [],
+        platform: 'GitHub',
+        tests: [
+            'https://ng-semantic.herokuapp.com/#/'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.System',
+                'window.System.import'
+                'window.System.register'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.System.version'
+        ]
+    }
+}
+,
 // src/tinymce/tinymce.js
 {
     properties: {
---
+++ src/systemjs/systemjs.js
@@ -0,0 +1,25 @@
+{
+    properties: {
+        link: 'https://github.com/systemjs/systemjs',
+        notes: [],
+        platform: 'GitHub',
+        tests: [
+            'https://ng-semantic.herokuapp.com/#/'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.System',
+                'window.System.import'
+                'window.System.register'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.System.version'
+        ]
+    }
+}

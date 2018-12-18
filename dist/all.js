0510fbdbd1061be590f731c8a339d4a86de28688
onassar <onassar@gmail.com>
2018-12-17 21:54:50 -0500

    New

--- dist/all.js
+++ dist/all.js
@@ -962,6 +962,24 @@
     },
 }
 ,
+// src/ccampbell/rainbow.js
+{
+    properties: {
+        link: 'https://github.com/ccampbell/rainbow',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.Rainbow',
+                'window.Rainbow.addClass',
+                'window.Rainbow.extend'
+            ]
+        ]
+    },
+}
+,
 // src/chartjs/Chart.js.js
 {
     properties: {
---
+++ src/ccampbell/rainbow.js
@@ -0,0 +1,16 @@
+{
+    properties: {
+        link: 'https://github.com/ccampbell/rainbow',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.Rainbow',
+                'window.Rainbow.addClass',
+                'window.Rainbow.extend'
+            ]
+        ]
+    },
+}

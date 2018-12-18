6a347ad3498a8176a0e571f0ebdb5640fad94f07
onassar <onassar@gmail.com>
2018-12-18 16:23:59 -0500

    Yep

--- dist/all.js
+++ dist/all.js
@@ -1702,6 +1702,26 @@
     }
 }
 ,
+// src/filamentgroup/grunticon.js
+{
+    properties: {
+        link: 'https://github.com/filamentgroup/grunticon',
+        notes: [],
+        platform: 'GitHub',
+        tests: [
+            'https://www.bbc.com/news/entertainment-arts-46524504'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.grunticon'
+            ]
+        ]
+    }
+}
+,
 // src/filepicker/filepicker-js.js
 {
     properties: {
---
+++ src/filamentgroup/grunticon.js
@@ -0,0 +1,18 @@
+{
+    properties: {
+        link: 'https://github.com/filamentgroup/grunticon',
+        notes: [],
+        platform: 'GitHub',
+        tests: [
+            'https://www.bbc.com/news/entertainment-arts-46524504'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.grunticon'
+            ]
+        ]
+    }
+}

33638d3e9364ca6b6661c89fac3a4d99e59ed2f9
onassar <onassar@gmail.com>
2018-12-20 18:46:27 -0500

    New one

--- src/gitHub/marcj/jquery-selectBox.js
+++ src/gitHub/marcj/jquery-selectBox.js
@@ -2,13 +2,26 @@
     properties: {
         link: 'https://github.com/marcj/jquery-selectBox',
         platform: 'GitHub',
+        tests: [
+            'https://greensock.com/',
+            'http://marcj.github.io/jquery-selectBox/'
+        ],
         type: 'repository'
     },
     tests: {
         references: [
             [
+                'window.SelectBox',
+                'window.SelectBox.prototype.getOptions'
+            ],
+            [
                 'window.jQuery.fn.selectBox'
             ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.SelectBox.prototype.version'
         ]
     }
 }

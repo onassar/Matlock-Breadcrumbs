a4cf21cbd394c886f8a447b2786cb608708d564b
onassar <onassar@gmail.com>
2018-12-20 16:52:22 -0500

    New one

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -2617,6 +2617,32 @@
     }
 }
 ,
+// src/gitHub/jquerytools/jquerytools.js
+{
+    properties: {
+        link: 'https://github.com/jquerytools/jquerytools',
+        notes: [],
+        platform: 'GitHub',
+        tests: [
+            'http://jquerytools.org/'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.tools',
+                'window.jQuery.tools.tooltip'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.jQuery.tools.version'
+        ]
+    }
+}
+,
 // src/gitHub/jrburke/requirejs.js
 {
     properties: {
---
+++ src/gitHub/jquerytools/jquerytools.js
@@ -0,0 +1,24 @@
+{
+    properties: {
+        link: 'https://github.com/jquerytools/jquerytools',
+        notes: [],
+        platform: 'GitHub',
+        tests: [
+            'http://jquerytools.org/'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.tools',
+                'window.jQuery.tools.tooltip'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.jQuery.tools.version'
+        ]
+    }
+}

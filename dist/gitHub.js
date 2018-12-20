ad3cbc9d7ac885e81a9dc182f106f4eaf69d2ef2
onassar <onassar@gmail.com>
2018-12-20 03:33:20 -0500

    cleanup

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -1789,6 +1789,27 @@
     }
 }
 ,
+// src/gitHub/flatlogic/bootstrap-tabcollapse.js
+{
+    properties: {
+        link: 'https://github.com/flatlogic/bootstrap-tabcollapse',
+        notes: [],
+        platform: 'GitHub',
+        tests: [
+            'https://www.toronto.ca/'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.fn.Constructor',
+                'window.jQuery.fn.tabCollapse'
+            ]
+        ]
+    }
+}
+,
 // src/gitHub/ftlabs/fastclick.js
 {
     properties: {

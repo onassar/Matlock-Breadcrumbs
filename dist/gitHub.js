43fe0741dc837374f8d12474bda9f2b1d368d97e
onassar <onassar@gmail.com>
2018-12-20 03:26:23 -0500

    Out and done

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -2705,6 +2705,29 @@
     }
 }
 ,
+// src/gitHub/liabru/jquery-match-height.js
+{
+    properties: {
+        link: 'https://github.com/liabru/jquery-match-height',
+        notes: [],
+        platform: 'GitHub',
+        tests: [
+            'https://www.london.gov.uk/'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            ['window.jQuery.fn.matchHeight']
+        ]
+    },
+    versions: {
+        references: [
+            'window.jQuery.fn.matchHeight.version'
+        ]
+    }
+}
+,
 // src/gitHub/linkedin/dustjs.js
 {
     properties: {
---
+++ src/gitHub/liabru/jquery-match-height.js
@@ -0,0 +1,21 @@
+{
+    properties: {
+        link: 'https://github.com/liabru/jquery-match-height',
+        notes: [],
+        platform: 'GitHub',
+        tests: [
+            'https://www.london.gov.uk/'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            ['window.jQuery.fn.matchHeight']
+        ]
+    },
+    versions: {
+        references: [
+            'window.jQuery.fn.matchHeight.version'
+        ]
+    }
+}

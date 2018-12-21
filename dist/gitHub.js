062680aa917d9756493c6c8c451c881ce9664a3d
onassar <onassar@gmail.com>
2018-12-21 03:22:02 -0500

    New one

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -5693,6 +5693,31 @@
     }
 }
 ,
+// src/gitHub/timdown/rangy.js
+{
+    properties: {
+        link: 'https://github.com/timdown/rangy',
+        notes: [],
+        platform: 'GitHub',
+        tests: [
+            'http://jakiestfu.github.io/Medium.js/docs/'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.rangy'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.rangy.version'
+        ]
+    }
+}
+,
 // src/gitHub/tinymce/tinymce.js
 {
     properties: {
---
+++ src/gitHub/timdown/rangy.js
@@ -0,0 +1,23 @@
+{
+    properties: {
+        link: 'https://github.com/timdown/rangy',
+        notes: [],
+        platform: 'GitHub',
+        tests: [
+            'http://jakiestfu.github.io/Medium.js/docs/'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.rangy'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.rangy.version'
+        ]
+    }
+}

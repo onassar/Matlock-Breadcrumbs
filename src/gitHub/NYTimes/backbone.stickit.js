14ba97492d300f11476f696a390988d73be4def7
onassar <onassar@gmail.com>
2018-12-21 03:53:38 -0500

    New one

--- src/gitHub/NYTimes/backbone.stickit.js
+++ src/gitHub/NYTimes/backbone.stickit.js
@@ -1,9 +1,11 @@
 {
     properties: {
-        link: 'https://github.com/backbone.stickit',
+        link: 'https://github.com/NYTimes/backbone.stickit',
         notes: [],
         platform: 'GitHub',
-        tests: [],
+        tests: [
+            'https://nytimes.github.io/backbone.stickit/'
+        ],
         type: 'repository'
     },
     tests: {

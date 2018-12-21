a5f996fcbec2a60fb84305992d56b58f87c96a9b
onassar <onassar@gmail.com>
2018-12-20 23:19:19 -0500

    New one

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -596,6 +596,28 @@
     }
 }
 ,
+// src/gitHub/ajaxorg/ace.js
+{
+    properties: {
+        link: 'https://github.com/ajaxorg/ace',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.ace',
+                'window.ace.edit'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.ace.version'
+        ]
+    }
+}
+,
 // src/gitHub/akamai/boomerang.js
 {
     properties: {
---
+++ src/gitHub/ajaxorg/ace.js
@@ -0,0 +1,20 @@
+{
+    properties: {
+        link: 'https://github.com/ajaxorg/ace',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.ace',
+                'window.ace.edit'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.ace.version'
+        ]
+    }
+}

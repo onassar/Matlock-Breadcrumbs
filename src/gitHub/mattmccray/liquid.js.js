6daba1e4d4d2d7e47a33175391fdf631a90b089a
onassar <onassar@gmail.com>
2018-12-21 01:44:44 -0500

    New one

---
+++ src/gitHub/mattmccray/liquid.js.js
@@ -0,0 +1,27 @@
+{
+    properties: {
+        link: 'https://github.com/mattmccray/liquid.js',
+        notes: [],
+        platform: 'GitHub',
+        tags: [
+            'Shopify'
+        ],
+        tests: [
+            'https://makitweb.com/take-screenshot-of-webpage-with-html2canvas/'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.Liquid',
+                'window.Liquid.author'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.Liquid.version'
+        ]
+    }
+}

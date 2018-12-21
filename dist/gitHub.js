123e497bd796dbf2f3c3daa064ee7eaf77427c47
onassar <onassar@gmail.com>
2018-12-21 03:29:31 -0500

    New one

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -3355,6 +3355,32 @@
     }
 }
 ,
+// src/gitHub/jakiestfu/Ripple.js.js
+{
+    properties: {
+        link: 'https://github.com/jakiestfu/Ripple.js',
+        notes: [],
+        platform: 'GitHub',
+        tests: [
+            'http://jakiestfu.github.io/Ripple.js/demo/'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.ripple',
+                'window.jQuery.ripple.version'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.jQuery.ripple.version'
+        ]
+    }
+}
+,
 // src/gitHub/janl/mustache.js
 {
     properties: {
---
+++ src/gitHub/jakiestfu/Ripple.js.js
@@ -0,0 +1,24 @@
+{
+    properties: {
+        link: 'https://github.com/jakiestfu/Ripple.js',
+        notes: [],
+        platform: 'GitHub',
+        tests: [
+            'http://jakiestfu.github.io/Ripple.js/demo/'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.ripple',
+                'window.jQuery.ripple.version'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.jQuery.ripple.version'
+        ]
+    }
+}

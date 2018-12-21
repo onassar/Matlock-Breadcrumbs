aa33729211180e61de4cf50f5f0a0d1a4c170bbf
onassar <onassar@gmail.com>
2018-12-20 22:53:51 -0500

    New one

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -4277,6 +4277,27 @@
     }
 }
 ,
+// src/gitHub/socketio/socket.io.js
+{
+    properties: {
+        link: 'https://github.com/socketio/socket.io',
+        notes: [],
+        platform: 'GitHub',
+        tests: [
+            'https://socket.io/'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.io',
+                'window.io.Socket'
+            ]
+        ]
+    }
+}
+,
 // src/gitHub/somewebmedia/hc-sticky.js
 {
     properties: {
---
+++ src/gitHub/socketio/socket.io.js
@@ -0,0 +1,19 @@
+{
+    properties: {
+        link: 'https://github.com/socketio/socket.io',
+        notes: [],
+        platform: 'GitHub',
+        tests: [
+            'https://socket.io/'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.io',
+                'window.io.Socket'
+            ]
+        ]
+    }
+}

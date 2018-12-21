9e7e8bdd7b8b9aec3512caf6bba1f4d2955b8ba8
onassar <onassar@gmail.com>
2018-12-20 20:34:46 -0500

    New one

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -397,6 +397,31 @@
     }
 }
 ,
+// src/gitHub/Valve/fingerprintjs2.js
+{
+    properties: {
+        link: 'https://github.com/Valve/fingerprintjs2',
+        notes: [],
+        platform: 'GitHub',
+        tests: [
+            'http://valve.github.io/fingerprintjs2/'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.Fingerprint2'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.Fingerprint2.VERSION'
+        ]
+    }
+}
+,
 // src/gitHub/VincentGarreau/particles.js.js
 {
     properties: {
---
+++ src/gitHub/Valve/fingerprintjs2.js
@@ -0,0 +1,23 @@
+{
+    properties: {
+        link: 'https://github.com/Valve/fingerprintjs2',
+        notes: [],
+        platform: 'GitHub',
+        tests: [
+            'http://valve.github.io/fingerprintjs2/'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.Fingerprint2'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.Fingerprint2.VERSION'
+        ]
+    }
+}

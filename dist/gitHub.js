836835679ce04384552b7c075f1ed1859e2214ce
onassar <onassar@gmail.com>
2018-12-21 03:33:57 -0500

    New one

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -689,12 +689,16 @@
     properties: {
         link: 'https://github.com/adobe-webplatform/Snap.svg',
         platform: 'GitHub',
+        tests: [
+            'http://snapsvg.io/'
+        ],
         type: 'repository'
     },
     tests: {
         references: [
             [
-                'window.Snap'
+                'window.Snap',
+                'window.Snap.parseTransformString',
             ]
         ]
     },
--- src/gitHub/adobe-webplatform/Snap.svg.js
+++ src/gitHub/adobe-webplatform/Snap.svg.js
@@ -2,12 +2,16 @@
     properties: {
         link: 'https://github.com/adobe-webplatform/Snap.svg',
         platform: 'GitHub',
+        tests: [
+            'http://snapsvg.io/'
+        ],
         type: 'repository'
     },
     tests: {
         references: [
             [
-                'window.Snap'
+                'window.Snap',
+                'window.Snap.parseTransformString',
             ]
         ]
     },

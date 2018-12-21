4044b81eaacb9e2242dc4dfacba34e1592ec84cf
onassar <onassar@gmail.com>
2018-12-21 03:19:04 -0500

    New one

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -3351,6 +3351,29 @@
     }
 }
 ,
+// src/gitHub/jaredreich/pell.js
+{
+    properties: {
+        link: 'https://github.com/jaredreich/pell',
+        notes: [],
+        platform: 'GitHub',
+        tests: [
+            'https://www.cssscript.com/demo/minimalist-wysiwyg-editor-pell-js/'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.pell',
+                'window.pell.default',
+                'window.pell.init',
+                'window.pell.__esModule'
+            ]
+        ]
+    }
+}
+,
 // src/gitHub/jashkenas/backbone.js
 {
     dependencies: [
---
+++ src/gitHub/jaredreich/pell.js
@@ -0,0 +1,21 @@
+{
+    properties: {
+        link: 'https://github.com/jaredreich/pell',
+        notes: [],
+        platform: 'GitHub',
+        tests: [
+            'https://www.cssscript.com/demo/minimalist-wysiwyg-editor-pell-js/'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.pell',
+                'window.pell.default',
+                'window.pell.init',
+                'window.pell.__esModule'
+            ]
+        ]
+    }
+}

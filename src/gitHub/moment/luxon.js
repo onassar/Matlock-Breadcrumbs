db751c65a1164b504e8085be06a22652e5ede9d3
onassar <onassar@gmail.com>
2018-12-20 19:40:21 -0500

    New one

---
+++ src/gitHub/moment/luxon.js
@@ -0,0 +1,21 @@
+{
+    properties: {
+        link: 'https://github.com/moment/luxon',
+        platform: 'GitHub',
+        tags: [
+            'datetime'
+        ],
+        tests: [
+            'https://moment.github.io/luxon/docs/'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.luxon',
+                'window.luxon.DateTime'
+            ]
+        ]
+    }
+}

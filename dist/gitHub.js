5ef8c6969b144af009b3a149fe426f392e3bee29
onassar <onassar@gmail.com>
2018-12-21 00:32:21 -0500

    New one

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -817,6 +817,31 @@
     }
 }
 ,
+// src/gitHub/ampproject/amphtml.js
+{
+    properties: {
+        link: 'https://github.com/ampproject/amphtml',
+        notes: [],
+        platform: 'GitHub',
+        tests: [
+            'https://www.independent.co.uk/news/world/americas/us-politics/trump-james-mattis-defense-secretary-white-house-tweet-replace-retire-latest-a8693756.html'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.AMP_CONFIG'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.AMP_CONFIG.v'
+        ]
+    }
+}
+,
 // src/gitHub/andris9/jStorage.js
 {
     properties: {

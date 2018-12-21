2e213bd9e778c22613c7a1d9e65ddea4fd63a53c
onassar <onassar@gmail.com>
2018-12-21 03:06:58 -0500

    New one

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -4626,6 +4626,32 @@
     }
 }
 ,
+// src/gitHub/neilj/Squire.js
+{
+    properties: {
+        link: 'https://github.com/neilj/Squire',
+        notes: [],
+        platform: 'GitHub',
+        tests: [
+            'http://neilj.github.io/Squire/'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.Squire',
+                'window.Squire.empty'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.Squire'
+        ]
+    }
+}
+,
 // src/gitHub/netlify/netlify-cms.js
 {
     properties: {
---
+++ src/gitHub/neilj/Squire.js
@@ -0,0 +1,24 @@
+{
+    properties: {
+        link: 'https://github.com/neilj/Squire',
+        notes: [],
+        platform: 'GitHub',
+        tests: [
+            'http://neilj.github.io/Squire/'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.Squire',
+                'window.Squire.empty'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.Squire'
+        ]
+    }
+}

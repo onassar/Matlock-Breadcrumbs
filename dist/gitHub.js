b9c15403ff00014d0c93b0b03ba04958cccd6c79
onassar <onassar@gmail.com>
2018-12-21 01:49:58 -0500

    New one

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -4812,6 +4812,32 @@
     }
 }
 ,
+// src/gitHub/sciactive/pnotify.js
+{
+    properties: {
+        link: 'https://github.com/sciactive/pnotify',
+        notes: [],
+        platform: 'GitHub',
+        tests: [
+            'https://sciactive.com/pnotify/',
+            'https://packagemapping.com/'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.PNotify'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.PNotify.prototype.version'
+        ]
+    }
+}
+,
 // src/gitHub/scottjehl/Respond.js
 {
     properties: {
---
+++ src/gitHub/sciactive/pnotify.js
@@ -0,0 +1,24 @@
+{
+    properties: {
+        link: 'https://github.com/sciactive/pnotify',
+        notes: [],
+        platform: 'GitHub',
+        tests: [
+            'https://sciactive.com/pnotify/',
+            'https://packagemapping.com/'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.PNotify'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.PNotify.prototype.version'
+        ]
+    }
+}

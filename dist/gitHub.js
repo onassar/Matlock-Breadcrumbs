488490a6e7ec868e0858ddfe677a1910caa3c679
onassar <onassar@gmail.com>
2018-12-21 02:54:10 -0500

    New one

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -424,6 +424,27 @@
     }
 }
 ,
+// src/gitHub/PrismJS/prism.js
+{
+    properties: {
+        link: 'https://github.com/PrismJS/prism',
+        notes: [],
+        platform: 'GitHub',
+        tests: [
+            'https://prismjs.com/'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.Prism',
+                'window.Prism.highlight'
+            ]
+        ]
+    }
+}
+,
 // src/gitHub/RubaXa/Sortable.js
 {
     properties: {
---
+++ src/gitHub/PrismJS/prism.js
@@ -0,0 +1,19 @@
+{
+    properties: {
+        link: 'https://github.com/PrismJS/prism',
+        notes: [],
+        platform: 'GitHub',
+        tests: [
+            'https://prismjs.com/'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.Prism',
+                'window.Prism.highlight'
+            ]
+        ]
+    }
+}

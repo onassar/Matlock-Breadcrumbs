ac22a2d4290f0603d8c6e1dd2d1529116a7f910d
onassar <onassar@gmail.com>
2018-12-21 01:33:20 -0500

    New one

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -2375,6 +2375,31 @@
     }
 }
 ,
+// src/gitHub/froala/wysiwyg-editor.js
+{
+    properties: {
+        link: 'https://github.com/froala/wysiwyg-editor',
+        notes: [],
+        platform: 'GitHub',
+        tests: [
+            'https://www.froala.com/wysiwyg-editor'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.FroalaEditor'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.FroalaEditor.VERSION'
+        ]
+    }
+}
+,
 // src/gitHub/ftlabs/fastclick.js
 {
     properties: {
---
+++ src/gitHub/froala/wysiwyg-editor.js
@@ -0,0 +1,23 @@
+{
+    properties: {
+        link: 'https://github.com/froala/wysiwyg-editor',
+        notes: [],
+        platform: 'GitHub',
+        tests: [
+            'https://www.froala.com/wysiwyg-editor'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.FroalaEditor'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.FroalaEditor.VERSION'
+        ]
+    }
+}

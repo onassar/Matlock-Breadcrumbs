ac73e0daabed0d1601d391e2a906db52da40c609
onassar <onassar@gmail.com>
2018-12-21 13:08:12 -0500

    New one

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -5861,6 +5861,26 @@
     }
 }
 ,
+// src/gitHub/ten1seven/what-input.js
+{
+    properties: {
+        link: 'https://github.com/ten1seven/what-input',
+        notes: [],
+        platform: 'GitHub',
+        tests: [
+            'https://ten1seven.github.io/what-input/'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.whatInput'
+            ]
+        ]
+    }
+}
+,
 // src/gitHub/timdown/rangy.js
 {
     properties: {
---
+++ src/gitHub/ten1seven/what-input.js
@@ -0,0 +1,18 @@
+{
+    properties: {
+        link: 'https://github.com/ten1seven/what-input',
+        notes: [],
+        platform: 'GitHub',
+        tests: [
+            'https://ten1seven.github.io/what-input/'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.whatInput'
+            ]
+        ]
+    }
+}

45558df1d22b74854f025eada552d7cb0432034c
onassar <onassar@gmail.com>
2018-12-20 19:31:39 -0500

    New one

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -207,6 +207,35 @@
     }
 }
 ,
+// src/gitHub/MithrilJS/mithril.js.js
+{
+    properties: {
+        link: 'https://github.com/MithrilJS/mithril.js',
+        notes: [],
+        platform: 'GitHub',
+        tests: [
+            'https://www.pexels.com/',
+            'https://mithril.js.org/api.html'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.m',
+                'window.m.buildQueryString',
+                'window.m.parseQueryString',
+                'window.m.request'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.m.version'
+        ]
+    }
+}
+,
 // src/gitHub/Modernizr/Modernizr.js
 {
     properties: {
---
+++ src/gitHub/MithrilJS/mithril.js.js
@@ -0,0 +1,27 @@
+{
+    properties: {
+        link: 'https://github.com/MithrilJS/mithril.js',
+        notes: [],
+        platform: 'GitHub',
+        tests: [
+            'https://www.pexels.com/',
+            'https://mithril.js.org/api.html'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.m',
+                'window.m.buildQueryString',
+                'window.m.parseQueryString',
+                'window.m.request'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.m.version'
+        ]
+    }
+}

72e11c7be95d1acf3bfd180a2e2b1535fa3b534c
onassar <onassar@gmail.com>
2018-12-21 02:04:31 -0500

    New one

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -1528,6 +1528,39 @@
     }
 }
 ,
+// src/gitHub/ckeditor/ckeditor-dev.js
+{
+    properties: {
+        link: 'https://github.com/ckeditor/ckeditor-dev',
+        notes: [],
+        platform: 'GitHub',
+        tests: [
+            'https://ckeditor.com/ckeditor-4/demo/'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.ContentTools',
+                'window.ContentTools.Tools'
+            ]
+        ]
+    },
+    tests: {
+        references: [
+            [
+                'window.CKEDITOR'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.CKEDITOR.version'
+        ]
+    }
+}
+,
 // src/gitHub/cloudinary/cloudinary_js.js
 {
     properties: {
---
+++ src/gitHub/ckeditor/ckeditor-dev.js
@@ -0,0 +1,31 @@
+{
+    properties: {
+        link: 'https://github.com/ckeditor/ckeditor-dev',
+        notes: [],
+        platform: 'GitHub',
+        tests: [
+            'https://ckeditor.com/ckeditor-4/demo/'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.ContentTools',
+                'window.ContentTools.Tools'
+            ]
+        ]
+    },
+    tests: {
+        references: [
+            [
+                'window.CKEDITOR'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.CKEDITOR.version'
+        ]
+    }
+}

36a81af6296a9e0ce207f4cad1c3b3855fd44fc7
onassar <onassar@gmail.com>
2018-12-21 01:04:51 -0500

    New one

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -1014,6 +1014,35 @@
     }
 }
 ,
+// src/gitHub/arasatasaygin/is.js.js
+{
+    properties: {
+        link: 'https://github.com/arasatasaygin/is.js',
+        notes: [],
+        platform: 'GitHub',
+        tests: [
+            'https://www.bundestag.de/en/'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.is',
+                'window.is.affirmative',
+                'window.is.weekday',
+                'window.is.weekend',
+                'window.is.VERSION'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.is.VERSION'
+        ]
+    }
+}
+,
 // src/gitHub/auth0/lock.js
 {
     properties: {
---
+++ src/gitHub/arasatasaygin/is.js.js
@@ -0,0 +1,27 @@
+{
+    properties: {
+        link: 'https://github.com/arasatasaygin/is.js',
+        notes: [],
+        platform: 'GitHub',
+        tests: [
+            'https://www.bundestag.de/en/'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.is',
+                'window.is.affirmative',
+                'window.is.weekday',
+                'window.is.weekend',
+                'window.is.VERSION'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.is.VERSION'
+        ]
+    }
+}

2afc852ba1b98cc5e6f5fd423106b109689ee5f8
onassar <onassar@gmail.com>
2018-12-22 17:32:30 -0500

    New one

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -1962,7 +1962,15 @@
     properties: {
         link: 'https://github.com/d3/d3',
         platform: 'GitHub',
-        type: 'repository'
+        tests: [
+            'https://bower.io/'
+        ],
+        type: 'repository',
+        wikiPedia: {
+            load: true,
+            title: 'D3.js',
+            sections: []
+        }
     },
     tests: {
         references: [

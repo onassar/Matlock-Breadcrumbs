558e582de6d363d136a56a2b0d6b67e983ef8688
onassar <onassar@gmail.com>
2018-12-22 15:10:29 -0500

    New one

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -2422,7 +2422,15 @@
     properties: {
         link: 'https://github.com/expressjs/express',
         platform: 'GitHub',
-        type: 'repository'
+        tests: [
+            'https://www.nbcnews.com/politics/national-security/russians-launched-pro-jill-stein-social-media-blitz-help-trump-n951166'
+        ],
+        type: 'repository',
+        wikiPedia: {
+            load: true,
+            title: 'Express.js',
+            sections: []
+        }
     },
     tests: {
         patterns: {

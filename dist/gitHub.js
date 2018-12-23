5ec59c63c481c96a7fbd87b7bbb98d76f76ef159
onassar <onassar@gmail.com>
2018-12-23 15:15:48 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -1467,6 +1467,32 @@
     }
 }
 ,
+// src/gitHub/auth0/auth0.js.js
+{
+    properties: {
+        link: 'https://github.com/auth0/auth0.js',
+        notes: [],
+        platform: 'GitHub',
+        tests: [
+            'https://community.dentistry.com/'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.auth0',
+                'window.auth0.WebAuth'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.auth0.version'
+        ]
+    }
+}
+,
 // src/gitHub/auth0/lock.js
 {
     properties: {
---
+++ src/gitHub/auth0/auth0.js.js
@@ -0,0 +1,24 @@
+{
+    properties: {
+        link: 'https://github.com/auth0/auth0.js',
+        notes: [],
+        platform: 'GitHub',
+        tests: [
+            'https://community.dentistry.com/'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.auth0',
+                'window.auth0.WebAuth'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.auth0.version'
+        ]
+    }
+}

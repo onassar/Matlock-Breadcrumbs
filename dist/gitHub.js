1a056fa5aab27ac213edb341e661c08e67ccd499
onassar <onassar@gmail.com>
2018-12-23 15:42:06 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -7432,6 +7432,25 @@
     }
 }
 ,
+// src/gitHub/upstage/showup.js
+{
+    properties: {
+        link: 'https://github.com/upstage/showup',
+        platform: 'GitHub',
+        tests: [
+            'http://assemble.io/'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.fn.showUp'
+            ]
+        ]
+    }
+}
+,
 // src/gitHub/urish/angular-moment.js
 {
     properties: {
---
+++ src/gitHub/upstage/showup.js
@@ -0,0 +1,17 @@
+{
+    properties: {
+        link: 'https://github.com/upstage/showup',
+        platform: 'GitHub',
+        tests: [
+            'http://assemble.io/'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.fn.showUp'
+            ]
+        ]
+    }
+}

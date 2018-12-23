b2cb12f8ff728111cf7137186bb8c956c39e056d
onassar <onassar@gmail.com>
2018-12-23 14:32:23 -0500

    New ones

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -4256,6 +4256,30 @@
     }
 }
 ,
+// src/gitHub/jquery-form/form.js
+{
+    properties: {
+        link: 'https://github.com/jquery-form/form',
+        notes: [],
+        platform: 'GitHub',
+        tests: [
+            'http://malsup.com/jquery/form/#ajaxForm'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.ajaxForm',
+                'window.jQuery.ajaxSubmit',
+                'window.jQuery.formSerialize',
+                'window.jQuery.fieldSerialize',
+                'window.jQuery.fieldValue'
+            ]
+        ]
+    }
+}
+,
 // src/gitHub/jquery-validation/jquery-validation.js
 {
     properties: {
---
+++ src/gitHub/jquery-form/form.js
@@ -0,0 +1,22 @@
+{
+    properties: {
+        link: 'https://github.com/jquery-form/form',
+        notes: [],
+        platform: 'GitHub',
+        tests: [
+            'http://malsup.com/jquery/form/#ajaxForm'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.ajaxForm',
+                'window.jQuery.ajaxSubmit',
+                'window.jQuery.formSerialize',
+                'window.jQuery.fieldSerialize',
+                'window.jQuery.fieldValue'
+            ]
+        ]
+    }
+}

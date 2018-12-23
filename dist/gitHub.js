1e00d9ec5e688e740aab020a8f8002bf16b733d0
onassar <onassar@gmail.com>
2018-12-22 22:43:33 -0500

    New one

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -7032,6 +7032,30 @@
     }
 }
 ,
+// src/gitHub/uxsolutions/bootstrap-datepicker.js
+{
+    properties: {
+        link: 'https://github.com/uxsolutions/bootstrap-datepicker',
+        platform: 'GitHub',
+        tests: [
+            'https://uxsolutions.github.io/bootstrap-datepicker/?markup=input&format=&weekStart=&startDate=&endDate=&startView=0&minViewMode=0&maxViewMode=4&todayBtn=false&clearBtn=false&language=en&orientation=auto&multidate=&multidateSeparator=&keyboardNavigation=on&forceParse=on#sandbox'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.fn.datepicker.DPGlobal'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.jQuery.fn.datepicker.version'
+        ]
+    }
+}
+,
 // src/gitHub/videojs/video.js.js
 {
     properties: {

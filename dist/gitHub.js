95941058e8c2e6626581bea1449bfcbe7b3ade2d
onassar <onassar@gmail.com>
2018-12-22 21:31:18 -0500

    New one

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -6413,6 +6413,55 @@
     }
 }
 ,
+// src/gitHub/sweetalert2/sweetalert2.js
+{
+    properties: {
+        donate: {
+            link: 'https://www.patreon.com/limonte'
+        },
+        link: 'https://github.com/sweetalert2/sweetalert2',
+        notes: [],
+        platform: 'GitHub',
+        tests: [
+            'https://sweetalert2.github.io/'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.swal',
+                'window.swal.setDefaults',
+            ],
+            [
+                'window.Swal',
+                'window.Swal.setDefaults',
+            ],
+            [
+                'window.sweetAlert',
+                'window.sweetAlert.setDefaults',
+            ],
+            [
+                'window.SweetAlert',
+                'window.SweetAlert.setDefaults',
+            ],
+            [
+                'window.Sweetalert2',
+                'window.Sweetalert2.setDefaults',
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.swal.version',
+            'window.Swal.version',
+            'window.sweetAlert.version',
+            'window.SweetAlert.version',
+            'window.Sweetalert2.version'
+        ]
+    }
+}
+,
 // src/gitHub/swfobject/swfobject.js
 {
     properties: {
---
+++ src/gitHub/sweetalert2/sweetalert2.js
@@ -0,0 +1,47 @@
+{
+    properties: {
+        donate: {
+            link: 'https://www.patreon.com/limonte'
+        },
+        link: 'https://github.com/sweetalert2/sweetalert2',
+        notes: [],
+        platform: 'GitHub',
+        tests: [
+            'https://sweetalert2.github.io/'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.swal',
+                'window.swal.setDefaults',
+            ],
+            [
+                'window.Swal',
+                'window.Swal.setDefaults',
+            ],
+            [
+                'window.sweetAlert',
+                'window.sweetAlert.setDefaults',
+            ],
+            [
+                'window.SweetAlert',
+                'window.SweetAlert.setDefaults',
+            ],
+            [
+                'window.Sweetalert2',
+                'window.Sweetalert2.setDefaults',
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.swal.version',
+            'window.Swal.version',
+            'window.sweetAlert.version',
+            'window.SweetAlert.version',
+            'window.Sweetalert2.version'
+        ]
+    }
+}

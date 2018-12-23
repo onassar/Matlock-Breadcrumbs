d5bedf94c14ff3dd47635ab4f4aea239bd307cde
onassar <onassar@gmail.com>
2018-12-22 23:08:15 -0500

    New one

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -4977,7 +4977,8 @@
         link: 'https://github.com/mattbryson/TouchSwipe-Jquery-Plugin',
         platform: 'GitHub',
         tests: [
-            'https://www.tsx.com/'
+            'https://www.tsx.com/',
+            'http://www.africa-business.com/news-africa/wp-content/plugins/bwp-minify/cache/minify_jquery.themepunch.tools.min,jquery.themepunch.revolution.min_68fa8acd2b8e0474c1b74ef316b329fa'
         ],
         type: 'repository'
     },
@@ -4986,12 +4987,16 @@
             [
                 'window.jQuery.fn.swipe',
                 'window.jQuery.fn.swipe.directions'
+            ],
+            [
+                'window.jQuery.fn.swipetp',
+                'window.jQuery.fn.swipetp.directions'
             ]
         ]
     },
     versions: {
         references: [
-            'window.BOOMR.version'
+            'window.jQuery.swipetp.version'
         ]
     }
 }

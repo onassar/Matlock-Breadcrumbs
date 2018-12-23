b74eeeb391bcd888713dd62f99eff3c904698eb0
onassar <onassar@gmail.com>
2018-12-23 15:34:49 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -5703,6 +5703,29 @@
     }
 }
 ,
+// src/gitHub/mootools/slick.js
+{
+    properties: {
+        link: 'https://github.com/mootools/slick',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.Slick',
+                'window.Slick.contains',
+                'window.Slick.escapeRegExp'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.Slick.version'
+        ]
+    }
+}
+,
 // src/gitHub/mozilla/pdf.js.js
 {
     properties: {
---
+++ src/gitHub/mootools/slick.js
@@ -0,0 +1,21 @@
+{
+    properties: {
+        link: 'https://github.com/mootools/slick',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.Slick',
+                'window.Slick.contains',
+                'window.Slick.escapeRegExp'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.Slick.version'
+        ]
+    }
+}

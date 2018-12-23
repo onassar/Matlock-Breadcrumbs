9e4775f084ad7775c00537eade4e46995815692c
onassar <onassar@gmail.com>
2018-12-23 16:08:20 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -783,6 +783,13 @@
 // src/gitHub/Voog/wysihtml.js
 {
     properties: {
+        alternatives: [
+            'https://github.com/tinymce/tinymce',
+            'https://github.com/froala/wysiwyg-editor',
+            'https://github.com/yabwe/medium-editor',
+            'https://github.com/ckeditor/ckeditor5',
+            'https://github.com/ckeditor/ckeditor-dev'
+        ],
         link: 'https://github.com/Voog/wysihtml',
         name: 'wysihtml',
         platform: 'GitHub',
--- src/gitHub/Voog/wysihtml.js
+++ src/gitHub/Voog/wysihtml.js
@@ -1,5 +1,12 @@
 {
     properties: {
+        alternatives: [
+            'https://github.com/tinymce/tinymce',
+            'https://github.com/froala/wysiwyg-editor',
+            'https://github.com/yabwe/medium-editor',
+            'https://github.com/ckeditor/ckeditor5',
+            'https://github.com/ckeditor/ckeditor-dev'
+        ],
         link: 'https://github.com/Voog/wysihtml',
         name: 'wysihtml',
         platform: 'GitHub',

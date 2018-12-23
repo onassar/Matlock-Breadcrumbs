39c45ee2ea2b79a99b9a432a6eef64058b781d68
onassar <onassar@gmail.com>
2018-12-23 16:05:49 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -780,6 +780,31 @@
     }
 }
 ,
+// src/gitHub/Voog/wysihtml.js
+{
+    properties: {
+        link: 'https://github.com/Voog/wysihtml',
+        name: 'wysihtml',
+        platform: 'GitHub',
+        tests: [
+            'http://wysihtml.com/'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.wysihtml5'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.wysihtml5.version'
+        ]
+    }
+}
+,
 // src/gitHub/WeCodePixels/theia-sticky-sidebar.js
 {
     properties: {
---
+++ src/gitHub/Voog/wysihtml.js
@@ -0,0 +1,23 @@
+{
+    properties: {
+        link: 'https://github.com/Voog/wysihtml',
+        name: 'wysihtml',
+        platform: 'GitHub',
+        tests: [
+            'http://wysihtml.com/'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.wysihtml5'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.wysihtml5.version'
+        ]
+    }
+}

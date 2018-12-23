821762db6e15b53943272d90b8f09c3203ddc89e
onassar <onassar@gmail.com>
2018-12-22 22:28:50 -0500

    New one

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -228,6 +228,31 @@
     }
 }
 ,
+// src/gitHub/FrDH/jQuery.dotdotdot.js
+{
+    properties: {
+        link: 'https://github.com/keen/keen-tracking.js',
+        platform: 'GitHub',
+        tests: [
+            'https://keen.io/'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.dotdotdot',
+                'window.jQuery.dotdotdot.defaults'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.jQuery.dotdotdot.version'
+        ]
+    }
+}
+,
 // src/gitHub/GetmeUK/ContentTools.js
 {
     properties: {
---
+++ src/gitHub/FrDH/jQuery.dotdotdot.js
@@ -0,0 +1,23 @@
+{
+    properties: {
+        link: 'https://github.com/keen/keen-tracking.js',
+        platform: 'GitHub',
+        tests: [
+            'https://keen.io/'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.dotdotdot',
+                'window.jQuery.dotdotdot.defaults'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.jQuery.dotdotdot.version'
+        ]
+    }
+}

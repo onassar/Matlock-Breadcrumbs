7bb4ad8126833c9cbc515611cc7cc833c7d6e41b
onassar <onassar@gmail.com>
2018-12-22 19:23:23 -0500

    New one

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -5191,6 +5191,34 @@
     }
 }
 ,
+// src/gitHub/nanostudio-org/nanogallery2.js
+{
+    properties: {
+        link: 'https://github.com/nanostudio-org/nanogallery2',
+        notes: [],
+        platform: 'GitHub',
+        tags: [
+            'lightbox'
+        ],
+        tests: [],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.NGHammer',
+                'window.jQuery.nanogallery2',
+                'window.jQuery.fn.nanogallery2'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.NGHammer.version'
+        ]
+    }
+}
+,
 // src/gitHub/neilj/Squire.js
 {
     properties: {
---
+++ src/gitHub/nanostudio-org/nanogallery2.js
@@ -0,0 +1,26 @@
+{
+    properties: {
+        link: 'https://github.com/nanostudio-org/nanogallery2',
+        notes: [],
+        platform: 'GitHub',
+        tags: [
+            'lightbox'
+        ],
+        tests: [],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.NGHammer',
+                'window.jQuery.nanogallery2',
+                'window.jQuery.fn.nanogallery2'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.NGHammer.version'
+        ]
+    }
+}

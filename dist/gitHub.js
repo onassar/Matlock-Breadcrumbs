1daabd68e6bcf51013d718d5ec85300d6e85d0c2
onassar <onassar@gmail.com>
2018-12-22 23:14:27 -0500

    New one

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -410,6 +410,31 @@
     }
 }
 ,
+// src/gitHub/Mottie/tablesorter.js
+{
+    properties: {
+        link: 'https://github.com/Mottie/tablesorter',
+        platform: 'GitHub',
+        tests: [
+            'https://newscapegroup.com/'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.tablesorter',
+                'window.jQuery.tablesorter.addParser'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.jQuery.tablesorter.version'
+        ]
+    }
+}
+,
 // src/gitHub/NYTimes/backbone.stickit.js
 {
     properties: {
---
+++ src/gitHub/Mottie/tablesorter.js
@@ -0,0 +1,23 @@
+{
+    properties: {
+        link: 'https://github.com/Mottie/tablesorter',
+        platform: 'GitHub',
+        tests: [
+            'https://newscapegroup.com/'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.tablesorter',
+                'window.jQuery.tablesorter.addParser'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.jQuery.tablesorter.version'
+        ]
+    }
+}

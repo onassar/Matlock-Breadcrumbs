9543d3eafa25be7ce1ad08663ded9b7085fce436
onassar <onassar@gmail.com>
2018-12-22 19:03:25 -0500

    New one

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -5823,6 +5823,25 @@
     }
 }
 ,
+// src/gitHub/sachinchoolur/lightGallery.js
+{
+    properties: {
+        link: 'https://github.com/sachinchoolur/lightGallery',
+        notes: [],
+        platform: 'GitHub',
+        tests: [],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.fn.lightGallery',
+                'window.jQuery.fn.lightGallery.modules'
+            ]
+        ]
+    }
+}
+,
 // src/gitHub/scaron/prettyphoto.js
 {
     properties: {
---
+++ src/gitHub/sachinchoolur/lightGallery.js
@@ -0,0 +1,17 @@
+{
+    properties: {
+        link: 'https://github.com/sachinchoolur/lightGallery',
+        notes: [],
+        platform: 'GitHub',
+        tests: [],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.fn.lightGallery',
+                'window.jQuery.fn.lightGallery.modules'
+            ]
+        ]
+    }
+}

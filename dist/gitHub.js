b1ac57a37290b51a141be478e0088b5ca1090ce6
onassar <onassar@gmail.com>
2018-12-22 22:06:19 -0500

    New one

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -3948,6 +3948,30 @@
     }
 }
 ,
+// src/gitHub/jquery-validation/jquery-validation.js
+{
+    properties: {
+        link: 'https://github.com/jquery-validation/jquery-validation',
+        notes: [],
+        platform: 'GitHub',
+        tests: [
+            'https://jqueryvalidation.org/files/demo/errorcontainer-demo.html'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.fn.validate',
+                'window.jQuery.fn.valid',
+                'window.jQuery.fn.rules',
+                'window.jQuery.validator',
+                'window.jQuery.validator.addClassRules'
+            ]
+        ]
+    }
+}
+,
 // src/gitHub/jquery/jquery-migrate.js
 {
     properties: {
---
+++ src/gitHub/jquery-validation/jquery-validation.js
@@ -0,0 +1,22 @@
+{
+    properties: {
+        link: 'https://github.com/jquery-validation/jquery-validation',
+        notes: [],
+        platform: 'GitHub',
+        tests: [
+            'https://jqueryvalidation.org/files/demo/errorcontainer-demo.html'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.fn.validate',
+                'window.jQuery.fn.valid',
+                'window.jQuery.fn.rules',
+                'window.jQuery.validator',
+                'window.jQuery.validator.addClassRules'
+            ]
+        ]
+    }
+}

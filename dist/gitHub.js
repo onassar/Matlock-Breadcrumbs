6dc72ef92f20d3a67a571e93f8f64bc7c746d271
onassar <onassar@gmail.com>
2018-12-23 00:36:11 -0500

    New one

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -755,6 +755,26 @@
     }
 }
 ,
+// src/gitHub/WeCodePixels/theia-sticky-sidebar.js
+{
+    properties: {
+        link: 'https://github.com/WeCodePixels/theia-sticky-sidebar',
+        notes: [],
+        platform: 'GitHub',
+        tests: [
+            'http://htmlpreview.github.io/?https://github.com/liviucmg/theia-sticky-sidebar/blob/master/examples/4-columns.html'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        statements: [
+            [
+                'window.jQuery(\'div\').theiaStickySidebar'
+            ]
+        ]
+    }
+}
+,
 // src/gitHub/WordPress/WordPress.js
 {
     dependencies: [

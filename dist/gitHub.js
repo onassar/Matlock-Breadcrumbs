e5c8cd6486e8685ff9299a2cbb240090211c22d3
onassar <onassar@gmail.com>
2018-12-23 15:40:35 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -4282,6 +4282,26 @@
     }
 }
 ,
+// src/gitHub/jonschlinkert/remarkable.js
+{
+    properties: {
+        link: 'https://github.com/jonschlinkert/remarkable',
+        platform: 'GitHub',
+        tests: [
+            'https://jonschlinkert.github.io/remarkable/demo/'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.Remarkable',
+                'window.Remarkable.utils'
+            ]
+        ]
+    }
+}
+,
 // src/gitHub/jquery-form/form.js
 {
     properties: {
---
+++ src/gitHub/jonschlinkert/remarkable.js
@@ -0,0 +1,18 @@
+{
+    properties: {
+        link: 'https://github.com/jonschlinkert/remarkable',
+        platform: 'GitHub',
+        tests: [
+            'https://jonschlinkert.github.io/remarkable/demo/'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.Remarkable',
+                'window.Remarkable.utils'
+            ]
+        ]
+    }
+}

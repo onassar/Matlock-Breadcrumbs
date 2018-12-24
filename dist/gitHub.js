f4c3cb439b9ddbf83969d57ab2d1a8ae46b4c010
onassar <onassar@gmail.com>
2018-12-23 22:56:03 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -5990,6 +5990,32 @@
     }
 }
 ,
+// src/gitHub/naver/billboard.js.js
+{
+    properties: {
+        link: 'https://github.com/naver/billboard.js',
+        platform: 'GitHub',
+        tests: [
+            'https://github.com/naver/billboard.js'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.bb',
+                'window.bb.chart',
+                'window.bb.generate'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.bb.version'
+        ]
+    }
+}
+,
 // src/gitHub/neilj/Squire.js
 {
     properties: {
---
+++ src/gitHub/naver/billboard.js.js
@@ -0,0 +1,24 @@
+{
+    properties: {
+        link: 'https://github.com/naver/billboard.js',
+        platform: 'GitHub',
+        tests: [
+            'https://github.com/naver/billboard.js'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.bb',
+                'window.bb.chart',
+                'window.bb.generate'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.bb.version'
+        ]
+    }
+}

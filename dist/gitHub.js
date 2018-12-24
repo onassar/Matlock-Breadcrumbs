18d32875b6100777c1120fa05a722694e6980fe4
onassar <onassar@gmail.com>
2018-12-24 04:52:31 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -234,6 +234,31 @@
     }
 }
 ,
+// src/gitHub/FrDH/jQuery.TosRUs.js
+{
+    properties: {
+        link: 'https://github.com/FrDH/jQuery.TosRUs',
+        platform: 'GitHub',
+        tests: [
+            'http://tosrus.frebsite.nl/'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.tosrus',
+                'window.jQuery.tosrus.defaults'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.jQuery.tosrus.version'
+        ]
+    }
+}
+,
 // src/gitHub/FrDH/jQuery.dotdotdot.js
 {
     properties: {
---
+++ src/gitHub/FrDH/jQuery.TosRUs.js
@@ -0,0 +1,23 @@
+{
+    properties: {
+        link: 'https://github.com/FrDH/jQuery.TosRUs',
+        platform: 'GitHub',
+        tests: [
+            'http://tosrus.frebsite.nl/'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.tosrus',
+                'window.jQuery.tosrus.defaults'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.jQuery.tosrus.version'
+        ]
+    }
+}

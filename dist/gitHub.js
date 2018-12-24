b52d20255427846916e08d25cbddcc089e8e2edd
onassar <onassar@gmail.com>
2018-12-24 03:09:33 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -7312,6 +7312,33 @@
     }
 }
 ,
+// src/gitHub/pusher/pusher-js.js
+{
+    properties: {
+        link: 'https://github.com/pusher/pusher-js',
+        name: 'Pusher Javascript Client',
+        platform: 'GitHub',
+        tests: [
+            'https://www.nytimes.com/interactive/2016/sports/olympics/rio-olympics-photo-firehose.html'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.Pusher',
+                'window.Pusher.HTTP',
+                'window.Pusher.Base64'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.Pusher.VERSION'
+        ]
+    }
+}
+,
 // src/gitHub/quilljs/quill.js
 {
     properties: {
---
+++ src/gitHub/pusher/pusher-js.js
@@ -0,0 +1,25 @@
+{
+    properties: {
+        link: 'https://github.com/pusher/pusher-js',
+        name: 'Pusher Javascript Client',
+        platform: 'GitHub',
+        tests: [
+            'https://www.nytimes.com/interactive/2016/sports/olympics/rio-olympics-photo-firehose.html'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.Pusher',
+                'window.Pusher.HTTP',
+                'window.Pusher.Base64'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.Pusher.VERSION'
+        ]
+    }
+}

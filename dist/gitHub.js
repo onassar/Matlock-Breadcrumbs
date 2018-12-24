501d2875ad7b0617ae453aadf14983449803c651
onassar <onassar@gmail.com>
2018-12-24 00:35:22 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -2652,6 +2652,26 @@
     }
 }
 ,
+// src/gitHub/dirkgroenen/SVGMagic.js
+{
+    properties: {
+        link: 'https://github.com/dirkgroenen/SVGMagic',
+        notes: [],
+        platform: 'GitHub',
+        tests: [
+            'http://dirkgroenen.nl/SVGMagic/index.html'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.fn.svgmagic'
+            ]
+        ]
+    }
+}
+,
 // src/gitHub/dirkgroenen/jQuery-viewport-checker.js
 {
     properties: {
---
+++ src/gitHub/dirkgroenen/SVGMagic.js
@@ -0,0 +1,18 @@
+{
+    properties: {
+        link: 'https://github.com/dirkgroenen/SVGMagic',
+        notes: [],
+        platform: 'GitHub',
+        tests: [
+            'http://dirkgroenen.nl/SVGMagic/index.html'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.fn.svgmagic'
+            ]
+        ]
+    }
+}

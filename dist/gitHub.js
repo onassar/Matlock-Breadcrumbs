9d6506ac139443c2551f368063641046eeefd37e
onassar <onassar@gmail.com>
2018-12-24 03:45:54 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -934,6 +934,31 @@
     }
 }
 ,
+// src/gitHub/ZaDarkSide/simpleStorage.js
+{
+    properties: {
+        link: 'https://github.com/ZaDarkSide/simpleStorage',
+        platform: 'GitHub',
+        tests: [
+            'http://tahvel.info/simpleStorage/example/'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.simpleStorage',
+                'window.simpleStorage.canUse'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.simpleStorage.version'
+        ]
+    }
+}
+,
 // src/gitHub/aFarkas/html5shiv.js
 {
     properties: {
---
+++ src/gitHub/ZaDarkSide/simpleStorage.js
@@ -0,0 +1,23 @@
+{
+    properties: {
+        link: 'https://github.com/ZaDarkSide/simpleStorage',
+        platform: 'GitHub',
+        tests: [
+            'http://tahvel.info/simpleStorage/example/'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.simpleStorage',
+                'window.simpleStorage.canUse'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.simpleStorage.version'
+        ]
+    }
+}

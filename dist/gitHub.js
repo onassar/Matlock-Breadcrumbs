53b74add3046c68879e9e69ab0dda25703353929
onassar <onassar@gmail.com>
2018-12-23 23:02:47 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -3578,6 +3578,28 @@
     }
 }
 ,
+// src/gitHub/google/material-design-icons.js
+{
+    properties: {
+        link: 'https://github.com/ionic-team/ionicons',
+        platform: 'GitHub',
+        tests: [
+            'https://lto.network/',
+            'https://ionicons.com/'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        patterns: {
+            page: [
+                [
+                    /\bmaterial-icons md-/i
+                ]
+            ]
+        }
+    }
+}
+,
 // src/gitHub/google/recaptcha.js
 {
     properties: {

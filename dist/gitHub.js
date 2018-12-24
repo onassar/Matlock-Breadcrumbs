e803378f8033a3ac3f159b222c8015ff7baacf97
onassar <onassar@gmail.com>
2018-12-24 00:13:03 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -3803,6 +3803,26 @@
     }
 }
 ,
+// src/gitHub/haltu/muuri.js
+{
+    properties: {
+        link: 'https://github.com/haltu/muuri',
+        platform: 'GitHub',
+        tests: [
+            'https://haltu.github.io/muuri/'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.Muuri',
+                'window.Muuri.defaultOptions'
+            ]
+        ]
+    }
+}
+,
 // src/gitHub/hammerjs/hammer.js.js
 {
     properties: {
---
+++ src/gitHub/haltu/muuri.js
@@ -0,0 +1,18 @@
+{
+    properties: {
+        link: 'https://github.com/haltu/muuri',
+        platform: 'GitHub',
+        tests: [
+            'https://haltu.github.io/muuri/'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.Muuri',
+                'window.Muuri.defaultOptions'
+            ]
+        ]
+    }
+}

5c8173726ce5daa0eabe3a9be2a4207ae99d8831
onassar <onassar@gmail.com>
2018-12-24 03:12:22 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -4544,6 +4544,27 @@
     }
 }
 ,
+// src/gitHub/insites/cookieconsent.js
+{
+    properties: {
+        link: 'https://github.com/insites/cookieconsent',
+        name: 'Pusher Javascript Client',
+        platform: 'GitHub',
+        tests: [
+            'https://cookieconsent.insites.com/app/themes/insites-cookie-consent/examples/example-1-themes.html'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.cookieconsent',
+                'window.cookieconsent.status'
+            ]
+        ]
+    }
+}
+,
 // src/gitHub/ionic-team/ionic.js
 {
     properties: {

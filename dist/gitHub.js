8d19147162e561eca9219faca44a1e5adef450e6
onassar <onassar@gmail.com>
2018-12-24 03:49:39 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -7456,6 +7456,34 @@
     }
 }
 ,
+// src/gitHub/qTip2/qTip2.js
+{
+    properties: {
+        donate: {
+            link: 'http://qtip2.com/donate'
+        },
+        link: 'https://github.com/qTip2/qTip2',
+        platform: 'GitHub',
+        tests: [
+            'http://qtip2.com/'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.fn.qtip',
+                'window.jQuery.fn.qtip.defaults'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.jQuery.fn.qtip.version'
+        ]
+    }
+}
+,
 // src/gitHub/quilljs/quill.js
 {
     properties: {
---
+++ src/gitHub/qTip2/qTip2.js
@@ -0,0 +1,26 @@
+{
+    properties: {
+        donate: {
+            link: 'http://qtip2.com/donate'
+        },
+        link: 'https://github.com/qTip2/qTip2',
+        platform: 'GitHub',
+        tests: [
+            'http://qtip2.com/'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.fn.qtip',
+                'window.jQuery.fn.qtip.defaults'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.jQuery.fn.qtip.version'
+        ]
+    }
+}

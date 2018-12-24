84ca31a7a6bf0eb7bc9b2e6296d30562bc9b06ed
onassar <onassar@gmail.com>
2018-12-23 23:32:39 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -2326,6 +2326,33 @@
     }
 }
 ,
+// src/gitHub/cubiq/iscroll.js
+{
+    properties: {
+        link: 'https://github.com/cubiq/iscroll',
+        notes: [],
+        platform: 'GitHub',
+        tests: [
+            'https://www.thewatergatehotel.com/offers/good-morning',
+            'http://iscrolljs.com/'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.IScroll',
+                'window.IScroll.utils'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.IScroll.version'
+        ]
+    }
+}
+,
 // src/gitHub/cujojs/curl.js
 {
     properties: {
---
+++ src/gitHub/cubiq/iscroll.js
@@ -0,0 +1,25 @@
+{
+    properties: {
+        link: 'https://github.com/cubiq/iscroll',
+        notes: [],
+        platform: 'GitHub',
+        tests: [
+            'https://www.thewatergatehotel.com/offers/good-morning',
+            'http://iscrolljs.com/'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.IScroll',
+                'window.IScroll.utils'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.IScroll.version'
+        ]
+    }
+}

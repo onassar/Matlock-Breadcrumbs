f635bd605006569d66d7dd07f46ba2930c34175b
onassar <onassar@gmail.com>
2018-12-23 22:42:30 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -4321,6 +4321,27 @@
     }
 }
 ,
+// src/gitHub/jasny/bootstrap.js
+{
+    properties: {
+        link: 'https://github.com/jasny/bootstrap',
+        notes: [],
+        platform: 'GitHub',
+        tests: [
+            'https://animals.howstuffworks.com/fish/sharks/shark.htm'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.fn.offcanvas',
+                'window.jQuery.fn.rowlink'
+            ]
+        ]
+    }
+}
+,
 // src/gitHub/jaywcjlove/hotkeys.js
 {
     properties: {
---
+++ src/gitHub/jasny/bootstrap.js
@@ -0,0 +1,19 @@
+{
+    properties: {
+        link: 'https://github.com/jasny/bootstrap',
+        notes: [],
+        platform: 'GitHub',
+        tests: [
+            'https://animals.howstuffworks.com/fish/sharks/shark.htm'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.fn.offcanvas',
+                'window.jQuery.fn.rowlink'
+            ]
+        ]
+    }
+}

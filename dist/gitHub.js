5af84b7c8a253dfd77bfc7830d7cc8b18d6ae126
onassar <onassar@gmail.com>
2018-12-23 23:46:59 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -1892,6 +1892,28 @@
     }
 }
 ,
+// src/gitHub/bryanbraun/anchorjs.js
+{
+    properties: {
+        link: 'https://github.com/bryanbraun/anchorjs',
+        notes: [],
+        platform: 'GitHub',
+        tests: [
+            'https://domvm.github.io/domvm/',
+            'https://www.bryanbraun.com/anchorjs/'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.AnchorJS',
+                'window.anchors.options'
+            ]
+        ]
+    }
+}
+,
 // src/gitHub/bugsnag/bugsnag-js.js
 {
     properties: {
---
+++ src/gitHub/bryanbraun/anchorjs.js
@@ -0,0 +1,20 @@
+{
+    properties: {
+        link: 'https://github.com/bryanbraun/anchorjs',
+        notes: [],
+        platform: 'GitHub',
+        tests: [
+            'https://domvm.github.io/domvm/',
+            'https://www.bryanbraun.com/anchorjs/'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.AnchorJS',
+                'window.anchors.options'
+            ]
+        ]
+    }
+}

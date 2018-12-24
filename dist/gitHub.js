c3141897e107dc55cfaa230f37b71fe2b2e09336
onassar <onassar@gmail.com>
2018-12-23 23:39:53 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -4478,6 +4478,35 @@
     }
 }
 ,
+// src/gitHub/jplayer/jPlayer.js
+{
+    properties: {
+        donate: {
+            link: 'http://jplayer.org/donate/'
+        },
+        link: 'https://github.com/jplayer/jPlayer',
+        notes: [],
+        platform: 'GitHub',
+        tests: [
+            'https://www.roblox.com/groups/group.aspx?gid=2788849'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.jPlayer',
+                'window.jQuery.jPlayer.browser'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.jQuery.jPlayer.prototype.version.script'
+        ]
+    }
+}
+,
 // src/gitHub/jquery-form/form.js
 {
     properties: {
---
+++ src/gitHub/jplayer/jPlayer.js
@@ -0,0 +1,27 @@
+{
+    properties: {
+        donate: {
+            link: 'http://jplayer.org/donate/'
+        },
+        link: 'https://github.com/jplayer/jPlayer',
+        notes: [],
+        platform: 'GitHub',
+        tests: [
+            'https://www.roblox.com/groups/group.aspx?gid=2788849'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.jPlayer',
+                'window.jQuery.jPlayer.browser'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.jQuery.jPlayer.prototype.version.script'
+        ]
+    }
+}

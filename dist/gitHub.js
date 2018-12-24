073cae1676d2e43bea5ff6898408ea778da8a6ca
onassar <onassar@gmail.com>
2018-12-24 02:24:19 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -518,6 +518,27 @@
     }
 }
 ,
+// src/gitHub/Pixabay/jQuery-autoComplete.js
+{
+    properties: {
+        link: 'https://github.com/Pixabay/jQuery-autoComplete',
+        notes: [],
+        platform: 'GitHub',
+        tests: [
+            'https://goodies.pixabay.com/jquery/auto-complete/demo.html'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.fn.autoComplete',
+                'window.jQuery.fn.autoComplete.defaults'
+            ]
+        ]
+    }
+}
+,
 // src/gitHub/Polymer/polymer.js
 {
     properties: {
---
+++ src/gitHub/Pixabay/jQuery-autoComplete.js
@@ -0,0 +1,19 @@
+{
+    properties: {
+        link: 'https://github.com/Pixabay/jQuery-autoComplete',
+        notes: [],
+        platform: 'GitHub',
+        tests: [
+            'https://goodies.pixabay.com/jquery/auto-complete/demo.html'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.fn.autoComplete',
+                'window.jQuery.fn.autoComplete.defaults'
+            ]
+        ]
+    }
+}

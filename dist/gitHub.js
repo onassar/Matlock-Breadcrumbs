50ec5dce611e4e755e9630be80ccfeea09eb91fa
onassar <onassar@gmail.com>
2018-12-24 04:09:15 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -2805,6 +2805,26 @@
     }
 }
 ,
+// src/gitHub/defunkt/jquery-pjax.js
+{
+    properties: {
+        link: 'https://github.com/defunkt/jquery-pjax',
+        platform: 'GitHub',
+        tests: [
+            'https://themeforest.net/item/soledad-multiconcept-blogmagazine-wp-theme/12945398'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.pjax',
+                'window.pjax.reload'
+            ]
+        ]
+    }
+}
+,
 // src/gitHub/desandro/imagesloaded.js
 {
     properties: {
---
+++ src/gitHub/defunkt/jquery-pjax.js
@@ -0,0 +1,18 @@
+{
+    properties: {
+        link: 'https://github.com/defunkt/jquery-pjax',
+        platform: 'GitHub',
+        tests: [
+            'https://themeforest.net/item/soledad-multiconcept-blogmagazine-wp-theme/12945398'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.pjax',
+                'window.pjax.reload'
+            ]
+        ]
+    }
+}

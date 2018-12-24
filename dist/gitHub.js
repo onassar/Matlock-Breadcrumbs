36f586fff382cc7a1605aef11c9301881d71958a
onassar <onassar@gmail.com>
2018-12-23 23:13:25 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -8372,6 +8372,23 @@
     }
 }
 ,
+// src/gitHub/zohararad/audio5js.js
+{
+    properties: {
+        link: 'https://github.com/zohararad/audio5js',
+        name: 'Audio5js',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.Audio5js'
+            ]
+        ]
+    }
+}
+,
 // src/gitHub/zurb/foundation-sites.js
 {
     properties: {
---
+++ src/gitHub/zohararad/audio5js.js
@@ -0,0 +1,15 @@
+{
+    properties: {
+        link: 'https://github.com/zohararad/audio5js',
+        name: 'Audio5js',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.Audio5js'
+            ]
+        ]
+    }
+}

6933cec35c8e5a082eb5a9058276fa0a39fc7332
onassar <onassar@gmail.com>
2018-12-24 00:16:52 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -4154,6 +4154,27 @@
     }
 }
 ,
+// src/gitHub/ilkeryilmaz/timelinejs.js
+{
+    properties: {
+        link: 'https://github.com/ilkeryilmaz/timelinejs',
+        platform: 'GitHub',
+        tests: [
+            'https://ilkeryilmaz.github.io/timelinejs/'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.Timeline',
+                'window.Timeline.init',
+                'window.Timeline.create_timeline'
+            ]
+        ]
+    }
+}
+,
 // src/gitHub/imakewebthings/jquery-waypoints.js
 {
     properties: {
---
+++ src/gitHub/ilkeryilmaz/timelinejs.js
@@ -0,0 +1,19 @@
+{
+    properties: {
+        link: 'https://github.com/ilkeryilmaz/timelinejs',
+        platform: 'GitHub',
+        tests: [
+            'https://ilkeryilmaz.github.io/timelinejs/'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.Timeline',
+                'window.Timeline.init',
+                'window.Timeline.create_timeline'
+            ]
+        ]
+    }
+}

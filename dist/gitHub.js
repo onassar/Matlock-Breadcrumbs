bfb1731acad9e401860aaea1c5a615447973ca18
onassar <onassar@gmail.com>
2018-12-24 03:12:35 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -4548,7 +4548,7 @@
 {
     properties: {
         link: 'https://github.com/insites/cookieconsent',
-        name: 'Pusher Javascript Client',
+        name: 'Cookie Consent',
         platform: 'GitHub',
         tests: [
             'https://cookieconsent.insites.com/app/themes/insites-cookie-consent/examples/example-1-themes.html'

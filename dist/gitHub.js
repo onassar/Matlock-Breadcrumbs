69a5435b738b6403a0ce75c56709a3b92208efb8
onassar <onassar@gmail.com>
2018-12-24 14:22:22 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -6567,6 +6567,34 @@
     }
 }
 ,
+// src/gitHub/mavoweb/mavo.js
+{
+    properties: {
+        link: 'https://github.com/mavoweb/mavo',
+        platform: 'GitHub',
+        tags: [
+            'search'
+        ],
+        tests: [
+            'https://mavo.io/'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.Mavo',
+                'window.Mavo.Actions'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.Mavo.version'
+        ]
+    }
+}
+,
 // src/gitHub/maxwellito/vivus.js
 {
     properties: {
---
+++ src/gitHub/mavoweb/mavo.js
@@ -0,0 +1,26 @@
+{
+    properties: {
+        link: 'https://github.com/mavoweb/mavo',
+        platform: 'GitHub',
+        tags: [
+            'search'
+        ],
+        tests: [
+            'https://mavo.io/'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.Mavo',
+                'window.Mavo.Actions'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.Mavo.version'
+        ]
+    }
+}

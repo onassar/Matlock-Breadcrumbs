b6b2a40bc814d2d6dbb4940023a47e9b26e429df
onassar <onassar@gmail.com>
2018-12-24 02:51:49 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -6616,6 +6616,28 @@
     }
 }
 ,
+// src/gitHub/nckprsn/scrollme.js
+{
+    properties: {
+        link: 'https://github.com/nckprsn/scrollme',
+        notes: [],
+        platform: 'GitHub',
+        tests: [
+            'http://scrollme.nckprsn.com/'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.scrollme',
+                'window.scrollme.init',
+                'window.scrollme.update_viewport'
+            ]
+        ]
+    }
+}
+,
 // src/gitHub/neilj/Squire.js
 {
     properties: {
---
+++ src/gitHub/nckprsn/scrollme.js
@@ -0,0 +1,20 @@
+{
+    properties: {
+        link: 'https://github.com/nckprsn/scrollme',
+        notes: [],
+        platform: 'GitHub',
+        tests: [
+            'http://scrollme.nckprsn.com/'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.scrollme',
+                'window.scrollme.init',
+                'window.scrollme.update_viewport'
+            ]
+        ]
+    }
+}

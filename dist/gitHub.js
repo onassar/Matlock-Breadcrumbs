38ccbb0b4b07bfc86a5a1e8d0713965e967438eb
onassar <onassar@gmail.com>
2018-12-24 00:25:24 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -7372,6 +7372,30 @@
     }
 }
 ,
+// src/gitHub/soundar24/roundSlider.js
+{
+    properties: {
+        link: 'https://github.com/soundar24/roundSlider',
+        platform: 'GitHub',
+        tests: [
+            'https://roundsliderui.com/'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.fn.roundSlider'
+            ]
+        ]
+    }
+    versions: {
+        references: [
+            'window.jQuery.fn.roundSlider.prototype.version'
+        ]
+    }
+}
+,
 // src/gitHub/sphinx-doc/sphinx.js
 {
     properties: {
---
+++ src/gitHub/soundar24/roundSlider.js
@@ -0,0 +1,22 @@
+{
+    properties: {
+        link: 'https://github.com/soundar24/roundSlider',
+        platform: 'GitHub',
+        tests: [
+            'https://roundsliderui.com/'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.fn.roundSlider'
+            ]
+        ]
+    }
+    versions: {
+        references: [
+            'window.jQuery.fn.roundSlider.prototype.version'
+        ]
+    }
+}

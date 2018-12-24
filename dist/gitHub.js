37c0b4ed3b561c299c0e092b5a83c29b20879ee8
onassar <onassar@gmail.com>
2018-12-24 01:16:57 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -1546,6 +1546,33 @@
     }
 }
 ,
+// src/gitHub/artpolikarpov/fotorama.js
+{
+    properties: {
+        link: 'https://github.com/artpolikarpov/fotorama',
+        platform: 'GitHub',
+        tests: [
+            'http://fotorama.io/'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.Fotorama'
+            ],
+            [
+                'window.jQuery.fn.fotorama'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.fotoramaVersion'
+        ]
+    }
+}
+,
 // src/gitHub/auth0/auth0.js.js
 {
     properties: {
---
+++ src/gitHub/artpolikarpov/fotorama.js
@@ -0,0 +1,25 @@
+{
+    properties: {
+        link: 'https://github.com/artpolikarpov/fotorama',
+        platform: 'GitHub',
+        tests: [
+            'http://fotorama.io/'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.Fotorama'
+            ],
+            [
+                'window.jQuery.fn.fotorama'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.fotoramaVersion'
+        ]
+    }
+}

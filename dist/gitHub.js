a5d459985a887d18fbd9457d0b4cb5df6934735d
onassar <onassar@gmail.com>
2018-12-24 04:16:01 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -4186,6 +4186,31 @@
     }
 }
 ,
+// src/gitHub/gregjacobs/Autolinker.js.js
+{
+    properties: {
+        link: 'https://github.com/gregjacobs/Autolinker.js',
+        platform: 'GitHub',
+        tests: [
+            'http://greg-jacobs.com/Autolinker.js/examples/live-example/'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.Autolinker',
+                'window.Autolinker.Util'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.Autolinker.version'
+        ]
+    }
+}
+,
 // src/gitHub/grevory/angular-local-storage.js
 {
     properties: {
---
+++ src/gitHub/gregjacobs/Autolinker.js.js
@@ -0,0 +1,23 @@
+{
+    properties: {
+        link: 'https://github.com/gregjacobs/Autolinker.js',
+        platform: 'GitHub',
+        tests: [
+            'http://greg-jacobs.com/Autolinker.js/examples/live-example/'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.Autolinker',
+                'window.Autolinker.Util'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.Autolinker.version'
+        ]
+    }
+}

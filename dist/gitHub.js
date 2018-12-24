8e0f94101fdacadc2752dceb14ee3efa4ef83e9e
onassar <onassar@gmail.com>
2018-12-24 04:51:13 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -264,6 +264,41 @@
     }
 }
 ,
+// src/gitHub/FrDH/jQuery.mmenu.js
+{
+    properties: {
+        licenses: {
+            commercial: {
+                currency: 'USD',
+                paid: true,
+                range: [
+                    10,
+                    75
+                ]
+            }
+        },
+        link: 'https://github.com/FrDH/jQuery.mmenu',
+        platform: 'GitHub',
+        tests: [
+            'https://mmenu.frebsite.nl/'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.mmenu',
+                'window.jQuery.mmenu.defaults'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.jQuery.mmenu.version'
+        ]
+    }
+}
+,
 // src/gitHub/GetmeUK/ContentTools.js
 {
     properties: {
---
+++ src/gitHub/FrDH/jQuery.mmenu.js
@@ -0,0 +1,33 @@
+{
+    properties: {
+        licenses: {
+            commercial: {
+                currency: 'USD',
+                paid: true,
+                range: [
+                    10,
+                    75
+                ]
+            }
+        },
+        link: 'https://github.com/FrDH/jQuery.mmenu',
+        platform: 'GitHub',
+        tests: [
+            'https://mmenu.frebsite.nl/'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.mmenu',
+                'window.jQuery.mmenu.defaults'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.jQuery.mmenu.version'
+        ]
+    }
+}

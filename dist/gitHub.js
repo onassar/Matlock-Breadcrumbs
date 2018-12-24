24ce4af1b28162dd59b534c8703c43ff85108d20
onassar <onassar@gmail.com>
2018-12-24 00:42:33 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -7188,6 +7188,26 @@
     }
 }
 ,
+// src/gitHub/sachinchoolur/lightslider.js
+{
+    properties: {
+        link: 'https://github.com/sachinchoolur/lightslider',
+        notes: [],
+        platform: 'GitHub',
+        tests: [
+            'http://sachinchoolur.github.io/lightslider/'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.fn.lightSlider'
+            ]
+        ]
+    }
+}
+,
 // src/gitHub/scaron/prettyphoto.js
 {
     properties: {
---
+++ src/gitHub/sachinchoolur/lightslider.js
@@ -0,0 +1,18 @@
+{
+    properties: {
+        link: 'https://github.com/sachinchoolur/lightslider',
+        notes: [],
+        platform: 'GitHub',
+        tests: [
+            'http://sachinchoolur.github.io/lightslider/'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.fn.lightSlider'
+            ]
+        ]
+    }
+}

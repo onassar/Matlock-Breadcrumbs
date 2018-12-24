0afe3954c646fda81a0a147218a5a9c374b15669
onassar <onassar@gmail.com>
2018-12-24 04:42:47 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -7096,6 +7096,25 @@
     }
 }
 ,
+// src/gitHub/nnattawat/slideReveal.js
+{
+    properties: {
+        link: 'https://github.com/nnattawat/slideReveal',
+        platform: 'GitHub',
+        tests: [
+            'http://nnattawat.github.io/slideReveal/'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.fn.slideReveal'
+            ]
+        ]
+    }
+}
+,
 // src/gitHub/noelboss/featherlight.js
 {
     properties: {
---
+++ src/gitHub/nnattawat/slideReveal.js
@@ -0,0 +1,17 @@
+{
+    properties: {
+        link: 'https://github.com/nnattawat/slideReveal',
+        platform: 'GitHub',
+        tests: [
+            'http://nnattawat.github.io/slideReveal/'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.fn.slideReveal'
+            ]
+        ]
+    }
+}

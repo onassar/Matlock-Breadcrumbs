ad421ee45e8c670c2ddf9b3a3b0f99aa881200b6
onassar <onassar@gmail.com>
2018-12-25 03:44:49 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -2288,6 +2288,29 @@
     }
 }
 ,
+// src/gitHub/bevacqua/horsey.js
+{
+    properties: {
+        link: 'https://github.com/bevacqua/horsey',
+        notes: [],
+        platform: 'GitHub',
+        tags: [
+            'date-picker'
+        ],
+        tests: [
+            'https://bevacqua.github.io/horsey/'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.horsey'
+            ]
+        ]
+    }
+}
+,
 // src/gitHub/bevacqua/rome.js
 {
     properties: {
---
+++ src/gitHub/bevacqua/horsey.js
@@ -0,0 +1,21 @@
+{
+    properties: {
+        link: 'https://github.com/bevacqua/horsey',
+        notes: [],
+        platform: 'GitHub',
+        tags: [
+            'date-picker'
+        ],
+        tests: [
+            'https://bevacqua.github.io/horsey/'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.horsey'
+            ]
+        ]
+    }
+}

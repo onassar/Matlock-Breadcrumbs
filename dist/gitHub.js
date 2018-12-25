d31e3d50cacbeedf3655b5bb3baa4bedad86ab03
onassar <onassar@gmail.com>
2018-12-25 15:24:42 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -9796,6 +9796,27 @@
     }
 }
 ,
+// src/gitHub/thorst/jquery-idletimer.js
+{
+    properties: {
+        link: 'https://github.com/thorst/jquery-idletimer',
+        notes: [],
+        platform: 'GitHub',
+        tags: [],
+        tests: [
+            'http://thorst.github.io/jquery-idletimer/prod/demos/'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.fn.idleTimer'
+            ]
+        ]
+    }
+}
+,
 // src/gitHub/timdown/rangy.js
 {
     properties: {
---
+++ src/gitHub/thorst/jquery-idletimer.js
@@ -0,0 +1,19 @@
+{
+    properties: {
+        link: 'https://github.com/thorst/jquery-idletimer',
+        notes: [],
+        platform: 'GitHub',
+        tags: [],
+        tests: [
+            'http://thorst.github.io/jquery-idletimer/prod/demos/'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.fn.idleTimer'
+            ]
+        ]
+    }
+}

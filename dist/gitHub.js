172ec3dc307673e962583137263de88e2963f620
onassar <onassar@gmail.com>
2018-12-25 01:52:18 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -4610,6 +4610,26 @@
     }
 }
 ,
+// src/gitHub/hakimel/kontext.js
+{
+    properties: {
+        link: 'https://github.com/hakimel/kontext',
+        platform: 'GitHub',
+        tests: [
+            'https://lab.hakim.se/kontext/'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.kontext',
+                'window.kontext.Signal'
+            ]
+        ]
+    }
+}
+,
 // src/gitHub/hakimel/reveal.js.js
 {
     properties: {
---
+++ src/gitHub/hakimel/kontext.js
@@ -0,0 +1,18 @@
+{
+    properties: {
+        link: 'https://github.com/hakimel/kontext',
+        platform: 'GitHub',
+        tests: [
+            'https://lab.hakim.se/kontext/'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.kontext',
+                'window.kontext.Signal'
+            ]
+        ]
+    }
+}

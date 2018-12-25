2f54f0e372d2c879e95eaf82e83f56e3caf6659c
onassar <onassar@gmail.com>
2018-12-25 03:30:36 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -5466,6 +5466,30 @@
     }
 }
 ,
+// src/gitHub/infusion/Fraction.js
+{
+    properties: {
+        link: 'https://github.com/infusion/Fraction.js',
+        notes: [],
+        platform: 'GitHub',
+        tags: [
+            'math'
+        ],
+        tests: [
+            'https://www.xarg.org/2014/03/rational-numbers-in-javascript/'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.Fraction',
+                'window.Fraction.DivisionByZero'
+            ]
+        ]
+    }
+}
+,
 // src/gitHub/infusion/jQuery-xcolor.js
 {
     properties: {
---
+++ src/gitHub/infusion/Fraction.js
@@ -0,0 +1,22 @@
+{
+    properties: {
+        link: 'https://github.com/infusion/Fraction.js',
+        notes: [],
+        platform: 'GitHub',
+        tags: [
+            'math'
+        ],
+        tests: [
+            'https://www.xarg.org/2014/03/rational-numbers-in-javascript/'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.Fraction',
+                'window.Fraction.DivisionByZero'
+            ]
+        ]
+    }
+}

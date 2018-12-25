a199daf7ff65078186d35205e6a480c632c3b3dc
onassar <onassar@gmail.com>
2018-12-25 03:47:29 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -10211,6 +10211,33 @@
     }
 }
 ,
+// src/gitHub/vuejs/vuepress.js
+{
+    properties: {
+        donate: {
+            link: 'https://vuejs.org/support-vuejs/'
+        },
+        link: 'https://github.com/vuejs/vuepress',
+        platform: 'GitHub',
+        tests: [
+            'https://vuepress.vuejs.org/'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.__VUEPRESS_VERSION__'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.__VUEPRESS_VERSION__.version'
+        ]
+    }
+}
+,
 // src/gitHub/w3co/jcf.js
 {
     properties: {
---
+++ src/gitHub/vuejs/vuepress.js
@@ -0,0 +1,25 @@
+{
+    properties: {
+        donate: {
+            link: 'https://vuejs.org/support-vuejs/'
+        },
+        link: 'https://github.com/vuejs/vuepress',
+        platform: 'GitHub',
+        tests: [
+            'https://vuepress.vuejs.org/'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.__VUEPRESS_VERSION__'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.__VUEPRESS_VERSION__.version'
+        ]
+    }
+}

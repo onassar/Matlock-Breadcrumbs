e3b6e0b1e848a205ab1b41a9e19e916a096ec91c
onassar <onassar@gmail.com>
2018-12-25 01:31:28 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -5336,6 +5336,29 @@
     }
 }
 ,
+// src/gitHub/jasonlong/cayman-theme.js
+{
+    properties: {
+        link: 'https://github.com/jasonlong/cayman-theme',
+        notes: [],
+        platform: 'GitHub',
+        tests: [
+            'http://auxiliary.github.io/rater/'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        strings: {
+            page: [
+                [
+                    'Cayman theme',
+                    'Jason Long'
+                ]
+            ]
+        }
+    }
+}
+,
 // src/gitHub/jaysalvat/buzz.js
 {
     properties: {
---
+++ src/gitHub/jasonlong/cayman-theme.js
@@ -0,0 +1,21 @@
+{
+    properties: {
+        link: 'https://github.com/jasonlong/cayman-theme',
+        notes: [],
+        platform: 'GitHub',
+        tests: [
+            'http://auxiliary.github.io/rater/'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        strings: {
+            page: [
+                [
+                    'Cayman theme',
+                    'Jason Long'
+                ]
+            ]
+        }
+    }
+}

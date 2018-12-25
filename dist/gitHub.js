08bf2a906475326c747636410ddb56f4a6027b22
onassar <onassar@gmail.com>
2018-12-25 03:23:08 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -5442,6 +5442,30 @@
     }
 }
 ,
+// src/gitHub/infusion/jQuery-xcolor.js
+{
+    properties: {
+        link: 'https://github.com/infusion/jQuery-xcolor',
+        notes: [],
+        platform: 'GitHub',
+        tags: [
+            'color'
+        ],
+        tests: [
+            'https://www.xarg.org/project/jquery-color-plugin-xcolor/'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.xcolor',
+                'window.jQuery.fn.colorize'
+            ]
+        ]
+    }
+}
+,
 // src/gitHub/insites/cookieconsent.js
 {
     properties: {
---
+++ src/gitHub/infusion/jQuery-xcolor.js
@@ -0,0 +1,22 @@
+{
+    properties: {
+        link: 'https://github.com/infusion/jQuery-xcolor',
+        notes: [],
+        platform: 'GitHub',
+        tags: [
+            'color'
+        ],
+        tests: [
+            'https://www.xarg.org/project/jquery-color-plugin-xcolor/'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.xcolor',
+                'window.jQuery.fn.colorize'
+            ]
+        ]
+    }
+}

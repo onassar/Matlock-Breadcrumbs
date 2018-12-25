832a62b7de092999ce52a7a4b0c1390bbbb2295b
onassar <onassar@gmail.com>
2018-12-25 03:25:41 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -2281,6 +2281,30 @@
     }
 }
 ,
+// src/gitHub/bgrins/TinyColor.js
+{
+    properties: {
+        link: 'https://github.com/bgrins/TinyColor',
+        notes: [],
+        platform: 'GitHub',
+        tags: [
+            'color'
+        ],
+        tests: [
+            'https://bgrins.github.io/spectrum/'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.tinycolor',
+                'window.tinycolor.equals'
+            ]
+        ]
+    }
+}
+,
 // src/gitHub/bgrins/spectrum.js
 {
     properties: {
---
+++ src/gitHub/bgrins/TinyColor.js
@@ -0,0 +1,22 @@
+{
+    properties: {
+        link: 'https://github.com/bgrins/TinyColor',
+        notes: [],
+        platform: 'GitHub',
+        tags: [
+            'color'
+        ],
+        tests: [
+            'https://bgrins.github.io/spectrum/'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.tinycolor',
+                'window.tinycolor.equals'
+            ]
+        ]
+    }
+}

c92ba900edb4b261770d51314aecddb242ceddfb
onassar <onassar@gmail.com>
2018-12-25 15:42:46 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -10762,6 +10762,28 @@
     }
 }
 ,
+// src/gitHub/wilq32/jqueryrotate.js
+{
+    properties: {
+        link: 'https://github.com/wilq32/jqueryrotate',
+        notes: [],
+        platform: 'GitHub',
+        tags: [],
+        tests: [
+            'http://jqueryrotate.com/'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.fn.rotate',
+                'window.jQuery.fn.getRotateAngle'
+            ]
+        ]
+    }
+}
+,
 // src/gitHub/woocommerce/FlexSlider.js
 {
     properties: {
---
+++ src/gitHub/wilq32/jqueryrotate.js
@@ -0,0 +1,20 @@
+{
+    properties: {
+        link: 'https://github.com/wilq32/jqueryrotate',
+        notes: [],
+        platform: 'GitHub',
+        tags: [],
+        tests: [
+            'http://jqueryrotate.com/'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.fn.rotate',
+                'window.jQuery.fn.getRotateAngle'
+            ]
+        ]
+    }
+}

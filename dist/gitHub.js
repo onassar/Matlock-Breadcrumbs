e888e4c7456b94869b0c94d521a1381e8af437c4
onassar <onassar@gmail.com>
2018-12-25 15:59:01 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -8171,6 +8171,31 @@
     }
 }
 ,
+// src/gitHub/naptha/tesseract.js.js
+{
+    properties: {
+        link: 'https://github.com/naptha/tesseract.js',
+        notes: [],
+        platform: 'GitHub',
+        tests: [
+            'http://tesseract.projectnaptha.com/'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.Tesseract'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.Tesseract.version'
+        ]
+    }
+}
+,
 // src/gitHub/nashio/star-rating-svg.js
 {
     properties: {
---
+++ src/gitHub/naptha/tesseract.js.js
@@ -0,0 +1,23 @@
+{
+    properties: {
+        link: 'https://github.com/naptha/tesseract.js',
+        notes: [],
+        platform: 'GitHub',
+        tests: [
+            'http://tesseract.projectnaptha.com/'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.Tesseract'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.Tesseract.version'
+        ]
+    }
+}

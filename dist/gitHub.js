2a102bee80690303254ab4aa1d82e27acd5d7eb5
onassar <onassar@gmail.com>
2018-12-25 17:16:09 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -6096,6 +6096,31 @@
     }
 }
 ,
+// src/gitHub/jacomyal/conrad.js.js
+{
+    properties: {
+        link: 'https://github.com/jacomyal/conrad.js',
+        platform: 'GitHub',
+        tests: [
+            'http://sigmajs.org/'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.conrad',
+                'window.conrad.addJob'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.conrad.version'
+        ]
+    }
+}
+,
 // src/gitHub/jacomyal/sigma.js.js
 {
     properties: {
---
+++ src/gitHub/jacomyal/conrad.js.js
@@ -0,0 +1,23 @@
+{
+    properties: {
+        link: 'https://github.com/jacomyal/conrad.js',
+        platform: 'GitHub',
+        tests: [
+            'http://sigmajs.org/'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.conrad',
+                'window.conrad.addJob'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.conrad.version'
+        ]
+    }
+}

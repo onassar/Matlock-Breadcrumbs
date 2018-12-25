be53e4dc175ab67a9724a12cc251e087533f35cd
onassar <onassar@gmail.com>
2018-12-25 01:17:56 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -7819,6 +7819,26 @@
     }
 }
 ,
+// src/gitHub/prrashi/rateYo.js
+{
+    properties: {
+        link: 'https://github.com/prrashi/rateYo',
+        notes: [],
+        platform: 'GitHub',
+        tests: [
+            'http://rateyo.fundoocode.ninja/'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.fn.rateYo'
+            ]
+        ]
+    }
+}
+,
 // src/gitHub/pure-css/pure.js
 {
     properties: {
---
+++ src/gitHub/prrashi/rateYo.js
@@ -0,0 +1,18 @@
+{
+    properties: {
+        link: 'https://github.com/prrashi/rateYo',
+        notes: [],
+        platform: 'GitHub',
+        tests: [
+            'http://rateyo.fundoocode.ninja/'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.fn.rateYo'
+            ]
+        ]
+    }
+}

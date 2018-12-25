683d2440b5572710183fb3162f153f6780e58770
onassar <onassar@gmail.com>
2018-12-25 01:49:49 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -4634,6 +4634,27 @@
     }
 }
 ,
+// src/gitHub/hakimel/stroll.js.js
+{
+    properties: {
+        link: 'https://github.com/hakimel/stroll.js',
+        platform: 'GitHub',
+        tests: [
+            'https://lab.hakim.se/scroll-effects/'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.stroll',
+                'window.stroll.bind',
+                'window.stroll.unbind'
+            ]
+        ]
+    }
+}
+,
 // src/gitHub/haltu/muuri.js
 {
     properties: {
---
+++ src/gitHub/hakimel/stroll.js.js
@@ -0,0 +1,19 @@
+{
+    properties: {
+        link: 'https://github.com/hakimel/stroll.js',
+        platform: 'GitHub',
+        tests: [
+            'https://lab.hakim.se/scroll-effects/'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.stroll',
+                'window.stroll.bind',
+                'window.stroll.unbind'
+            ]
+        ]
+    }
+}

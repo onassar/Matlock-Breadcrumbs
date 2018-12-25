f4bec4ce5043c2a905654b85e0a8d01173a6b62d
onassar <onassar@gmail.com>
2018-12-25 03:06:53 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -2789,6 +2789,26 @@
     }
 }
 ,
+// src/gitHub/claviska/jquery-minicolors.js
+{
+    properties: {
+        link: 'https://github.com/claviska/jquery-minicolors',
+        notes: [],
+        platform: 'GitHub',
+        tests: [
+            'http://labs.abeautifulsite.net/jquery-minicolors/'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.fn.minicolors'
+            ]
+        ]
+    }
+}
+,
 // src/gitHub/cloudinary/cloudinary_js.js
 {
     properties: {
---
+++ src/gitHub/claviska/jquery-minicolors.js
@@ -0,0 +1,18 @@
+{
+    properties: {
+        link: 'https://github.com/claviska/jquery-minicolors',
+        notes: [],
+        platform: 'GitHub',
+        tests: [
+            'http://labs.abeautifulsite.net/jquery-minicolors/'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.fn.minicolors'
+            ]
+        ]
+    }
+}

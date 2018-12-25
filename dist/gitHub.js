8692f8364f8cea5781cc04bef0f0d8daf53e4558
onassar <onassar@gmail.com>
2018-12-25 17:31:19 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -886,6 +886,31 @@
     }
 }
 ,
+// src/gitHub/SheetJS/js-xlsx.js
+{
+    properties: {
+        link: 'https://github.com/SheetJS/js-xlsx',
+        platform: 'GitHub',
+        tests: [
+            'https://oss.sheetjs.com/'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.XLSX',
+                'window.XLSX.read'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.XLSX.version'
+        ]
+    }
+}
+,
 // src/gitHub/Sjeiti/TinySort.js
 {
     properties: {
---
+++ src/gitHub/SheetJS/js-xlsx.js
@@ -0,0 +1,23 @@
+{
+    properties: {
+        link: 'https://github.com/SheetJS/js-xlsx',
+        platform: 'GitHub',
+        tests: [
+            'https://oss.sheetjs.com/'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.XLSX',
+                'window.XLSX.read'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.XLSX.version'
+        ]
+    }
+}

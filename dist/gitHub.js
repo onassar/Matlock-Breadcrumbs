f4a38027f429fc74d5e3e020d56eac3092e77820
onassar <onassar@gmail.com>
2018-12-25 14:48:06 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -9061,6 +9061,35 @@
     }
 }
 ,
+// src/gitHub/scrollreveal/scrollreveal.js
+{
+    properties: {
+        link: 'https://github.com/scrollreveal/scrollreveal',
+        notes: [],
+        platform: 'GitHub',
+        tags: [
+            'scrolling'
+        ],
+        tests: [
+            'https://scrollrevealjs.org/'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.ScrollReveal',
+                'window.ScrollReveal.isSupported'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.ScrollReveal.fn.jquery'
+        ]
+    }
+}
+,
 // src/gitHub/segmentio/analytics.js.js
 {
     properties: {
---
+++ src/gitHub/scrollreveal/scrollreveal.js
@@ -0,0 +1,27 @@
+{
+    properties: {
+        link: 'https://github.com/scrollreveal/scrollreveal',
+        notes: [],
+        platform: 'GitHub',
+        tags: [
+            'scrolling'
+        ],
+        tests: [
+            'https://scrollrevealjs.org/'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.ScrollReveal',
+                'window.ScrollReveal.isSupported'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.ScrollReveal.fn.jquery'
+        ]
+    }
+}

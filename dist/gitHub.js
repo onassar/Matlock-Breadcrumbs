62794dbd4827646a3d3aa456c09c6ed8ed84d35c
onassar <onassar@gmail.com>
2018-12-25 01:45:44 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -4570,6 +4570,26 @@
     }
 }
 ,
+// src/gitHub/hakimel/Ladda.js
+{
+    properties: {
+        link: 'https://github.com/hakimel/Ladda',
+        platform: 'GitHub',
+        tests: [
+            'https://lab.hakim.se/ladda/'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.Ladda',
+                'window.Ladda.create'
+            ]
+        ]
+    }
+}
+,
 // src/gitHub/hakimel/reveal.js.js
 {
     properties: {
---
+++ src/gitHub/hakimel/Ladda.js
@@ -0,0 +1,18 @@
+{
+    properties: {
+        link: 'https://github.com/hakimel/Ladda',
+        platform: 'GitHub',
+        tests: [
+            'https://lab.hakim.se/ladda/'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.Ladda',
+                'window.Ladda.create'
+            ]
+        ]
+    }
+}

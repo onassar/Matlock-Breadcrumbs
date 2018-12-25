cd55382270f487d766718f002711e5d8fbbe226c
onassar <onassar@gmail.com>
2018-12-25 15:50:01 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -9248,6 +9248,29 @@
     }
 }
 ,
+// src/gitHub/sampotts/shr.js
+{
+    properties: {
+        link: 'https://github.com/sampotts/shr',
+        platform: 'GitHub',
+        tags: [
+            'social-media-links'
+        ],
+        tests: [
+            'https://shr.one/'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.shr',
+                'window.shr.setup'
+            ]
+        ]
+    }
+}
+,
 // src/gitHub/scaron/prettyphoto.js
 {
     properties: {
---
+++ src/gitHub/sampotts/shr.js
@@ -0,0 +1,21 @@
+{
+    properties: {
+        link: 'https://github.com/sampotts/shr',
+        platform: 'GitHub',
+        tags: [
+            'social-media-links'
+        ],
+        tests: [
+            'https://shr.one/'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.shr',
+                'window.shr.setup'
+            ]
+        ]
+    }
+}

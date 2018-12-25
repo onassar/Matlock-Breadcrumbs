40a8b2c79082d27e9f220df84553d0b784fe7765
onassar <onassar@gmail.com>
2018-12-25 02:13:49 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -363,6 +363,31 @@
     }
 }
 ,
+// src/gitHub/KaTeX/KaTeX.js
+{
+    properties: {
+        link: 'https://github.com/KaTeX/KaTeX',
+        platform: 'GitHub',
+        tests: [
+            'https://slides.com/'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.katex',
+                'window.katex.render'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.katex.version'
+        ]
+    }
+}
+,
 // src/gitHub/LeaVerou/awesomplete.js
 {
     properties: {
---
+++ src/gitHub/KaTeX/KaTeX.js
@@ -0,0 +1,23 @@
+{
+    properties: {
+        link: 'https://github.com/KaTeX/KaTeX',
+        platform: 'GitHub',
+        tests: [
+            'https://slides.com/'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.katex',
+                'window.katex.render'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.katex.version'
+        ]
+    }
+}

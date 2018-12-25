2a84333eb5fe9c6ef26eddf1b314e4ca0ece2f90
onassar <onassar@gmail.com>
2018-12-25 03:03:12 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -4389,6 +4389,33 @@
     }
 }
 ,
+// src/gitHub/gionkunz/chartist-js.js
+{
+    properties: {
+        link: 'https://github.com/gionkunz/chartist-js',
+        platform: 'GitHub',
+        tags: [
+            'charts'
+        ],
+        tests: [
+            'http://gionkunz.github.io/chartist-js/'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.Chartist'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.Chartist.version'
+        ]
+    }
+}
+,
 // src/gitHub/gjunge/rateit.js.js
 {
     properties: {
---
+++ src/gitHub/gionkunz/chartist-js.js
@@ -0,0 +1,25 @@
+{
+    properties: {
+        link: 'https://github.com/gionkunz/chartist-js',
+        platform: 'GitHub',
+        tags: [
+            'charts'
+        ],
+        tests: [
+            'http://gionkunz.github.io/chartist-js/'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.Chartist'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.Chartist.version'
+        ]
+    }
+}

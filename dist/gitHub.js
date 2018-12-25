7720369f42037cc50f648ffc96f3d244822d506c
onassar <onassar@gmail.com>
2018-12-25 17:25:00 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -8845,6 +8845,26 @@
     }
 }
 ,
+// src/gitHub/nosir/obelisk.js.js
+{
+    properties: {
+        link: 'https://github.com/nosir/obelisk.js',
+        platform: 'GitHub',
+        tests: [
+            'https://nosir.github.io/cleave.js/'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.obelisk',
+                'window.obelisk.version'
+            ]
+        ]
+    }
+}
+,
 // src/gitHub/novus/nvd3.js
 {
     properties: {
---
+++ src/gitHub/nosir/obelisk.js.js
@@ -0,0 +1,18 @@
+{
+    properties: {
+        link: 'https://github.com/nosir/obelisk.js',
+        platform: 'GitHub',
+        tests: [
+            'https://nosir.github.io/cleave.js/'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.obelisk',
+                'window.obelisk.version'
+            ]
+        ]
+    }
+}

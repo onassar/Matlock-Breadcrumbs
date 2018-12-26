d7b005fc82ae0733c299f63368166493507e3005
onassar <onassar@gmail.com>
2018-12-25 23:35:21 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -6084,6 +6084,35 @@
     }
 }
 ,
+// src/gitHub/imgix/imgix.js.js
+{
+    properties: {
+        link: 'https://github.com/imgix/imgix.js',
+        notes: [],
+        platform: 'GitHub',
+        examples: [
+            'https://www.imgix.com/'
+        ],
+        tags: [
+            'lightbox'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.imgix',
+                'window.imgix.config'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.imgix.VERSION'
+        ]
+    }
+}
+,
 // src/gitHub/impress/impress.js
 {
     properties: {
---
+++ src/gitHub/imgix/imgix.js.js
@@ -0,0 +1,27 @@
+{
+    properties: {
+        link: 'https://github.com/imgix/imgix.js',
+        notes: [],
+        platform: 'GitHub',
+        examples: [
+            'https://www.imgix.com/'
+        ],
+        tags: [
+            'lightbox'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.imgix',
+                'window.imgix.config'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.imgix.VERSION'
+        ]
+    }
+}

e57f3f771724c515fcb74dbc674b6a89e172e02d
onassar <onassar@gmail.com>
2018-12-26 02:39:31 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -4364,6 +4364,28 @@
     }
 }
 ,
+// src/gitHub/emn178/js-sha256.js
+{
+    properties: {
+        examples: [
+            'http://emn178.github.io/online-tools/sha256.html'
+        ],
+        link: 'https://github.com/emn178/js-sha256',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.sha224',
+                'window.sha224.create',
+                'window.sha256',
+                'window.sha256.create'
+            ]
+        ]
+    }
+}
+,
 // src/gitHub/emojione/emojione.js
 {
     properties: {
---
+++ src/gitHub/emn178/js-sha256.js
@@ -0,0 +1,20 @@
+{
+    properties: {
+        examples: [
+            'http://emn178.github.io/online-tools/sha256.html'
+        ],
+        link: 'https://github.com/emn178/js-sha256',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.sha224',
+                'window.sha224.create',
+                'window.sha256',
+                'window.sha256.create'
+            ]
+        ]
+    }
+}

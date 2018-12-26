5a08e3c79092e5e3068e419c03c17718fbc4500e
onassar <onassar@gmail.com>
2018-12-26 04:05:45 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -11167,6 +11167,35 @@
     }
 }
 ,
+// src/gitHub/tabalinas/jsgrid.js
+{
+    properties: {
+        examples: [
+            'http://js-grid.com/'
+        ],
+        link: 'https://github.com/tabalinas/jsgrid',
+        name: 'jsGrid',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jsGrid',
+                'window.jsGrid.setDefaults'
+            ],
+            [
+                'window.jQuery.fn.jsGrid'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.jsGrid.version'
+        ]
+    }
+}
+,
 // src/gitHub/tabalinas/jssocials.js
 {
     properties: {
---
+++ src/gitHub/tabalinas/jsgrid.js
@@ -0,0 +1,27 @@
+{
+    properties: {
+        examples: [
+            'http://js-grid.com/'
+        ],
+        link: 'https://github.com/tabalinas/jsgrid',
+        name: 'jsGrid',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jsGrid',
+                'window.jsGrid.setDefaults'
+            ],
+            [
+                'window.jQuery.fn.jsGrid'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.jsGrid.version'
+        ]
+    }
+}

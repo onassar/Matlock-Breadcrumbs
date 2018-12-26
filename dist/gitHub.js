3f833936b0906b5050216742cd452c911449fb60
onassar <onassar@gmail.com>
2018-12-26 02:36:37 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -8239,6 +8239,28 @@
     }
 }
 ,
+// src/gitHub/marklagendijk/jquery.tabbable.js
+{
+    properties: {
+        examples: [
+            'https://www.kickstarter.com/'
+        ],
+        link: 'https://github.com/marklagendijk/jquery.tabbable',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.focusNext',
+                'window.jQuery.focusPrev',
+                'window.jQuery.tabNext',
+                'window.jQuery.tabPrev'
+            ]
+        ]
+    }
+}
+,
 // src/gitHub/marmelab/gremlins.js.js
 {
     properties: {
---
+++ src/gitHub/marklagendijk/jquery.tabbable.js
@@ -0,0 +1,20 @@
+{
+    properties: {
+        examples: [
+            'https://www.kickstarter.com/'
+        ],
+        link: 'https://github.com/marklagendijk/jquery.tabbable',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.focusNext',
+                'window.jQuery.focusPrev',
+                'window.jQuery.tabNext',
+                'window.jQuery.tabPrev'
+            ]
+        ]
+    }
+}

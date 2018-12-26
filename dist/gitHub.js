dbb88c168af9b709d8279b28b1aff29558be1956
onassar <onassar@gmail.com>
2018-12-26 01:45:36 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -9285,6 +9285,24 @@
     }
 }
 ,
+// src/gitHub/notwaldorf/emoji-translate.js
+{
+    properties: {
+        link: 'https://github.com/notwaldorf/emoji-translate',
+        platform: 'GitHub',
+        examples: [
+            'https://meowni.ca/emoji-translate/'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.EmojiTranslate'
+            ]
+    }
+}
+,
 // src/gitHub/novus/nvd3.js
 {
     properties: {
---
+++ src/gitHub/notwaldorf/emoji-translate.js
@@ -0,0 +1,16 @@
+{
+    properties: {
+        link: 'https://github.com/notwaldorf/emoji-translate',
+        platform: 'GitHub',
+        examples: [
+            'https://meowni.ca/emoji-translate/'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.EmojiTranslate'
+            ]
+    }
+}

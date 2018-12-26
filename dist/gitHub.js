e5c94b6da8dab8a5685483c67d9355f797afc29f
onassar <onassar@gmail.com>
2018-12-26 03:14:09 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -4789,6 +4789,30 @@
     }
 }
 ,
+// src/gitHub/filamentgroup/tablesaw.js
+{
+    properties: {
+        examples: [
+            'http://www.coca-cola.com/global/'
+        ],
+        link: 'https://github.com/filamentgroup/tablesaw',
+        name: 'Tablesaw',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.Tablesaw',
+                'window.Tablesaw.init'
+            ],
+            [
+                'window.jQuery.fn.tablesawbtn'
+            ]
+        ]
+    }
+}
+,
 // src/gitHub/filepicker/filepicker-js.js
 {
     properties: {
---
+++ src/gitHub/filamentgroup/tablesaw.js
@@ -0,0 +1,22 @@
+{
+    properties: {
+        examples: [
+            'http://www.coca-cola.com/global/'
+        ],
+        link: 'https://github.com/filamentgroup/tablesaw',
+        name: 'Tablesaw',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.Tablesaw',
+                'window.Tablesaw.init'
+            ],
+            [
+                'window.jQuery.fn.tablesawbtn'
+            ]
+        ]
+    }
+}

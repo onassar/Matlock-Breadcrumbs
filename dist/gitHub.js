2cbf7fb6a1f107474e666224fee8e5ced1bd07c3
onassar <onassar@gmail.com>
2018-12-26 01:40:42 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -11851,6 +11851,68 @@
     }
 }
 ,
+// src/gitHub/winmarkltd/BootstrapFormHelpers.js
+{
+    properties: {
+        link: 'https://github.com/winmarkltd/BootstrapFormHelpers',
+        platform: 'GitHub',
+        examples: [
+            'https://www.namecheap.com/',
+            'http://bootstrapformhelpers.com/colorpicker/'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.fn.bfhcolorpicker'
+            ],
+            [
+                'window.jQuery.fn.bfhcountries'
+            ],
+            [
+                'window.jQuery.fn.bfhcurrencies'
+            ],
+            [
+                'window.jQuery.fn.bfhdatepicker'
+            ],
+            [
+                'window.jQuery.fn.bfhfonts'
+            ],
+            [
+                'window.jQuery.fn.bfhfontsizes'
+            ],
+            [
+                'window.jQuery.fn.bfhgooglefonts'
+            ],
+            [
+                'window.jQuery.fn.bfhlanguages'
+            ],
+            [
+                'window.jQuery.fn.bfhnumber'
+            ],
+            [
+                'window.jQuery.fn.bfhphone'
+            ],
+            [
+                'window.jQuery.fn.bfhselectbox'
+            ],
+            [
+                'window.jQuery.fn.bfhslider'
+            ],
+            [
+                'window.jQuery.fn.bfhstates'
+            ],
+            [
+                'window.jQuery.fn.bfhtimepicker'
+            ],
+            [
+                'window.jQuery.fn.bfhtimezones'
+            ]
+        ]
+    }
+}
+,
 // src/gitHub/woocommerce/FlexSlider.js
 {
     properties: {
---
+++ src/gitHub/winmarkltd/BootstrapFormHelpers.js
@@ -0,0 +1,60 @@
+{
+    properties: {
+        link: 'https://github.com/winmarkltd/BootstrapFormHelpers',
+        platform: 'GitHub',
+        examples: [
+            'https://www.namecheap.com/',
+            'http://bootstrapformhelpers.com/colorpicker/'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.fn.bfhcolorpicker'
+            ],
+            [
+                'window.jQuery.fn.bfhcountries'
+            ],
+            [
+                'window.jQuery.fn.bfhcurrencies'
+            ],
+            [
+                'window.jQuery.fn.bfhdatepicker'
+            ],
+            [
+                'window.jQuery.fn.bfhfonts'
+            ],
+            [
+                'window.jQuery.fn.bfhfontsizes'
+            ],
+            [
+                'window.jQuery.fn.bfhgooglefonts'
+            ],
+            [
+                'window.jQuery.fn.bfhlanguages'
+            ],
+            [
+                'window.jQuery.fn.bfhnumber'
+            ],
+            [
+                'window.jQuery.fn.bfhphone'
+            ],
+            [
+                'window.jQuery.fn.bfhselectbox'
+            ],
+            [
+                'window.jQuery.fn.bfhslider'
+            ],
+            [
+                'window.jQuery.fn.bfhstates'
+            ],
+            [
+                'window.jQuery.fn.bfhtimepicker'
+            ],
+            [
+                'window.jQuery.fn.bfhtimezones'
+            ]
+        ]
+    }
+}

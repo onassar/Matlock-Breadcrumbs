e1ae7a994f293568028ec4c8dcfaa99aa135bc39
onassar <onassar@gmail.com>
2018-12-26 04:27:27 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -8029,6 +8029,29 @@
     }
 }
 ,
+// src/gitHub/louisremi/jquery-smartresize.js
+{
+    properties: {
+        link: 'https://github.com/louisremi/jquery-smartresize',
+        platform: 'GitHub',
+        examples: [
+            'http://louisremi.github.io/jquery-smartresize/demo/index.html',
+            'http://indigovolunteers.org/'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.event.special.debouncedresize'
+            ],
+            [
+                'window.jQuery.event.special.throttledresize'
+            ]
+        ]
+    }
+}
+,
 // src/gitHub/lukaszfiszer/selectnav.js.js
 {
     properties: {
---
+++ src/gitHub/louisremi/jquery-smartresize.js
@@ -0,0 +1,21 @@
+{
+    properties: {
+        link: 'https://github.com/louisremi/jquery-smartresize',
+        platform: 'GitHub',
+        examples: [
+            'http://louisremi.github.io/jquery-smartresize/demo/index.html',
+            'http://indigovolunteers.org/'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.event.special.debouncedresize'
+            ],
+            [
+                'window.jQuery.event.special.throttledresize'
+            ]
+        ]
+    }
+}

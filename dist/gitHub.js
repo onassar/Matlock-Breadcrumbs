5b36e5189af996bcefbd30dd93b84876903c1512
onassar <onassar@gmail.com>
2018-12-26 02:43:21 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -7064,6 +7064,30 @@
     }
 }
 ,
+// src/gitHub/jquery/jquery-color.js
+{
+    properties: {
+        donate: {
+            link: 'https://js.foundation/about/donate'
+        },
+        examples: [
+            'https://www.kickstarter.com/projects/microcosmpublishing/unf-ck-your-intimacy-relationships-sex-and-dating'
+        ],
+        link: 'https://github.com/jquery/jquery',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.Color',
+                'window.jQuery.Color.fn',
+                'window.jQuery.Color.fn.parse'
+            ]
+        ]
+    }
+}
+,
 // src/gitHub/jquery/jquery-migrate.js
 {
     properties: {
---
+++ src/gitHub/jquery/jquery-color.js
@@ -0,0 +1,22 @@
+{
+    properties: {
+        donate: {
+            link: 'https://js.foundation/about/donate'
+        },
+        examples: [
+            'https://www.kickstarter.com/projects/microcosmpublishing/unf-ck-your-intimacy-relationships-sex-and-dating'
+        ],
+        link: 'https://github.com/jquery/jquery',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.Color',
+                'window.jQuery.Color.fn',
+                'window.jQuery.Color.fn.parse'
+            ]
+        ]
+    }
+}

7ea290546fb4045f858112aacc52c5e4ef7f3796
onassar <onassar@gmail.com>
2018-12-25 20:50:39 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -6364,6 +6364,31 @@
     }
 }
 ,
+// src/gitHub/janpaepke/ScrollMagic.js
+{
+    properties: {
+        link: 'https://github.com/janpaepke/ScrollMagic',
+        notes: [],
+        platform: 'GitHub',
+        tests: [
+            'http://scrollmagic.io/'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.ScrollMagic'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.ScrollMagic.version'
+        ]
+    }
+}
+,
 // src/gitHub/jaredreich/pell.js
 {
     properties: {
---
+++ src/gitHub/janpaepke/ScrollMagic.js
@@ -0,0 +1,23 @@
+{
+    properties: {
+        link: 'https://github.com/janpaepke/ScrollMagic',
+        notes: [],
+        platform: 'GitHub',
+        tests: [
+            'http://scrollmagic.io/'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.ScrollMagic'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.ScrollMagic.version'
+        ]
+    }
+}

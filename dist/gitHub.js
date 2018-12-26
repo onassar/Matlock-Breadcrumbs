15bf6df26a0d98d8c8d894c76fc3dd725a94d8fa
onassar <onassar@gmail.com>
2018-12-25 20:54:59 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -1422,6 +1422,27 @@
     }
 }
 ,
+// src/gitHub/a-jie/Proton.js
+{
+    properties: {
+        link: 'https://github.com/a-jie/Proton',
+        notes: [],
+        platform: 'GitHub',
+        tests: [
+            'http://a-jie.github.io/Proton/example/sparks/sun/fireball.html'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.Proton',
+                'window.Proton.Alpha'
+            ]
+        ]
+    }
+}
+,
 // src/gitHub/aFarkas/html5shiv.js
 {
     properties: {
---
+++ src/gitHub/a-jie/Proton.js
@@ -0,0 +1,19 @@
+{
+    properties: {
+        link: 'https://github.com/a-jie/Proton',
+        notes: [],
+        platform: 'GitHub',
+        tests: [
+            'http://a-jie.github.io/Proton/example/sparks/sun/fireball.html'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.Proton',
+                'window.Proton.Alpha'
+            ]
+        ]
+    }
+}

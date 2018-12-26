2adf11c46f48f36e7413323a865ba01c2ed6fc2e
onassar <onassar@gmail.com>
2018-12-25 21:59:58 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -8432,6 +8432,27 @@
     }
 }
 ,
+// src/gitHub/missive/emoji-mart.js
+{
+    properties: {
+        link: 'https://github.com/missive/emoji-mart',
+        notes: [
+            'https://i.imgur.com/6QH50iS.jpg'
+        ],
+        platform: 'GitHub',
+        tests: [
+            'https://missive.github.io/emoji-mart/'
+        ]
+    },
+    tests: {
+        references: [
+            [
+                'window.EmojiMart'
+            ]
+        ]
+    }
+}
+,
 // src/gitHub/mnater/Hyphenator.js
 {
     properties: {
---
+++ src/gitHub/missive/emoji-mart.js
@@ -0,0 +1,19 @@
+{
+    properties: {
+        link: 'https://github.com/missive/emoji-mart',
+        notes: [
+            'https://i.imgur.com/6QH50iS.jpg'
+        ],
+        platform: 'GitHub',
+        tests: [
+            'https://missive.github.io/emoji-mart/'
+        ]
+    },
+    tests: {
+        references: [
+            [
+                'window.EmojiMart'
+            ]
+        ]
+    }
+}

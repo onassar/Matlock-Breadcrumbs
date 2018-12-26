df8d0a79e51831211e57d3f36a5dd42bbe2247f8
onassar <onassar@gmail.com>
2018-12-26 03:58:48 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -11167,6 +11167,27 @@
     }
 }
 ,
+// src/gitHub/tabalinas/jssocials.js
+{
+    properties: {
+        examples: [
+            'http://js-socials.com/demos/'
+        ],
+        link: 'https://github.com/tabalinas/jssocials',
+        name: 'jsSocials',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jsSocials',
+                'window.jsSocials.setDefaults'
+            ]
+        ]
+    }
+}
+,
 // src/gitHub/ten1seven/what-input.js
 {
     properties: {
---
+++ src/gitHub/tabalinas/jssocials.js
@@ -0,0 +1,19 @@
+{
+    properties: {
+        examples: [
+            'http://js-socials.com/demos/'
+        ],
+        link: 'https://github.com/tabalinas/jssocials',
+        name: 'jsSocials',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jsSocials',
+                'window.jsSocials.setDefaults'
+            ]
+        ]
+    }
+}

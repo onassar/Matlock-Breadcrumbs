8950373a3995bbc419bac61c682ddc0726602c59
onassar <onassar@gmail.com>
2018-12-26 02:49:06 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -4930,6 +4930,25 @@
     }
 }
 ,
+// src/gitHub/ftlabs/ftscroller.js
+{
+    properties: {
+        examples: [
+            'https://labs.ft.com/ftscroller/examples/verticalcontinuous.html'
+        ],
+        link: 'https://github.com/ftlabs/ftscroller',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.FTScroller'
+            ]
+        ]
+    }
+}
+,
 // src/gitHub/fullcalendar/fullcalendar.js
 {
     properties: {
---
+++ src/gitHub/ftlabs/ftscroller.js
@@ -0,0 +1,17 @@
+{
+    properties: {
+        examples: [
+            'https://labs.ft.com/ftscroller/examples/verticalcontinuous.html'
+        ],
+        link: 'https://github.com/ftlabs/ftscroller',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.FTScroller'
+            ]
+        ]
+    }
+}

f16c25982b8b70bf5639ad97cfd96071982fdd7a
onassar <onassar@gmail.com>
2018-12-26 02:52:38 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -9742,6 +9742,25 @@
     }
 }
 ,
+// src/gitHub/pbakaus/scroller.js
+{
+    properties: {
+        examples: [
+            'http://zynga.github.io/scroller/demo/easyscroller.html'
+        ],
+        link: 'https://github.com/pbakaus/scroller',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        statements: [
+            [
+                'new window.Scroller().options'
+            ]
+        ]
+    }
+}
+,
 // src/gitHub/peachananr/tiltedpage_scroll.js
 {
     properties: {
---
+++ src/gitHub/pbakaus/scroller.js
@@ -0,0 +1,17 @@
+{
+    properties: {
+        examples: [
+            'http://zynga.github.io/scroller/demo/easyscroller.html'
+        ],
+        link: 'https://github.com/pbakaus/scroller',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        statements: [
+            [
+                'new window.Scroller().options'
+            ]
+        ]
+    }
+}

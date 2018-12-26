f22cde735f367095a69a48190ffea0ba47594306
onassar <onassar@gmail.com>
2018-12-26 02:58:47 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -12317,6 +12317,28 @@
     }
 }
 ,
+// src/gitHub/yckart/jquery.base64.js.js
+{
+    properties: {
+        examples: [
+            'http://yckart.github.io/jquery.base64.js/'
+        ],
+        link: 'https://github.com/yckart/jquery.base64.js',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.base64',
+                'window.jQuery.base64.atob',
+                'window.jQuery.base64.btoa',
+                'window.jQuery.base64.utf8encode'
+            ]
+        ]
+    }
+}
+,
 // src/gitHub/yiisoft/yii.js
 {
     dependencies: [
---
+++ src/gitHub/yckart/jquery.base64.js.js
@@ -0,0 +1,20 @@
+{
+    properties: {
+        examples: [
+            'http://yckart.github.io/jquery.base64.js/'
+        ],
+        link: 'https://github.com/yckart/jquery.base64.js',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.base64',
+                'window.jQuery.base64.atob',
+                'window.jQuery.base64.btoa',
+                'window.jQuery.base64.utf8encode'
+            ]
+        ]
+    }
+}

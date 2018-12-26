eef59d8f61608f979cead0aeac0640683379c825
onassar <onassar@gmail.com>
2018-12-26 02:06:48 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -4194,6 +4194,25 @@
     }
 }
 ,
+// src/gitHub/eisbehr-/jquery.lazy.js
+{
+    properties: {
+        link: 'https://github.com/eisbehr-/jquery.lazy',
+        platform: 'GitHub',
+        examples: [
+            'https://getstencil.com/'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.fn.lazy'
+            ]
+        ]
+    }
+}
+,
 // src/gitHub/ejci/favico.js.js
 {
     properties: {
---
+++ src/gitHub/eisbehr-/jquery.lazy.js
@@ -0,0 +1,17 @@
+{
+    properties: {
+        link: 'https://github.com/eisbehr-/jquery.lazy',
+        platform: 'GitHub',
+        examples: [
+            'https://getstencil.com/'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.fn.lazy'
+            ]
+        ]
+    }
+}

d7e01c16680f68c0769c434149a3401abed7857c
onassar <onassar@gmail.com>
2018-12-26 03:18:39 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -12175,6 +12175,26 @@
     }
 }
 ,
+// src/gitHub/wmh/jquery-scrollbox.js
+{
+    properties: {
+        examples: [
+            'http://wmh.github.io/jquery-scrollbox/'
+        ],
+        link: 'https://github.com/wmh/jquery-scrollbox',
+        name: 'jQuery Scrollbox',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.fn.scrollbox'
+            ]
+        ]
+    }
+}
+,
 // src/gitHub/woocommerce/FlexSlider.js
 {
     properties: {
---
+++ src/gitHub/wmh/jquery-scrollbox.js
@@ -0,0 +1,18 @@
+{
+    properties: {
+        examples: [
+            'http://wmh.github.io/jquery-scrollbox/'
+        ],
+        link: 'https://github.com/wmh/jquery-scrollbox',
+        name: 'jQuery Scrollbox',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.fn.scrollbox'
+            ]
+        ]
+    }
+}

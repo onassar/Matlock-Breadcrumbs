59a743e9e7372981d8c3a5e0c3e788fc8cb14066
onassar <onassar@gmail.com>
2018-12-27 15:01:18 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -156,6 +156,30 @@
     }
 }
 ,
+// src/gitHub/DataTables/Editor-PHP.js
+{
+    properties: {
+        link: 'https://github.com/DataTables/Editor-PHP',
+        platform: 'GitHub',
+        examples: [
+            'https://editor.datatables.net/'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.fn.DataTable.Editor'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.jQuery.fn.DataTable.Editor.version'
+        ]
+    }
+}
+,
 // src/gitHub/DataTables/Responsive.js
 {
     properties: {
---
+++ src/gitHub/DataTables/Editor-PHP.js
@@ -0,0 +1,22 @@
+{
+    properties: {
+        link: 'https://github.com/DataTables/Editor-PHP',
+        platform: 'GitHub',
+        examples: [
+            'https://editor.datatables.net/'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.fn.DataTable.Editor'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.jQuery.fn.DataTable.Editor.version'
+        ]
+    }
+}

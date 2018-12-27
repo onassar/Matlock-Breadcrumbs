88fb82de93fc2c7b98a72c01a71e6a9a2d7b207a
onassar <onassar@gmail.com>
2018-12-27 18:23:48 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -4549,6 +4549,35 @@
     }
 }
 ,
+// src/gitHub/epeli/underscore.string.js
+{
+    properties: {
+        link: 'https://github.com/epeli/underscore.string',
+        platform: 'GitHub',
+        examples: [
+            'http://epeli.github.io/underscore.string/',
+            'https://wgntv.com/2018/12/27/macaulay-culkin-to-legally-change-his-middle-name-to-macaulay-culkin/'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.s',
+                'window.s.camelcase',
+                'window.s.camelize',
+                'window.s.capitalize',
+                'window.s.center'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.s.VERSION'
+        ]
+    }
+}
+,
 // src/gitHub/expressjs/express.js
 {
     properties: {

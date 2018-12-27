7348f99840f60d2274b19cafb66806fe62447834
onassar <onassar@gmail.com>
2018-12-27 14:58:10 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -156,6 +156,30 @@
     }
 }
 ,
+// src/gitHub/DataTables/Responsive.js
+{
+    properties: {
+        link: 'https://github.com/DataTables/Responsive',
+        platform: 'GitHub',
+        examples: [
+            'https://editor.datatables.net/'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.fn.DataTable.Responsive'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.jQuery.fn.DataTable.Responsive.version'
+        ]
+    }
+}
+,
 // src/gitHub/DmitryBaranovskiy/raphael.js
 {
     properties: {
---
+++ src/gitHub/DataTables/Responsive.js
@@ -0,0 +1,22 @@
+{
+    properties: {
+        link: 'https://github.com/DataTables/Responsive',
+        platform: 'GitHub',
+        examples: [
+            'https://editor.datatables.net/'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.fn.DataTable.Responsive'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.jQuery.fn.DataTable.Responsive.version'
+        ]
+    }
+}

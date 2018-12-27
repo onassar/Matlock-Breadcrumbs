0595a783c9e806a4e386e0bfff79f4c85eb68d99
onassar <onassar@gmail.com>
2018-12-27 18:59:24 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -2540,6 +2540,32 @@
     }
 }
 ,
+// src/gitHub/balupton/history.js.js
+{
+    properties: {
+        examples: [
+            'http://browserstate.github.io/history.js/demo/'
+        ],
+        link: 'https://github.com/browserstate/history.js',
+        notes: [],
+        platform: 'GitHub',
+        tags: [
+            'navigation'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.History',
+                'window.History.back',
+                'window.History.getHash',
+                'window.History.pushState'
+            ]
+        ]
+    }
+}
+,
 // src/gitHub/balupton/jquery-lightbox.js
 {
     properties: {

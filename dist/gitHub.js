7c7564019050f72d72e8980f458c7ffbc0afaea3
onassar <onassar@gmail.com>
2018-12-27 18:55:16 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -2563,6 +2563,29 @@
     }
 }
 ,
+// src/gitHub/balupton/jquery-syntaxhighlighter.js
+{
+    properties: {
+        examples: [
+            'http://balupton.github.io/jquery-syntaxhighlighter/demo/'
+        ],
+        link: 'https://github.com/balupton/jquery-syntaxhighlighter',
+        notes: [],
+        platform: 'GitHub',
+        tags: [
+            'syntax-hightlighter'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.fn.syntaxHighlight'
+            ]
+        ]
+    }
+}
+,
 // src/gitHub/basecamp/trix.js
 {
     properties: {
---
+++ src/gitHub/balupton/jquery-syntaxhighlighter.js
@@ -0,0 +1,21 @@
+{
+    properties: {
+        examples: [
+            'http://balupton.github.io/jquery-syntaxhighlighter/demo/'
+        ],
+        link: 'https://github.com/balupton/jquery-syntaxhighlighter',
+        notes: [],
+        platform: 'GitHub',
+        tags: [
+            'syntax-hightlighter'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.fn.syntaxHighlight'
+            ]
+        ]
+    }
+}

33d9d8a37b6b8e24379a8d236efb4da9e7672e0c
onassar <onassar@gmail.com>
2018-12-27 18:32:27 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -10029,6 +10029,49 @@
     }
 }
 ,
+// src/gitHub/paypal/paypal-checkout.js
+{
+    properties: {
+        examples: [
+            'https://developer.paypal.com/demo/checkout/#/pattern/client'
+        ],
+        link: 'https://github.com/paypal/paypal-checkout',
+        platform: 'GitHub',
+        type: 'repository',
+        wikiPedia: {
+            load: true,
+            title: 'PayPal',
+            sections: []
+        }
+    },
+    tests: {
+        references: [
+            [
+                'window.paypal',
+                'window.paypal.Button',
+                'window.paypal.FUNDING'
+            ],
+            [
+                'window.PAYPAL',
+                'window.PAYPAL.Button',
+                'window.PAYPAL.FUNDING'
+            ],
+            [
+                'window.ppxo',
+                'window.ppxo.Button',
+                'window.ppxo.FUNDING'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.paypal.version',
+            'window.PAYPAL.version',
+            'window.ppxo.version'
+        ]
+    }
+}
+,
 // src/gitHub/pbakaus/scroller.js
 {
     properties: {
---
+++ src/gitHub/paypal/paypal-checkout.js
@@ -0,0 +1,41 @@
+{
+    properties: {
+        examples: [
+            'https://developer.paypal.com/demo/checkout/#/pattern/client'
+        ],
+        link: 'https://github.com/paypal/paypal-checkout',
+        platform: 'GitHub',
+        type: 'repository',
+        wikiPedia: {
+            load: true,
+            title: 'PayPal',
+            sections: []
+        }
+    },
+    tests: {
+        references: [
+            [
+                'window.paypal',
+                'window.paypal.Button',
+                'window.paypal.FUNDING'
+            ],
+            [
+                'window.PAYPAL',
+                'window.PAYPAL.Button',
+                'window.PAYPAL.FUNDING'
+            ],
+            [
+                'window.ppxo',
+                'window.ppxo.Button',
+                'window.ppxo.FUNDING'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.paypal.version',
+            'window.PAYPAL.version',
+            'window.ppxo.version'
+        ]
+    }
+}

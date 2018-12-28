b9330566a70d31a3c16bd3b02aa42f2714500000
onassar <onassar@gmail.com>
2018-12-28 03:38:54 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -6730,6 +6730,30 @@
     }
 }
 ,
+// src/gitHub/jackmoore/colorbox.js
+{
+    properties: {
+        examples: [
+            'http://www.jacklmoore.com/colorbox/example1/',
+            'https://www.iom.int/'
+        ],
+        link: 'https://github.com/jackmoore/colorbox',
+        notes: [],
+        platform: 'GitHub',
+        tags: [
+            'lightbox'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.fn.colorbox'
+            ]
+        ]
+    }
+}
+,
 // src/gitHub/jackocnr/intl-tel-input.js
 {
     properties: {
---
+++ src/gitHub/jackmoore/colorbox.js
@@ -0,0 +1,22 @@
+{
+    properties: {
+        examples: [
+            'http://www.jacklmoore.com/colorbox/example1/',
+            'https://www.iom.int/'
+        ],
+        link: 'https://github.com/jackmoore/colorbox',
+        notes: [],
+        platform: 'GitHub',
+        tags: [
+            'lightbox'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.fn.colorbox'
+            ]
+        ]
+    }
+}

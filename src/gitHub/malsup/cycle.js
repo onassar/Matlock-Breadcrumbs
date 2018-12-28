59ca263771beecf0a9e5c06660b9d1d0a4d59118
onassar <onassar@gmail.com>
2018-12-28 05:55:31 -0500

    Breadcrumb update (auto)

---
+++ src/gitHub/malsup/cycle.js
@@ -0,0 +1,26 @@
+{
+    properties: {
+        donate: {
+            link: 'https://www.paypal.com/webapps/shoppingcart?flowlogging_id=5c45f4c86b318&mfid=1545295058279_5c45f4c86b318#/checkout/openButton'
+        },
+        link: 'https://github.com/malsup/cycle',
+        platform: 'GitHub',
+        examples: [
+            'http://jquery.malsup.com/cycle/'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.fn.cycle',
+                'window.jQuery.fn.cycle.ver'
+            ]
+        ]
+    },
+    versions: {
+        statements: [
+            'window.jQuery.fn.cycle.ver()'
+        ]
+    }
+}

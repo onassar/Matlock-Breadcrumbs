179bd5799489a94649aa3c06856db4a39026c90b
onassar <onassar@gmail.com>
2018-12-28 14:25:07 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -62,6 +62,27 @@
     }
 }
 ,
+// src/gitHub/BrainJS/brain.js.js
+{
+    properties: {
+        examples: [
+            'https://brain.js.org/'
+        ],
+        link: 'https://github.com/BrainJS/brain.js',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.brain',
+                'window.brain.likely',
+                'window.brain.NeuralNetwork'
+            ]
+        ]
+    }
+}
+,
 // src/gitHub/CSSLint/csslint.js
 {
     properties: {
---
+++ src/gitHub/BrainJS/brain.js.js
@@ -0,0 +1,19 @@
+{
+    properties: {
+        examples: [
+            'https://brain.js.org/'
+        ],
+        link: 'https://github.com/BrainJS/brain.js',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.brain',
+                'window.brain.likely',
+                'window.brain.NeuralNetwork'
+            ]
+        ]
+    }
+}

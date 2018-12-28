4ca14ff13e8e1ad6ae62a9e0a53756940b26df1c
onassar <onassar@gmail.com>
2018-12-28 04:38:52 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -10379,6 +10379,30 @@
     }
 }
 ,
+// src/gitHub/pixelcog/parallax.js.js
+{
+    properties: {
+        examples: [
+            'http://pixelcog.github.io/parallax.js/'
+        ],
+        link: 'https://github.com/pixelcog/parallax.js',
+        platform: 'GitHub',
+        tags: [
+            'parallax',
+            'scrolling'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.fn.parallax',
+                'window.jQuery.fn.parallax.noConflict'
+            ]
+        ]
+    }
+}
+,
 // src/gitHub/pixxelfactory/jInvertScroll.js
 {
     properties: {
---
+++ src/gitHub/pixelcog/parallax.js.js
@@ -0,0 +1,22 @@
+{
+    properties: {
+        examples: [
+            'http://pixelcog.github.io/parallax.js/'
+        ],
+        link: 'https://github.com/pixelcog/parallax.js',
+        platform: 'GitHub',
+        tags: [
+            'parallax',
+            'scrolling'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.fn.parallax',
+                'window.jQuery.fn.parallax.noConflict'
+            ]
+        ]
+    }
+}

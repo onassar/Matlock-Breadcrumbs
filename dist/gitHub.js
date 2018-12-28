09700096cbedcc7c1b8efc8e3f5a3526085041e1
onassar <onassar@gmail.com>
2018-12-28 05:02:50 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -11976,6 +11976,30 @@
     }
 }
 ,
+// src/gitHub/thednp/kute.js.js
+{
+    properties: {
+        examples: [
+            'http://thednp.github.io/kute.js/'
+        ],
+        link: 'https://github.com/thednp/kute.js',
+        platform: 'GitHub',
+        tags: [
+            'animation',
+            'svg'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.KUTE',
+                'window.KUTE.defaultOptions'
+            ]
+        ]
+    }
+}
+,
 // src/gitHub/thorst/jquery-idletimer.js
 {
     properties: {
---
+++ src/gitHub/thednp/kute.js.js
@@ -0,0 +1,22 @@
+{
+    properties: {
+        examples: [
+            'http://thednp.github.io/kute.js/'
+        ],
+        link: 'https://github.com/thednp/kute.js',
+        platform: 'GitHub',
+        tags: [
+            'animation',
+            'svg'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.KUTE',
+                'window.KUTE.defaultOptions'
+            ]
+        ]
+    }
+}

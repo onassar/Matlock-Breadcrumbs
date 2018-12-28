04073816e6094250129f76c2f697231e54562c9a
onassar <onassar@gmail.com>
2018-12-28 05:26:00 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -1001,6 +1001,28 @@
     }
 }
 ,
+// src/gitHub/SVG-Edit/svgedit.js
+{
+    properties: {
+        examples: [
+            'https://svg-edit.github.io/svgedit/releases/latest/editor/svg-editor.html'
+        ],
+        link: 'https://github.com/SVG-Edit/svgedit',
+        platform: 'GitHub',
+        tags: [
+            'svg'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.svgEditor'
+            ]
+        ]
+    }
+}
+,
 // src/gitHub/SginalR/SginalR.js
 {
     properties: {
---
+++ src/gitHub/SVG-Edit/svgedit.js
@@ -0,0 +1,20 @@
+{
+    properties: {
+        examples: [
+            'https://svg-edit.github.io/svgedit/releases/latest/editor/svg-editor.html'
+        ],
+        link: 'https://github.com/SVG-Edit/svgedit',
+        platform: 'GitHub',
+        tags: [
+            'svg'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.svgEditor'
+            ]
+        ]
+    }
+}

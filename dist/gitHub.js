c8d57977b373f63a1f6fa2edb82fef0e92b3f23b
onassar <onassar@gmail.com>
2018-12-28 04:08:45 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -10375,6 +10375,30 @@
     }
 }
 ,
+// src/gitHub/pupunzi/jquery.mb.YTPlayer.js
+{
+    properties: {
+        examples: [
+            'https://pupunzi.com/mb.components/mb.YTPlayer/demo/demo.html'
+        ],
+        link: 'https://github.com/pupunzi/jquery.mb.YTPlayer',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.fn.YTPlayer'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.jQuery.mbYTPlayer.version'
+        ]
+    }
+}
+,
 // src/gitHub/pure-css/pure.js
 {
     properties: {
---
+++ src/gitHub/pupunzi/jquery.mb.YTPlayer.js
@@ -0,0 +1,22 @@
+{
+    properties: {
+        examples: [
+            'https://pupunzi.com/mb.components/mb.YTPlayer/demo/demo.html'
+        ],
+        link: 'https://github.com/pupunzi/jquery.mb.YTPlayer',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.fn.YTPlayer'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.jQuery.mbYTPlayer.version'
+        ]
+    }
+}

63c3d31d7a4d27ecee877711bf90f08177eaca71
onassar <onassar@gmail.com>
2018-12-28 04:20:20 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -496,6 +496,25 @@
     }
 }
 ,
+// src/gitHub/Julienh/Sharrre.js
+{
+    properties: {
+        examples: [
+            'http://sharrre.com/'
+        ],
+        link: 'https://github.com/Julienh/Sharrre',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.fn.sharrre'
+            ]
+        ]
+    }
+}
+,
 // src/gitHub/KaTeX/KaTeX.js
 {
     properties: {
---
+++ src/gitHub/Julienh/Sharrre.js
@@ -0,0 +1,17 @@
+{
+    properties: {
+        examples: [
+            'http://sharrre.com/'
+        ],
+        link: 'https://github.com/Julienh/Sharrre',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.fn.sharrre'
+            ]
+        ]
+    }
+}

455b496500fcfbff3a347a51c8db73532e720045
onassar <onassar@gmail.com>
2018-12-28 05:28:58 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -7476,6 +7476,31 @@
     }
 }
 ,
+// src/gitHub/jeresig/jquery.hotkeys.js
+{
+    properties: {
+        link: 'https://github.com/jeresig/jquery.hotkeys',
+        platform: 'GitHub',
+        examples: [
+            'https://rawgit.com/jeresig/jquery.hotkeys/master/test-static-01.html'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.hotkeys',
+                'window.jQuery.hotkeys.specialKeys'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.jQuery.hotkeys.version'
+        ]
+    }
+}
+,
 // src/gitHub/jimmynotjim/scrollnav.js
 {
     properties: {
---
+++ src/gitHub/jeresig/jquery.hotkeys.js
@@ -0,0 +1,23 @@
+{
+    properties: {
+        link: 'https://github.com/jeresig/jquery.hotkeys',
+        platform: 'GitHub',
+        examples: [
+            'https://rawgit.com/jeresig/jquery.hotkeys/master/test-static-01.html'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.hotkeys',
+                'window.jQuery.hotkeys.specialKeys'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.jQuery.hotkeys.version'
+        ]
+    }
+}

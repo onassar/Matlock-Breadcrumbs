279a854b2de66704136813a6eb44f6e4bde4268e
onassar <onassar@gmail.com>
2018-12-28 06:14:27 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -9178,6 +9178,26 @@
     }
 }
 ,
+// src/gitHub/mattboldt/typed.js.js
+{
+    properties: {
+        examples: [
+            'https://mattboldt.com/demos/typed-js/'
+        ],
+        link: 'https://github.com/mattboldt/typed.js',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.fn.typed',
+                'window.jQuery.fn.typed.defaults'
+            ]
+        ]
+    }
+}
+,
 // src/gitHub/mattbryson/TouchSwipe-Jquery-Plugin.js
 {
     properties: {
---
+++ src/gitHub/mattboldt/typed.js.js
@@ -0,0 +1,18 @@
+{
+    properties: {
+        examples: [
+            'https://mattboldt.com/demos/typed-js/'
+        ],
+        link: 'https://github.com/mattboldt/typed.js',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.fn.typed',
+                'window.jQuery.fn.typed.defaults'
+            ]
+        ]
+    }
+}

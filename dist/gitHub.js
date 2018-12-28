47c6ea6efb6e995170bf8b21f6ba3c2312c9b20e
onassar <onassar@gmail.com>
2018-12-28 05:05:05 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -11096,6 +11096,35 @@
     }
 }
 ,
+// src/gitHub/schteppe/p2.js.js
+{
+    properties: {
+        examples: [
+            'http://schteppe.github.io/p2.js/demos/buoyancy.html'
+        ],
+        link: 'https://github.com/schteppe/p2.js',
+        platform: 'GitHub',
+        tags: [
+            'animation',
+            'svg'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.p2',
+                'window.p2.Body'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.p2.version'
+        ]
+    }
+}
+,
 // src/gitHub/sciactive/pnotify.js
 {
     properties: {
---
+++ src/gitHub/schteppe/p2.js.js
@@ -0,0 +1,27 @@
+{
+    properties: {
+        examples: [
+            'http://schteppe.github.io/p2.js/demos/buoyancy.html'
+        ],
+        link: 'https://github.com/schteppe/p2.js',
+        platform: 'GitHub',
+        tags: [
+            'animation',
+            'svg'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.p2',
+                'window.p2.Body'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.p2.version'
+        ]
+    }
+}

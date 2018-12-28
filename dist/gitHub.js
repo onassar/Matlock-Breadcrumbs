7e2cdcfb4c7a97e9a5da4c3499e308d5c63af4b6
onassar <onassar@gmail.com>
2018-12-28 03:54:18 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -11806,6 +11806,29 @@
     }
 }
 ,
+// src/gitHub/tuupola/jquery_chained.js
+{
+    properties: {
+        link: 'https://github.com/tuupola/jquery_chained',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.fn.chained',
+                'window.jQuery.fn.chained.defaults',
+                'window.jQuery.fn.chainedTo'
+            ],
+            [
+                'window.Zepto.fn.chained',
+                'window.Zepto.fn.chained.defaults',
+                'window.Zepto.fn.chainedTo'
+            ]
+        ]
+    }
+}
+,
 // src/gitHub/tuupola/jquery_lazyload.js
 {
     properties: {
---
+++ src/gitHub/tuupola/jquery_chained.js
@@ -0,0 +1,21 @@
+{
+    properties: {
+        link: 'https://github.com/tuupola/jquery_chained',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.fn.chained',
+                'window.jQuery.fn.chained.defaults',
+                'window.jQuery.fn.chainedTo'
+            ],
+            [
+                'window.Zepto.fn.chained',
+                'window.Zepto.fn.chained.defaults',
+                'window.Zepto.fn.chainedTo'
+            ]
+        ]
+    }
+}

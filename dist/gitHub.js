ec69e5cfda3b0d881f43819c7f6b5a66d0fce5be
onassar <onassar@gmail.com>
2018-12-28 04:17:16 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -10424,6 +10424,30 @@
     }
 }
 ,
+// src/gitHub/pupunzi/jquery.mb.extruder.js
+{
+    properties: {
+        examples: [
+            'https://pupunzi.com/mb.components/mb.extruder/demo/demo.html'
+        ],
+        link: 'https://github.com/pupunzi/jquery.mb.extruder',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.fn.buildMbExtruder'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.jQuery.mbExtruder.version'
+        ]
+    }
+}
+,
 // src/gitHub/pure-css/pure.js
 {
     properties: {
---
+++ src/gitHub/pupunzi/jquery.mb.extruder.js
@@ -0,0 +1,22 @@
+{
+    properties: {
+        examples: [
+            'https://pupunzi.com/mb.components/mb.extruder/demo/demo.html'
+        ],
+        link: 'https://github.com/pupunzi/jquery.mb.extruder',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.fn.buildMbExtruder'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.jQuery.mbExtruder.version'
+        ]
+    }
+}

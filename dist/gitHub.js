34579e54f2c451c792216714a156ebc0190da8a7
onassar <onassar@gmail.com>
2018-12-28 05:10:06 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -4712,6 +4712,30 @@
     }
 }
 ,
+// src/gitHub/evanw/glfx.js.js
+{
+    properties: {
+        examples: [
+            'http://evanw.github.io/glfx.js/demo/'
+        ],
+        link: 'https://github.com/evanw/glfx.js',
+        platform: 'GitHub',
+        tags: [
+            'canvas'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.fx',
+                'window.fx.canvas',
+                'window.fx.splineInterpolate'
+            ]
+        ]
+    }
+}
+,
 // src/gitHub/expressjs/express.js
 {
     properties: {
---
+++ src/gitHub/evanw/glfx.js.js
@@ -0,0 +1,22 @@
+{
+    properties: {
+        examples: [
+            'http://evanw.github.io/glfx.js/demo/'
+        ],
+        link: 'https://github.com/evanw/glfx.js',
+        platform: 'GitHub',
+        tags: [
+            'canvas'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.fx',
+                'window.fx.canvas',
+                'window.fx.splineInterpolate'
+            ]
+        ]
+    }
+}

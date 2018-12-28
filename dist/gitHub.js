83790a813ebec2409aaae9a30302c219bab22d16
onassar <onassar@gmail.com>
2018-12-27 19:05:35 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -11323,6 +11323,30 @@
     }
 }
 ,
+// src/gitHub/staltz/xstream.js
+{
+    properties: {
+        examples: [
+            'http://staltz.github.io/xstream/'
+        ],
+        link: 'https://github.com/staltz/xstream',
+        notes: [],
+        platform: 'GitHub',
+        tags: [],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.xs',
+                'window.xs.empty',
+                'window.xs.merge',
+                'window.xs.periodic'
+            ]
+        ]
+    }
+}
+,
 // src/gitHub/stripe/jquery.payment.js
 {
     properties: {
---
+++ src/gitHub/staltz/xstream.js
@@ -0,0 +1,22 @@
+{
+    properties: {
+        examples: [
+            'http://staltz.github.io/xstream/'
+        ],
+        link: 'https://github.com/staltz/xstream',
+        notes: [],
+        platform: 'GitHub',
+        tags: [],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.xs',
+                'window.xs.empty',
+                'window.xs.merge',
+                'window.xs.periodic'
+            ]
+        ]
+    }
+}

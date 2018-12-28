5dffcd8e5a8cf80fb99cf95a4135bff254a26bf6
onassar <onassar@gmail.com>
2018-12-28 04:36:23 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -5157,6 +5157,28 @@
     }
 }
 ,
+// src/gitHub/flesler/jquery.serialScroll.js
+{
+    properties: {
+        examples: [
+            'http://demos.flesler.com/jquery/serialScroll/'
+        ],
+        link: 'https://github.com/flesler/jquery.serialScroll',
+        platform: 'GitHub',
+        tags: [
+            'scrolling'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.fn.serialScroll'
+            ]
+        ]
+    }
+}
+,
 // src/gitHub/flot/flot.js
 {
     properties: {
---
+++ src/gitHub/flesler/jquery.serialScroll.js
@@ -0,0 +1,20 @@
+{
+    properties: {
+        examples: [
+            'http://demos.flesler.com/jquery/serialScroll/'
+        ],
+        link: 'https://github.com/flesler/jquery.serialScroll',
+        platform: 'GitHub',
+        tags: [
+            'scrolling'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.fn.serialScroll'
+            ]
+        ]
+    }
+}

91be36b3a4a0fe4d064b480c48701e091b4dfb39
onassar <onassar@gmail.com>
2018-12-28 05:11:36 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -5839,6 +5839,30 @@
     }
 }
 ,
+// src/gitHub/greggman/twgl.js.js
+{
+    properties: {
+        examples: [
+            'http://twgljs.org/'
+        ],
+        link: 'https://github.com/greggman/twgl.js',
+        platform: 'GitHub',
+        tags: [
+            'canvas',
+            'webgl'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.twgl',
+                'window.twgl.getContext'
+            ]
+        ]
+    }
+}
+,
 // src/gitHub/gregjacobs/Autolinker.js.js
 {
     properties: {
---
+++ src/gitHub/greggman/twgl.js.js
@@ -0,0 +1,22 @@
+{
+    properties: {
+        examples: [
+            'http://twgljs.org/'
+        ],
+        link: 'https://github.com/greggman/twgl.js',
+        platform: 'GitHub',
+        tags: [
+            'canvas',
+            'webgl'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.twgl',
+                'window.twgl.getContext'
+            ]
+        ]
+    }
+}

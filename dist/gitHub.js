fef6a29304e7c12dfc95d60755e5d70c9f312cdd
onassar <onassar@gmail.com>
2018-12-29 04:57:09 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -2999,6 +2999,36 @@
     }
 }
 ,
+// src/gitHub/bestiejs/punycode.js.js
+{
+    properties: {
+        examples: [
+            'https://imgur.com/FURy2EZ'
+        ],
+        link: 'https://github.com/bestiejs/punycode.js',
+        platform: 'GitHub',
+        tags: [
+            'converter'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.punycode',
+                'window.punycode.ucs2',
+                'window.punycode.decode',
+                'window.punycode.toASCII'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.punycode.version'
+        ]
+    }
+}
+,
 // src/gitHub/bevacqua/dragula.js
 {
     properties: {
---
+++ src/gitHub/bestiejs/punycode.js.js
@@ -0,0 +1,28 @@
+{
+    properties: {
+        examples: [
+            'https://imgur.com/FURy2EZ'
+        ],
+        link: 'https://github.com/bestiejs/punycode.js',
+        platform: 'GitHub',
+        tags: [
+            'converter'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.punycode',
+                'window.punycode.ucs2',
+                'window.punycode.decode',
+                'window.punycode.toASCII'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.punycode.version'
+        ]
+    }
+}

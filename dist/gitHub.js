16c34f6d8b1078b4aabbdfda0982c36760055637
onassar <onassar@gmail.com>
2018-12-29 04:36:40 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -3676,6 +3676,30 @@
     }
 }
 ,
+// src/gitHub/chriskempson/tomorrow-theme.js
+{
+    properties: {
+        examples: [
+            'https://farbelous.io/bootstrap-colorpicker/'
+        ],
+        link: 'https://github.com/chriskempson/tomorrow-theme',
+        platform: 'GitHub',
+        tags: [
+            'theme'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        patterns: {
+            page: [
+                [
+                    /<link[^>]+tomorrow.css[^>]+>/i
+                ]
+            ]
+        }
+    }
+}
+,
 // src/gitHub/chriso/validator.js.js
 {
     properties: {
---
+++ src/gitHub/chriskempson/tomorrow-theme.js
@@ -0,0 +1,22 @@
+{
+    properties: {
+        examples: [
+            'https://farbelous.io/bootstrap-colorpicker/'
+        ],
+        link: 'https://github.com/chriskempson/tomorrow-theme',
+        platform: 'GitHub',
+        tags: [
+            'theme'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        patterns: {
+            page: [
+                [
+                    /<link[^>]+tomorrow.css[^>]+>/i
+                ]
+            ]
+        }
+    }
+}

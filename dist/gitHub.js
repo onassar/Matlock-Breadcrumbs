9009fc0da2d854df7667f8e96841018e6782f6cd
onassar <onassar@gmail.com>
2018-12-29 05:47:03 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -13000,6 +13000,31 @@
     }
 }
 ,
+// src/gitHub/tholman/github-corners.js
+{
+    properties: {
+        examples: [
+            'http://thompsonemerson.github.io/zoomove/',
+            'http://tholman.com/github-corners/'
+        ],
+        link: 'https://github.com/tholman/github-corners',
+        platform: 'GitHub',
+        tags: [
+            'icons'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        strings: {
+            page: [
+                [
+                    'style="transform-origin: 130px 106px;" class="octo-arm"'
+                ]
+            ]
+        }
+    }
+}
+,
 // src/gitHub/thompsonemerson/zoomove.js
 {
     properties: {
---
+++ src/gitHub/tholman/github-corners.js
@@ -0,0 +1,23 @@
+{
+    properties: {
+        examples: [
+            'http://thompsonemerson.github.io/zoomove/',
+            'http://tholman.com/github-corners/'
+        ],
+        link: 'https://github.com/tholman/github-corners',
+        platform: 'GitHub',
+        tags: [
+            'icons'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        strings: {
+            page: [
+                [
+                    'style="transform-origin: 130px 106px;" class="octo-arm"'
+                ]
+            ]
+        }
+    }
+}

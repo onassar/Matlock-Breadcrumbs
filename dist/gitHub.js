6d96d12ed0fff6572eccea266209d3b498aeef2d
onassar <onassar@gmail.com>
2018-12-29 06:00:43 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -10630,6 +10630,31 @@
     }
 }
 ,
+// src/gitHub/neveldo/jQuery-Mapael.js
+{
+    properties: {
+        examples: [
+            'https://www.vincentbroute.fr/mapael/'
+        ],
+        link: 'https://github.com/neveldo/jQuery-Mapael',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.mapael',
+                'window.jQuery.fn.mapael'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.jQuery.mapael.version'
+        ]
+    }
+}
+,
 // src/gitHub/nfl/react-helmet.js
 {
     properties: {
---
+++ src/gitHub/neveldo/jQuery-Mapael.js
@@ -0,0 +1,23 @@
+{
+    properties: {
+        examples: [
+            'https://www.vincentbroute.fr/mapael/'
+        ],
+        link: 'https://github.com/neveldo/jQuery-Mapael',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.mapael',
+                'window.jQuery.fn.mapael'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.jQuery.mapael.version'
+        ]
+    }
+}

8c2450e8e41a91e142fe2cf4dda62f553b22bd79
onassar <onassar@gmail.com>
2018-12-29 06:44:46 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -10815,6 +10815,32 @@
     }
 }
 ,
+// src/gitHub/ngryman/jquery.finger.js
+{
+    properties: {
+        examples: [
+            'https://www.altitude-sports.com/collections/all/brands-oakley'
+        ],
+        link: 'https://github.com/ngryman/jquery.finger',
+        platform: 'GitHub',
+        tags: [
+            'touch-events'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.fn.tap',
+                'window.jQuery.fn.doubletap',
+                'window.jQuery.fn.press',
+                'window.jQuery.fn.drag',
+                'window.jQuery.fn.flick'
+            ]
+        ]
+    }
+}
+,
 // src/gitHub/nienines/cowboy.js
 {
     properties: {
---
+++ src/gitHub/ngryman/jquery.finger.js
@@ -0,0 +1,24 @@
+{
+    properties: {
+        examples: [
+            'https://www.altitude-sports.com/collections/all/brands-oakley'
+        ],
+        link: 'https://github.com/ngryman/jquery.finger',
+        platform: 'GitHub',
+        tags: [
+            'touch-events'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.fn.tap',
+                'window.jQuery.fn.doubletap',
+                'window.jQuery.fn.press',
+                'window.jQuery.fn.drag',
+                'window.jQuery.fn.flick'
+            ]
+        ]
+    }
+}

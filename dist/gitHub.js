f04294bf506c95c95173b02d72c0d8a168c4cfb3
onassar <onassar@gmail.com>
2018-12-29 02:09:30 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -34,6 +34,30 @@
     }
 }
 ,
+// src/gitHub/AudithSoftworks/Uniform.js
+{
+    properties: {
+        examples: [
+            'http://opensource.imanov.net/uniform/'
+        ],
+        link: 'https://github.com/AudithSoftworks/Uniform',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.uniform.options'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.jQuery.uniform.options.version'
+        ]
+    }
+}
+,
 // src/gitHub/Bilibili/flv.js.js
 {
     properties: {
---
+++ src/gitHub/AudithSoftworks/Uniform.js
@@ -0,0 +1,22 @@
+{
+    properties: {
+        examples: [
+            'http://opensource.imanov.net/uniform/'
+        ],
+        link: 'https://github.com/AudithSoftworks/Uniform',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.uniform.options'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.jQuery.uniform.options.version'
+        ]
+    }
+}

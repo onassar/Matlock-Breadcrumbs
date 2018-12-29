c547620e573b6838debedc0f9e84c7f9b0de684d
onassar <onassar@gmail.com>
2018-12-29 05:25:10 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -5399,6 +5399,25 @@
     }
 }
 ,
+// src/gitHub/fdansv/marginotes.js
+{
+    properties: {
+        examples: [
+            'http://francisco.dance/'
+        ],
+        link: 'https://github.com/fdansv/marginotes',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.fn.marginotes'
+            ]
+        ]
+    }
+}
+,
 // src/gitHub/fengyuanchen/cropper.js
 {
     properties: {
---
+++ src/gitHub/fdansv/marginotes.js
@@ -0,0 +1,17 @@
+{
+    properties: {
+        examples: [
+            'http://francisco.dance/'
+        ],
+        link: 'https://github.com/fdansv/marginotes',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.fn.marginotes'
+            ]
+        ]
+    }
+}

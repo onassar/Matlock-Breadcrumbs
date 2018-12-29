a3807b67294871d6c736aeed7c05f29f55515445
onassar <onassar@gmail.com>
2018-12-29 17:49:13 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -3601,6 +3601,32 @@
     }
 }
 ,
+// src/gitHub/canjs/canjs.js
+{
+    properties: {
+        examples: [
+            'https://canjs.com/',
+            'https://www.apple.com/ca/shop/refurbished/mac/15-inch'
+        ],
+        link: 'https://github.com/canjs/canjs',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.can',
+                'window.can.Animation'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.can.VERSION'
+        ]
+    }
+}
+,
 // src/gitHub/caolan/async.js
 {
     properties: {
---
+++ src/gitHub/canjs/canjs.js
@@ -0,0 +1,24 @@
+{
+    properties: {
+        examples: [
+            'https://canjs.com/',
+            'https://www.apple.com/ca/shop/refurbished/mac/15-inch'
+        ],
+        link: 'https://github.com/canjs/canjs',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.can',
+                'window.can.Animation'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.can.VERSION'
+        ]
+    }
+}

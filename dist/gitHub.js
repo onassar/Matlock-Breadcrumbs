8855e006c5001dd3bfe03c9f33dc556849a39f2d
onassar <onassar@gmail.com>
2018-12-29 05:03:59 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -11477,6 +11477,38 @@
     }
 }
 ,
+// src/gitHub/qunitjs/qunit.js
+{
+    properties: {
+        donate: {
+            link: 'https://js.foundation/about/donate'
+        },
+        examples: [
+            'https://qunitjs.com/resources/example-index.html'
+        ],
+        link: 'https://github.com/qunitjs/qunit',
+        platform: 'GitHub',
+        type: 'repository',
+        wikiPedia: {
+            load: true,
+            title: 'QUnit'
+        }
+    },
+    tests: {
+        references: [
+            [
+                'window.QUnit',
+                'window.QUnit.test'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.QUnit.version'
+        ]
+    }
+}
+,
 // src/gitHub/rails/rails.js
 {
     properties: {
---
+++ src/gitHub/qunitjs/qunit.js
@@ -0,0 +1,30 @@
+{
+    properties: {
+        donate: {
+            link: 'https://js.foundation/about/donate'
+        },
+        examples: [
+            'https://qunitjs.com/resources/example-index.html'
+        ],
+        link: 'https://github.com/qunitjs/qunit',
+        platform: 'GitHub',
+        type: 'repository',
+        wikiPedia: {
+            load: true,
+            title: 'QUnit'
+        }
+    },
+    tests: {
+        references: [
+            [
+                'window.QUnit',
+                'window.QUnit.test'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.QUnit.version'
+        ]
+    }
+}

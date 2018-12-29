ba6aac55f5edb67c0669e40ea1db7979524cb76a
onassar <onassar@gmail.com>
2018-12-29 04:33:19 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -5014,6 +5014,29 @@
     }
 }
 ,
+// src/gitHub/evoluteur/colorpicker.js
+{
+    properties: {
+        alternatives: [
+            'https://github.com/farbelous/bootstrap-colorpicker'
+        ],
+        examples: [
+            'http://evoluteur.github.io/colorpicker/'
+        ],
+        link: 'https://github.com/evoluteur/colorpicker',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.fn.colorpicker',
+                '!window.jQuery.colorpicker'
+            ]
+        ]
+    }
+}
+,
 // src/gitHub/expressjs/express.js
 {
     properties: {
---
+++ src/gitHub/evoluteur/colorpicker.js
@@ -0,0 +1,21 @@
+{
+    properties: {
+        alternatives: [
+            'https://github.com/farbelous/bootstrap-colorpicker'
+        ],
+        examples: [
+            'http://evoluteur.github.io/colorpicker/'
+        ],
+        link: 'https://github.com/evoluteur/colorpicker',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.fn.colorpicker',
+                '!window.jQuery.colorpicker'
+            ]
+        ]
+    }
+}

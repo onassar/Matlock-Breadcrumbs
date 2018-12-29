5f3eb16cd81c04b4927a4f38c6c07d4f7d67aeac
onassar <onassar@gmail.com>
2018-12-29 05:29:03 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -12963,6 +12963,25 @@
     }
 }
 ,
+// src/gitHub/thompsonemerson/zoomove.js
+{
+    properties: {
+        examples: [
+            'http://thompsonemerson.github.io/zoomove/'
+        ],
+        link: 'https://github.com/thompsonemerson/zoomove',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.window.jQuery.fn.ZooMove'
+            ]
+        ]
+    }
+}
+,
 // src/gitHub/thorst/jquery-idletimer.js
 {
     properties: {
---
+++ src/gitHub/thompsonemerson/zoomove.js
@@ -0,0 +1,17 @@
+{
+    properties: {
+        examples: [
+            'http://thompsonemerson.github.io/zoomove/'
+        ],
+        link: 'https://github.com/thompsonemerson/zoomove',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.window.jQuery.fn.ZooMove'
+            ]
+        ]
+    }
+}

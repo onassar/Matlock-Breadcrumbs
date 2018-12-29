1993586db607afd0a4d2f154850d2d32698ab4d4
onassar <onassar@gmail.com>
2018-12-29 06:04:27 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -8745,6 +8745,33 @@
     }
 }
 ,
+// src/gitHub/kartograph/kartograph.js.js
+{
+    properties: {
+        examples: [
+            'http://kartograph.org/'
+        ],
+        link: 'https://github.com/kartograph/kartograph.js',
+        platform: 'GitHub',
+        tags: [
+            'maps'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.kartograph'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.kartograph.version'
+        ]
+    }
+}
+,
 // src/gitHub/keen/keen-js.js
 {
     properties: {
---
+++ src/gitHub/kartograph/kartograph.js.js
@@ -0,0 +1,25 @@
+{
+    properties: {
+        examples: [
+            'http://kartograph.org/'
+        ],
+        link: 'https://github.com/kartograph/kartograph.js',
+        platform: 'GitHub',
+        tags: [
+            'maps'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.kartograph'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.kartograph.version'
+        ]
+    }
+}

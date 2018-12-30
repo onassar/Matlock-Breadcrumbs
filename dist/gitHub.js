4d213bd6f44de876e84d7191b45efc4729feed11
onassar <onassar@gmail.com>
2018-12-30 13:17:32 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -13631,6 +13631,31 @@
     }
 }
 ,
+// src/gitHub/telerik/kendo-ui-core.js
+{
+    properties: {
+        examples: [
+            'https://demos.telerik.com/kendo-ui/timepicker/index'
+        ],
+        link: 'https://github.com/telerik/kendo-ui-core',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.kendo',
+                'window.kendo.Class'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.kendo.version'
+        ]
+    }
+}
+,
 // src/gitHub/ten1seven/what-input.js
 {
     properties: {
---
+++ src/gitHub/telerik/kendo-ui-core.js
@@ -0,0 +1,23 @@
+{
+    properties: {
+        examples: [
+            'https://demos.telerik.com/kendo-ui/timepicker/index'
+        ],
+        link: 'https://github.com/telerik/kendo-ui-core',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.kendo',
+                'window.kendo.Class'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.kendo.version'
+        ]
+    }
+}

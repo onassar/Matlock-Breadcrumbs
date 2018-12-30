e06aca4f9761b850cc753d66303c367bfccd3359
onassar <onassar@gmail.com>
2018-12-29 21:12:42 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -12509,6 +12509,26 @@
     }
 }
 ,
+// src/gitHub/scottcheng/cropit.js
+{
+    properties: {
+        examples: [
+            'http://scottcheng.github.io/cropit/'
+        ],
+        link: 'https://github.com/scottcheng/cropit',
+        name: 'CKEditor 5',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.fn.cropit'
+            ]
+        ]
+    }
+}
+,
 // src/gitHub/scottjehl/Respond.js
 {
     properties: {
---
+++ src/gitHub/scottcheng/cropit.js
@@ -0,0 +1,18 @@
+{
+    properties: {
+        examples: [
+            'http://scottcheng.github.io/cropit/'
+        ],
+        link: 'https://github.com/scottcheng/cropit',
+        name: 'CKEditor 5',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.fn.cropit'
+            ]
+        ]
+    }
+}

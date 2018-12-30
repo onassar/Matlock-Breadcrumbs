7565032f1a4b32ddbe96650c97426aa8f02c7b60
onassar <onassar@gmail.com>
2018-12-29 22:15:47 -0500

    Breadcrumb update (auto)

--- src/gitHub/Semantic-Org/Semantic-UI.js
+++ src/gitHub/Semantic-Org/Semantic-UI.js
@@ -8,13 +8,12 @@
         type: 'repository'
     },
     tests: {
-        references: [
-            [
-                'window.jQuery.fn.site',
-                'window.jQuery.fn.site.settings',
-                'window.jQuery.fn.form',
-                'window.jQuery.fn.form.settings'
+        patterns: {
+            page: [
+                [
+                    /<link[^>]+href="[^"]+semantic(?:\.min)?\.css"/i
+                ]
             ]
-        ]
+        }
     }
 }

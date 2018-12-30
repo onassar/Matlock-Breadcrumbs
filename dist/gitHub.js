0b33bf223cfe2c37fc71a62ef7e666bf327c536f
onassar <onassar@gmail.com>
2018-12-29 21:17:32 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -1066,6 +1066,36 @@
     }
 }
 ,
+// src/gitHub/PopupMaker/Popup-Maker.js
+{
+    properties: {
+        examples: [
+            'https://www.harbr.com/'
+        ],
+        licenses: {
+            commercial: {
+                paid: true
+            }
+        },
+        link: 'https://github.com/PopupMaker/Popup-Maker',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.fn.popmake',
+                'window.jQuery.fn.popmake.animations'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.jQuery.fn.popmake.version'
+        ]
+    }
+}
+,
 // src/gitHub/Postleaf/postleaf.js
 {
     properties: {
---
+++ src/gitHub/PopupMaker/Popup-Maker.js
@@ -0,0 +1,28 @@
+{
+    properties: {
+        examples: [
+            'https://www.harbr.com/'
+        ],
+        licenses: {
+            commercial: {
+                paid: true
+            }
+        },
+        link: 'https://github.com/PopupMaker/Popup-Maker',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.fn.popmake',
+                'window.jQuery.fn.popmake.animations'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.jQuery.fn.popmake.version'
+        ]
+    }
+}

b81f0e6e7e4f4997877a6ceda384ab6c03b5a75d
onassar <onassar@gmail.com>
2018-12-30 13:08:24 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -10627,6 +10627,26 @@
     }
 }
 ,
+// src/gitHub/mobify/mobifyjs.js
+{
+    properties: {
+        examples: [
+            'https://www.merlinspotions.com/'
+        ],
+        link: 'https://github.com/mobify/mobifyjs',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.Mobify',
+                'window.Mobify.disableTag'
+            ]
+        ]
+    }
+}
+,
 // src/gitHub/mobxjs/mobx.js
 {
     properties: {
---
+++ src/gitHub/mobify/mobifyjs.js
@@ -0,0 +1,18 @@
+{
+    properties: {
+        examples: [
+            'https://www.merlinspotions.com/'
+        ],
+        link: 'https://github.com/mobify/mobifyjs',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.Mobify',
+                'window.Mobify.disableTag'
+            ]
+        ]
+    }
+}

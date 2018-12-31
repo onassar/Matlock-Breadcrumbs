54f1ff448f1532701d4378e26537d4e7873f36dc
onassar <onassar@gmail.com>
2018-12-31 14:07:58 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -4051,6 +4051,33 @@
     }
 }
 ,
+// src/gitHub/chrishunt/favcount.js
+{
+    properties: {
+        examples: [
+            'https://forums.donejs.com/c/documentjs'
+        ],
+        link: 'https://github.com/chrishunt/favcount',
+        platform: 'GitHub',
+        tags: [
+            'favicon'
+        ]
+    },
+    tests: {
+        references: [
+            [
+                'window.Favcount',
+                'window.Favcount.prototype.set'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.Favcount.VERSION'
+        ]
+    }
+}
+,
 // src/gitHub/chriskempson/tomorrow-theme.js
 {
     properties: {
---
+++ src/gitHub/chrishunt/favcount.js
@@ -0,0 +1,25 @@
+{
+    properties: {
+        examples: [
+            'https://forums.donejs.com/c/documentjs'
+        ],
+        link: 'https://github.com/chrishunt/favcount',
+        platform: 'GitHub',
+        tags: [
+            'favicon'
+        ]
+    },
+    tests: {
+        references: [
+            [
+                'window.Favcount',
+                'window.Favcount.prototype.set'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.Favcount.VERSION'
+        ]
+    }
+}

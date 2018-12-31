ba3df02b86fe8eafd70433de5a6184522db95e3b
onassar <onassar@gmail.com>
2018-12-31 17:38:31 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -5856,6 +5856,25 @@
     }
 }
 ,
+// src/gitHub/fat/bean.js
+{
+    properties: {
+        link: 'https://github.com/fat/bean',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.bean',
+                'window.bean.Event',
+                'window.bean.off',
+                'window.bean.on'
+            ]
+        ]
+    }
+}
+,
 // src/gitHub/fdansv/marginotes.js
 {
     properties: {
---
+++ src/gitHub/fat/bean.js
@@ -0,0 +1,17 @@
+{
+    properties: {
+        link: 'https://github.com/fat/bean',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.bean',
+                'window.bean.Event',
+                'window.bean.off',
+                'window.bean.on'
+            ]
+        ]
+    }
+}

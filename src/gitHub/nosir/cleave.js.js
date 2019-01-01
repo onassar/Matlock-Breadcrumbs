da768ac87a6c7b18e5ff44c9d3d298503254fdd6
onassar <onassar@gmail.com>
2018-12-31 19:35:33 -0500

    Breadcrumb update (auto)

--- src/gitHub/nosir/cleave.js.js
+++ src/gitHub/nosir/cleave.js.js
@@ -1,12 +1,14 @@
 {
     properties: {
+        examples: [
+            'https://nosir.github.io/cleave.js/',
+            'https://www.principles.com/#Part-1'
+        ],
         alternatives: [
             'https://github.com/stripe/jquery.payment'
         ],
-        examples: [
-            'https://nosir.github.io/cleave.js/'
-        ],
         link: 'https://github.com/nosir/cleave.js',
+        name: 'Cleave.js',
         platform: 'GitHub',
         type: 'repository'
     },

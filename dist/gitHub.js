1a7633399b05ae85acd24ff57108460741a765d4
onassar <onassar@gmail.com>
2019-01-01 17:13:28 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -3354,6 +3354,25 @@
     }
 }
 ,
+// src/gitHub/bas2k/jquery.appear.js
+{
+    properties: {
+        link: 'https://github.com/bas2k/jquery.appear',
+        name: 'jQuery Appear'
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.fn.appear',
+                'window.jQuery.fn.appear.checkAll',
+                'window.jQuery.fn.appear.checks'
+            ]
+        ]
+    }
+}
+,
 // src/gitHub/basecamp/trix.js
 {
     properties: {
---
+++ src/gitHub/bas2k/jquery.appear.js
@@ -0,0 +1,17 @@
+{
+    properties: {
+        link: 'https://github.com/bas2k/jquery.appear',
+        name: 'jQuery Appear'
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.fn.appear',
+                'window.jQuery.fn.appear.checkAll',
+                'window.jQuery.fn.appear.checks'
+            ]
+        ]
+    }
+}

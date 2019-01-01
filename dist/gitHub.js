5672266174399ba54d80d066f856672bdb7679b3
onassar <onassar@gmail.com>
2019-01-01 03:48:59 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -1,4 +1,29 @@
 [
+// src/gitHub/1000hz/bootstrap-validator.js
+{
+    properties: {
+        examples: [
+            'http://1000hz.github.io/bootstrap-validator/',
+            'https://account.kansascity.com/olive-auth'
+        ],
+        link: 'https://github.com/1000hz/bootstrap-validator',
+        name: 'Bootstrap Validator',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.fn.validator',
+                'window.jQuery.fn.validator.noConflict',
+                'window.jQuery.fn.validator.Constructor',
+                'window.jQuery.fn.validator.Constructor.DEFAULTS',
+                'window.jQuery.fn.validator.Constructor.VALIDATORS',
+            ]
+        ]
+    }
+}
+,
 // src/gitHub/10bestdesign/jqvmap.js
 {
     properties: {
---
+++ src/gitHub/1000hz/bootstrap-validator.js
@@ -0,0 +1,23 @@
+{
+    properties: {
+        examples: [
+            'http://1000hz.github.io/bootstrap-validator/',
+            'https://account.kansascity.com/olive-auth'
+        ],
+        link: 'https://github.com/1000hz/bootstrap-validator',
+        name: 'Bootstrap Validator',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.fn.validator',
+                'window.jQuery.fn.validator.noConflict',
+                'window.jQuery.fn.validator.Constructor',
+                'window.jQuery.fn.validator.Constructor.DEFAULTS',
+                'window.jQuery.fn.validator.Constructor.VALIDATORS',
+            ]
+        ]
+    }
+}

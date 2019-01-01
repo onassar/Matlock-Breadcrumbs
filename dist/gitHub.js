190be2f23048673f317d32c1c7e2e995a890e36f
onassar <onassar@gmail.com>
2019-01-01 17:15:23 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -12850,6 +12850,26 @@
     }
 }
 ,
+// src/gitHub/rendro/easy-pie-chart.js
+{
+    properties: {
+        examples: [
+            'http://rendro.github.io/easy-pie-chart/'
+        ],
+        link: 'https://github.com/rendro/easy-pie-chart',
+        name: 'Easy pie chart',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.fn.easyPieChart'
+            ]
+        ]
+    }
+}
+,
 // src/gitHub/rgrove/lazyload.js
 {
     properties: {
---
+++ src/gitHub/rendro/easy-pie-chart.js
@@ -0,0 +1,18 @@
+{
+    properties: {
+        examples: [
+            'http://rendro.github.io/easy-pie-chart/'
+        ],
+        link: 'https://github.com/rendro/easy-pie-chart',
+        name: 'Easy pie chart',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.fn.easyPieChart'
+            ]
+        ]
+    }
+}

ea20893b513e697a8b94d05e807c7c8396a4015d
onassar <onassar@gmail.com>
2018-12-31 20:30:32 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -15303,7 +15303,11 @@
 // src/gitHub/wycats/handlebars.js.js
 {
     properties: {
+        examples: [
+            'https://deadline.com/2018/08/roma-alfonso-cuaron-venice-film-festival-netflix-interview-1202455061/'
+        ],
         link: 'https://github.com/wycats/handlebars.js',
+        name: 'Handlebars.js',
         platform: 'GitHub',
         type: 'repository'
     },

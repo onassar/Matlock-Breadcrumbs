df92afe1f0231b21ef7f49434084ce104789f082
onassar <onassar@gmail.com>
2019-01-01 17:28:29 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -3053,6 +3053,7 @@
             'https://www.snakesandlattes.com/location/college/'
         ],
         link: 'https://github.com/ariutta/svg-pan-zoom',
+        name: 'SVG Pan & Zoom',
         platform: 'GitHub',
         type: 'repository'
     },
--- src/gitHub/ariutta/svg-pan-zoom.js
+++ src/gitHub/ariutta/svg-pan-zoom.js
@@ -4,6 +4,7 @@
             'https://www.snakesandlattes.com/location/college/'
         ],
         link: 'https://github.com/ariutta/svg-pan-zoom',
+        name: 'SVG Pan & Zoom',
         platform: 'GitHub',
         type: 'repository'
     },

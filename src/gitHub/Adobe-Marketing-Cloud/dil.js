45aad7fc503ad802755231ead0807ac484293f16
onassar <onassar@gmail.com>
2019-01-01 16:21:15 -0500

    Breadcrumb update (auto)

--- src/gitHub/Adobe-Marketing-Cloud/dil.js
+++ src/gitHub/Adobe-Marketing-Cloud/dil.js
@@ -1,9 +1,11 @@
 {
     properties: {
         examples: [
-            'https://www.kansascity.com/'
+            'https://www.kansascity.com/',
+            'https://www.loblaws.ca/'
         ],
         link: 'https://github.com/Adobe-Marketing-Cloud/dil',
+        name: 'Adobe Data Integration Library',
         platform: 'GitHub',
         type: 'repository'
     },

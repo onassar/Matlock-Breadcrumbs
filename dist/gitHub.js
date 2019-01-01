0b72c3ab8ba345a0fda0d2dc1c1c3fbef48e9445
onassar <onassar@gmail.com>
2019-01-01 17:10:00 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -2594,6 +2594,26 @@
     }
 }
 ,
+// src/gitHub/alvarotrigo/multiscroll.js.js
+{
+    properties: {
+        examples: [
+            'https://alvarotrigo.com/multiScroll/'
+        ],
+        link: 'https://github.com/alvarotrigo/multiscroll.js',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.fn.multiscroll',
+                'window.jQuery.fn.multiscroll.moveSectionDown'
+            ]
+        ]
+    }
+}
+,
 // src/gitHub/ampproject/amphtml.js
 {
     properties: {
---
+++ src/gitHub/alvarotrigo/multiscroll.js.js
@@ -0,0 +1,18 @@
+{
+    properties: {
+        examples: [
+            'https://alvarotrigo.com/multiScroll/'
+        ],
+        link: 'https://github.com/alvarotrigo/multiscroll.js',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.fn.multiscroll',
+                'window.jQuery.fn.multiscroll.moveSectionDown'
+            ]
+        ]
+    }
+}

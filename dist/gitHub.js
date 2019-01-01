c1466e02664081c1545f47b8252d06a1aa922928
onassar <onassar@gmail.com>
2019-01-01 17:45:18 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -4827,6 +4827,31 @@
     }
 }
 ,
+// src/gitHub/danvk/dygraphs.js
+{
+    properties: {
+        examples: [
+            'http://dygraphs.com/'
+        ],
+        link: 'https://github.com/danvk/dygraphs',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.Dygraph',
+                'window.Dygraph.PLUGINS'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.Dygraph.VERSION'
+        ]
+    }
+}
+,
 // src/gitHub/darklow/social-share-kit.js
 {
     properties: {
---
+++ src/gitHub/danvk/dygraphs.js
@@ -0,0 +1,23 @@
+{
+    properties: {
+        examples: [
+            'http://dygraphs.com/'
+        ],
+        link: 'https://github.com/danvk/dygraphs',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.Dygraph',
+                'window.Dygraph.PLUGINS'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.Dygraph.VERSION'
+        ]
+    }
+}

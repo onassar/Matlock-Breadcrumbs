63f533418b5eb447c22e846ccf156be72e955b5f
onassar <onassar@gmail.com>
2019-01-02 04:02:50 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -4067,7 +4067,11 @@
         'https://github.com/php/php-src'
     ],
     properties: {
+        examples: [
+            'https://www.tourismwinnipeg.com/'
+        ],
         link: 'https://github.com/cakephp/cakephp',
+        name: 'CakePHP',
         platform: 'GitHub',
         type: 'repository'
     },

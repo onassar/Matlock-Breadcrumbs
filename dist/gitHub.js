345274e5895b00966a20e7ce19674187b20979d7
onassar <onassar@gmail.com>
2019-01-02 04:47:19 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -1867,6 +1867,32 @@
     }
 }
 ,
+// src/gitHub/Wisembly/basil.js.js
+{
+    properties: {
+        examples: [
+            'https://www.vrbo.com/vacation-rentals/usa/florida/central-disney-orlando/orlando'
+        ],
+        link: 'https://github.com/Wisembly/basil.js',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.Basil',
+                'window.Basil.create',
+                'window.Basil.utils'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.Basil.version'
+        ]
+    }
+}
+,
 // src/gitHub/WordPress/WordPress.js
 {
     dependencies: [
---
+++ src/gitHub/Wisembly/basil.js.js
@@ -0,0 +1,24 @@
+{
+    properties: {
+        examples: [
+            'https://www.vrbo.com/vacation-rentals/usa/florida/central-disney-orlando/orlando'
+        ],
+        link: 'https://github.com/Wisembly/basil.js',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.Basil',
+                'window.Basil.create',
+                'window.Basil.utils'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.Basil.version'
+        ]
+    }
+}

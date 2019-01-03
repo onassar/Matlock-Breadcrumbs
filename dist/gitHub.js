f775e75c507047aec14f17ad6bcb636e7ffb5217
onassar <onassar@gmail.com>
2019-01-03 18:45:08 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -12241,6 +12241,34 @@
     }
 }
 ,
+// src/gitHub/necolas/normalize.css.js
+{
+    properties: {
+        examples: [
+            'https://milligram.io/'
+        ],
+        link: 'https://github.com/necolas/normalize.css',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        shortcuts: [
+            {
+                key: 'query.stylesheet',
+                name: 'normalize'
+            }
+        ]
+    },
+    versions: {
+        shortcuts: [
+            {
+                key: 'query.stylesheet',
+                name: 'normalize'
+            }
+        ]
+    }
+}
+,
 // src/gitHub/neilj/Squire.js
 {
     properties: {
---
+++ src/gitHub/necolas/normalize.css.js
@@ -0,0 +1,26 @@
+{
+    properties: {
+        examples: [
+            'https://milligram.io/'
+        ],
+        link: 'https://github.com/necolas/normalize.css',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        shortcuts: [
+            {
+                key: 'query.stylesheet',
+                name: 'normalize'
+            }
+        ]
+    },
+    versions: {
+        shortcuts: [
+            {
+                key: 'query.stylesheet',
+                name: 'normalize'
+            }
+        ]
+    }
+}

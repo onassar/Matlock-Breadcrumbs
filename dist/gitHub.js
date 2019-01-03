fdb2d6cc88522597a822c900f08f5530a37312e3
onassar <onassar@gmail.com>
2019-01-03 12:03:58 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -5984,6 +5984,30 @@
     }
 }
 ,
+// src/gitHub/epochjs/epoch.js
+{
+    dependencies: [
+        'https://github.com/d3/d3'
+    ],
+    properties: {
+        examples: [
+            'http://epochjs.github.io/epoch/'
+        ],
+        link: 'https://github.com/epochjs/epoch',
+        name: 'Epoch',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.Epoch',
+                'window.Epoch.Chart'
+            ]
+        ]
+    }
+}
+,
 // src/gitHub/eranhirs/jsTag.js
 {
     properties: {
---
+++ src/gitHub/epochjs/epoch.js
@@ -0,0 +1,22 @@
+{
+    dependencies: [
+        'https://github.com/d3/d3'
+    ],
+    properties: {
+        examples: [
+            'http://epochjs.github.io/epoch/'
+        ],
+        link: 'https://github.com/epochjs/epoch',
+        name: 'Epoch',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.Epoch',
+                'window.Epoch.Chart'
+            ]
+        ]
+    }
+}

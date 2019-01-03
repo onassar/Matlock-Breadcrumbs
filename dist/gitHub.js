1405952a8af7fb204574e0247fe6d0f38fddddb6
onassar <onassar@gmail.com>
2019-01-02 19:39:13 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -10238,6 +10238,26 @@
     }
 }
 ,
+// src/gitHub/lcdsantos/jquery-drawsvg.js
+{
+    properties: {
+        examples: [
+            'http://leocs.me/jquery-drawsvg/'
+        ],
+        link: 'https://github.com/lcdsantos/jquery-drawsvg',
+        name: 'jQuery DrawSVG',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.fn.drawsvg'
+            ]
+        ]
+    }
+}
+,
 // src/gitHub/leongersen/noUiSlider.js
 {
     properties: {
---
+++ src/gitHub/lcdsantos/jquery-drawsvg.js
@@ -0,0 +1,18 @@
+{
+    properties: {
+        examples: [
+            'http://leocs.me/jquery-drawsvg/'
+        ],
+        link: 'https://github.com/lcdsantos/jquery-drawsvg',
+        name: 'jQuery DrawSVG',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.fn.drawsvg'
+            ]
+        ]
+    }
+}

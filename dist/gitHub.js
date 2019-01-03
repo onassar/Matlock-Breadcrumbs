02437e81e3511528c778e03352769e584e634539
onassar <onassar@gmail.com>
2019-01-03 18:26:50 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -10438,6 +10438,32 @@
     }
 }
 ,
+// src/gitHub/less/less.js.js
+{
+    properties: {
+        examples: [
+            'https://semantic-ui.com/'
+        ],
+        link: 'https://github.com/less/less.js',
+        name: 'Less.js',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.less',
+                'window.less.AbstractFileManager'
+            ]
+        ]
+    },
+    versions: {
+        statements: [
+            'window.less.version.join(\'.\')'
+        ]
+    }
+}
+,
 // src/gitHub/liabru/jquery-match-height.js
 {
     properties: {
---
+++ src/gitHub/less/less.js.js
@@ -0,0 +1,24 @@
+{
+    properties: {
+        examples: [
+            'https://semantic-ui.com/'
+        ],
+        link: 'https://github.com/less/less.js',
+        name: 'Less.js',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.less',
+                'window.less.AbstractFileManager'
+            ]
+        ]
+    },
+    versions: {
+        statements: [
+            'window.less.version.join(\'.\')'
+        ]
+    }
+}

8405b66d83d146f412c923c4c0cf8943633c1561
onassar <onassar@gmail.com>
2019-01-02 19:48:53 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -11292,6 +11292,26 @@
     }
 }
 ,
+// src/gitHub/medialize/sass.js.js
+{
+    properties: {
+        examples: [
+            'http://sass.js.org/'
+        ],
+        link: 'https://github.com/medialize/sass.js',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.Sass',
+                'window.Sass.prototype.destroy'
+            ]
+        ]
+    }
+}
+,
 // src/gitHub/meltingice/CamanJS.js
 {
     properties: {
---
+++ src/gitHub/medialize/sass.js.js
@@ -0,0 +1,18 @@
+{
+    properties: {
+        examples: [
+            'http://sass.js.org/'
+        ],
+        link: 'https://github.com/medialize/sass.js',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.Sass',
+                'window.Sass.prototype.destroy'
+            ]
+        ]
+    }
+}

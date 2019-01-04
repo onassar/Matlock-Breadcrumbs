fc7e374d1c50e7cf69994e65b88be28c7e85245f
onassar <onassar@gmail.com>
2019-01-04 11:58:51 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -4611,6 +4611,28 @@
     }
 }
 ,
+// src/gitHub/carolineschnapp/currencies.js
+{
+    properties: {
+        examples: [
+            'https://missionworkshop.com/collections/backpack-accessories/products/internal-tool-roll-backpack-accessory'
+        ],
+        link: 'https://github.com/carolineschnapp/currencies',
+        name: 'jQuery.currencies.js',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.Currency',
+                'window.Currency.cookie',
+                'window.Currency.cookie.configuration',
+            ]
+        ]
+    }
+}
+,
 // src/gitHub/cbopp-art/lightcase.js
 {
     properties: {
---
+++ src/gitHub/carolineschnapp/currencies.js
@@ -0,0 +1,20 @@
+{
+    properties: {
+        examples: [
+            'https://missionworkshop.com/collections/backpack-accessories/products/internal-tool-roll-backpack-accessory'
+        ],
+        link: 'https://github.com/carolineschnapp/currencies',
+        name: 'jQuery.currencies.js',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.Currency',
+                'window.Currency.cookie',
+                'window.Currency.cookie.configuration',
+            ]
+        ]
+    }
+}

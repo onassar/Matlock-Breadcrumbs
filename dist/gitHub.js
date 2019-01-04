8bb4e906a13d38e1febed233668d2bf89c2e340e
onassar <onassar@gmail.com>
2019-01-04 00:26:52 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -1583,6 +1583,29 @@
     }
 }
 ,
+// src/gitHub/StephanWagner/jBox.js
+{
+    properties: {
+        examples: [
+            'https://stephanwagner.me/jBox'
+        ],
+        link: 'https://github.com/StephanWagner/jBox',
+        name: 'jBox',
+        platform: 'GitHub',
+        tags: [
+            'popup'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.fn.jBox'
+            ]
+        ]
+    }
+}
+,
 // src/gitHub/Stuk/jszip.js
 {
     uses: [
---
+++ src/gitHub/StephanWagner/jBox.js
@@ -0,0 +1,21 @@
+{
+    properties: {
+        examples: [
+            'https://stephanwagner.me/jBox'
+        ],
+        link: 'https://github.com/StephanWagner/jBox',
+        name: 'jBox',
+        platform: 'GitHub',
+        tags: [
+            'popup'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.fn.jBox'
+            ]
+        ]
+    }
+}

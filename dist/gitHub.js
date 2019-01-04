3960b0711718d60b3193f3a13b288bdd04bd9ced
onassar <onassar@gmail.com>
2019-01-04 18:09:03 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -1262,6 +1262,27 @@
     }
 }
 ,
+// src/gitHub/Ortus-Solutions/ContentBox.js
+{
+    properties: {
+        examples: [
+            'https://www.contentboxcms.org/'
+        ],
+        link: 'https://github.com/Ortus-Solutions/ContentBox',
+        name: 'ContentBox',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        shortcuts: [
+            {
+                key: 'query.meta.generator',
+                generator: 'ContentBox'
+            }
+        ]
+    }
+}
+,
 // src/gitHub/OwlCarousel2/OwlCarousel2.js
 {
     properties: {
---
+++ src/gitHub/Ortus-Solutions/ContentBox.js
@@ -0,0 +1,19 @@
+{
+    properties: {
+        examples: [
+            'https://www.contentboxcms.org/'
+        ],
+        link: 'https://github.com/Ortus-Solutions/ContentBox',
+        name: 'ContentBox',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        shortcuts: [
+            {
+                key: 'query.meta.generator',
+                generator: 'ContentBox'
+            }
+        ]
+    }
+}

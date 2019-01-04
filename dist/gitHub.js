455f46fcff5ef7f4c9093bba6aaefd6f114df941
onassar <onassar@gmail.com>
2019-01-03 23:50:47 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -355,6 +355,23 @@
     }
 }
 ,
+// src/gitHub/Codeinwp/Nivo-Slider-jQuery.js
+{
+    properties: {
+        link: 'https://github.com/Codeinwp/Nivo-Slider-jQuery',
+        name: 'Nivo Slider jQuery Plugin',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.fn.nivoSlider'
+            ]
+        ]
+    }
+}
+,
 // src/gitHub/ComputerWolf/SlickNav.js
 {
     properties: {
---
+++ src/gitHub/Codeinwp/Nivo-Slider-jQuery.js
@@ -0,0 +1,15 @@
+{
+    properties: {
+        link: 'https://github.com/Codeinwp/Nivo-Slider-jQuery',
+        name: 'Nivo Slider jQuery Plugin',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.fn.nivoSlider'
+            ]
+        ]
+    }
+}

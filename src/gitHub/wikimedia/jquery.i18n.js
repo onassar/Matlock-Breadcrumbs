a36126fbf6561dd93f208c4334941c73d8c6feeb
onassar <onassar@gmail.com>
2019-01-04 01:34:46 -0500

    Breadcrumb update (auto)

---
+++ src/gitHub/wikimedia/jquery.i18n.js
@@ -0,0 +1,21 @@
+{
+    properties: {
+        donate: {
+            link: 'https://donate.wikimedia.org/wiki/Ways_to_Give'
+        },
+        examples: [
+            'https://thottingal.in/projects/js/jquery.i18n/demo/'
+        ],
+        link: 'https://github.com/jquery.i18n',
+        name: 'jQuery i18n',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.fn.i18n'
+            ]
+        ]
+    }
+}

5a4d2bdf55caad7c5f959643c585ed5f18254683
onassar <onassar@gmail.com>
2019-01-03 23:40:49 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -13268,6 +13268,26 @@
     }
 }
 ,
+// src/gitHub/oriongunning/gridder.js
+{
+    properties: {
+        examples: [
+            'http://oriongunning.github.io/gridder/'
+        ],
+        link: 'https://github.com/oriongunning/gridder',
+        name: 'jQuery Gridder',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'jQuery.fn.gridderExpander'
+            ]
+        ]
+    }
+}
+,
 // src/gitHub/oyvindkinsey/easyXDM.js
 {
     properties: {
---
+++ src/gitHub/oriongunning/gridder.js
@@ -0,0 +1,18 @@
+{
+    properties: {
+        examples: [
+            'http://oriongunning.github.io/gridder/'
+        ],
+        link: 'https://github.com/oriongunning/gridder',
+        name: 'jQuery Gridder',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'jQuery.fn.gridderExpander'
+            ]
+        ]
+    }
+}

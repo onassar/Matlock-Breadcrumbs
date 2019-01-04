8c4289c19d505a3f0548bf9e427f187c37a90f2c
onassar <onassar@gmail.com>
2019-01-03 23:04:05 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -10072,6 +10072,10 @@
     tests: {
         references: [
             [
+                'window.Vel',
+                'window.Vel.Easings'
+            ],
+            [
                 'window.jQuery.fn.velocity'
             ],
             [
@@ -10081,6 +10085,7 @@
     },
     versions: {
         statements: [
+            'window.Vel.version.major + \'.\' + window.Vel.version.minor + \'.\' + window.Vel.version.patch',
             'window.jQuery.fn.velocity.version.major + \'.\' + window.jQuery.fn.velocity.version.minor + \'.\' + window.jQuery.fn.velocity.version.patch',
             'window.jQuery.Velocity.version.major + \'.\' + window.jQuery.Velocity.version.minor + \'.\' + window.jQuery.Velocity.version.patch'
         ]
--- src/gitHub/julianshapiro/velocity.js
+++ src/gitHub/julianshapiro/velocity.js
@@ -15,6 +15,10 @@
     tests: {
         references: [
             [
+                'window.Vel',
+                'window.Vel.Easings'
+            ],
+            [
                 'window.jQuery.fn.velocity'
             ],
             [
@@ -24,6 +28,7 @@
     },
     versions: {
         statements: [
+            'window.Vel.version.major + \'.\' + window.Vel.version.minor + \'.\' + window.Vel.version.patch',
             'window.jQuery.fn.velocity.version.major + \'.\' + window.jQuery.fn.velocity.version.minor + \'.\' + window.jQuery.fn.velocity.version.patch',
             'window.jQuery.Velocity.version.major + \'.\' + window.jQuery.Velocity.version.minor + \'.\' + window.jQuery.Velocity.version.patch'
         ]

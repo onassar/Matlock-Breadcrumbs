11630e6e87238dad1725dd825200b84821907a8d
onassar <onassar@gmail.com>
2019-01-04 01:23:51 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -575,6 +575,35 @@
     }
 }
 ,
+// src/gitHub/FancyGrid/FancyGrid.js
+{
+    properties: {
+        examples: [
+            'https://fancygrid.com/gallery/'
+        ],
+        link: 'https://github.com/FancyGrid/FancyGrid',
+        platform: 'GitHub',
+        tags: [
+            'layout'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.Fancy',
+                'window.Fancy.$',
+                'window.Fancy.select'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.Fancy.version'
+        ]
+    }
+}
+,
 // src/gitHub/FezVrasta/popper.js.js
 {
     properties: {
---
+++ src/gitHub/FancyGrid/FancyGrid.js
@@ -0,0 +1,27 @@
+{
+    properties: {
+        examples: [
+            'https://fancygrid.com/gallery/'
+        ],
+        link: 'https://github.com/FancyGrid/FancyGrid',
+        platform: 'GitHub',
+        tags: [
+            'layout'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.Fancy',
+                'window.Fancy.$',
+                'window.Fancy.select'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.Fancy.version'
+        ]
+    }
+}

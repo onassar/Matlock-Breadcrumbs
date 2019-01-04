8af6cbe03298ec3e3c313ebe0990a686b00ad9c6
onassar <onassar@gmail.com>
2019-01-04 00:23:13 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -16727,6 +16727,31 @@
     }
 }
 ,
+// src/gitHub/vast-engineering/jquery-popup-overlay.js
+{
+    properties: {
+        examples: [
+            'https://dev.vast.com/jquery-popup-overlay/'
+        ],
+        link: 'https://github.com/vast-engineering/jquery-popup-overlay',
+        name: 'jQuery Popup Overlay',
+        platform: 'GitHub',
+        tags: [
+            'popup'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.fn.popup',
+                'window.jQuery.fn.popup.defaults',
+                'window.jQuery.fn.popup.defaults.pagecontainer'
+            ]
+        ]
+    }
+}
+,
 // src/gitHub/vdw/Tabslet.js
 {
     properties: {
---
+++ src/gitHub/vast-engineering/jquery-popup-overlay.js
@@ -0,0 +1,23 @@
+{
+    properties: {
+        examples: [
+            'https://dev.vast.com/jquery-popup-overlay/'
+        ],
+        link: 'https://github.com/vast-engineering/jquery-popup-overlay',
+        name: 'jQuery Popup Overlay',
+        platform: 'GitHub',
+        tags: [
+            'popup'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.fn.popup',
+                'window.jQuery.fn.popup.defaults',
+                'window.jQuery.fn.popup.defaults.pagecontainer'
+            ]
+        ]
+    }
+}

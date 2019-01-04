e595afd2c23b0fbaf9e0bb34b5b9fc58af48c6cc
onassar <onassar@gmail.com>
2019-01-04 02:15:56 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -17537,6 +17537,27 @@
     }
 }
 ,
+// src/gitHub/vitalets/x-editable.js
+{
+    properties: {
+        examples: [
+            'http://vitalets.github.io/x-editable/demo.html'
+        ],
+        link: 'https://github.com/vitalets/x-editable',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.fn.editable',
+                'window.jQuery.fn.editableContainer',
+                'window.jQuery.fn.editableform'
+            ]
+        ]
+    }
+}
+,
 // src/gitHub/vitch/jScrollPane.js
 {
     properties: {
---
+++ src/gitHub/vitalets/x-editable.js
@@ -0,0 +1,19 @@
+{
+    properties: {
+        examples: [
+            'http://vitalets.github.io/x-editable/demo.html'
+        ],
+        link: 'https://github.com/vitalets/x-editable',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.fn.editable',
+                'window.jQuery.fn.editableContainer',
+                'window.jQuery.fn.editableform'
+            ]
+        ]
+    }
+}

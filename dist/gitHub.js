6a27048410795434fcbfd5ba462e15e22b2fa34d
onassar <onassar@gmail.com>
2019-01-04 00:38:39 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -14485,6 +14485,29 @@
     }
 }
 ,
+// src/gitHub/rmm5t/jquery-timeago.js
+{
+    properties: {
+        examples: [
+            'http://timeago.yarp.com/'
+        ],
+        link: 'https://github.com/rmm5t/jquery-timeago',
+        name: 'jQuery Timeago',
+        platform: 'GitHub',
+        tags: [
+            'datetime'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.timeago'
+            ]
+        ]
+    }
+}
+,
 // src/gitHub/rnmp/salvattore.js
 {
     properties: {
---
+++ src/gitHub/rmm5t/jquery-timeago.js
@@ -0,0 +1,21 @@
+{
+    properties: {
+        examples: [
+            'http://timeago.yarp.com/'
+        ],
+        link: 'https://github.com/rmm5t/jquery-timeago',
+        name: 'jQuery Timeago',
+        platform: 'GitHub',
+        tags: [
+            'datetime'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.timeago'
+            ]
+        ]
+    }
+}

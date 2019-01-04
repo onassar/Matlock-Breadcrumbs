70392819f2ee3b1b26fdcf30aff52775932530b4
onassar <onassar@gmail.com>
2019-01-03 19:33:29 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -5484,6 +5484,38 @@
     }
 }
 ,
+// src/gitHub/docpad/docpad.js
+{
+    dependencies: [
+        'https://github.com/nodejs/node'
+    ],
+    properties: {
+        examples: [
+            'https://semantic-ui.com/'
+        ],
+        link: 'https://github.com/docpad/docpad',
+        name: 'DocPad',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        shortcuts: [
+            {
+                key: 'query.meta.generator',
+                generator: 'DocPad'
+            }
+        ]
+    },
+    versions: {
+        shortcuts: [
+            {
+                key: 'query.meta.generator',
+                generator: 'DocPad'
+            }
+        ]
+    }
+}
+,
 // src/gitHub/doesdev/jquery-video-lightning.js
 {
     properties: {
---
+++ src/gitHub/docpad/docpad.js
@@ -0,0 +1,30 @@
+{
+    dependencies: [
+        'https://github.com/nodejs/node'
+    ],
+    properties: {
+        examples: [
+            'https://semantic-ui.com/'
+        ],
+        link: 'https://github.com/docpad/docpad',
+        name: 'DocPad',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        shortcuts: [
+            {
+                key: 'query.meta.generator',
+                generator: 'DocPad'
+            }
+        ]
+    },
+    versions: {
+        shortcuts: [
+            {
+                key: 'query.meta.generator',
+                generator: 'DocPad'
+            }
+        ]
+    }
+}

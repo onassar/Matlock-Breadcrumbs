da84358050386d9501e08a20094cf67f8293b99b
onassar <onassar@gmail.com>
2019-01-03 19:19:31 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -12158,6 +12158,43 @@
     }
 }
 ,
+// src/gitHub/muicss/mui.js
+{
+    properties: {
+        examples: [
+            'https://www.muicss.com/'
+        ],
+        link: 'https://github.com/muicss/mui',
+        name: 'MUI: Material Design CSS Framework',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        shortcuts: [
+            {
+                key: 'query.script',
+                name: 'mui'
+            },
+            {
+                key: 'query.stylesheet',
+                name: 'mui'
+            }
+        ]
+    },
+    versions: {
+        shortcuts: [
+            {
+                key: 'query.script',
+                name: 'mui'
+            },
+            {
+                key: 'query.stylesheet',
+                name: 'mui'
+            }
+        ]
+    }
+}
+,
 // src/gitHub/nanostudio-org/nanogallery2.js
 {
     properties: {
---
+++ src/gitHub/muicss/mui.js
@@ -0,0 +1,35 @@
+{
+    properties: {
+        examples: [
+            'https://www.muicss.com/'
+        ],
+        link: 'https://github.com/muicss/mui',
+        name: 'MUI: Material Design CSS Framework',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        shortcuts: [
+            {
+                key: 'query.script',
+                name: 'mui'
+            },
+            {
+                key: 'query.stylesheet',
+                name: 'mui'
+            }
+        ]
+    },
+    versions: {
+        shortcuts: [
+            {
+                key: 'query.script',
+                name: 'mui'
+            },
+            {
+                key: 'query.stylesheet',
+                name: 'mui'
+            }
+        ]
+    }
+}

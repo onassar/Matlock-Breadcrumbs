87b8dfa887ac037094594028723828bd0084ea44
onassar <onassar@gmail.com>
2019-01-04 00:58:11 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -11449,14 +11449,45 @@
     }
 }
 ,
-// src/gitHub/ludo copy/jqGrid.js
-
+// src/gitHub/ludo/jqGrid.js
+{
+    properties: {
+        examples: [
+            'http://www.guriddo.net/demo/guriddojs/loading_data/rest_million/index.html'
+        ],
+        link: 'https://github.com/tonytomov/jqGrid',
+        platform: 'GitHub',
+        tags: [
+            'tables'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.fn.jqGrid'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.jQuery.jgrid.version'
+        ]
+    }
+}
 ,
 // src/gitHub/ludo/jquery-treetable.js
 {
     properties: {
+        examples: [
+            'http://ludo.cubicphuse.nl/jquery-treetable/'
+        ],
         link: 'https://github.com/ludo/jquery-treetable',
+        name: 'jQuery treetable',
         platform: 'GitHub',
+        tags: [
+            'tables'
+        ],
         type: 'repository'
     },
     tests: {
--- src/gitHub/ludo/jquery-treetable.js
+++ src/gitHub/ludo/jquery-treetable.js
@@ -1,7 +1,14 @@
 {
     properties: {
+        examples: [
+            'http://ludo.cubicphuse.nl/jquery-treetable/'
+        ],
         link: 'https://github.com/ludo/jquery-treetable',
+        name: 'jQuery treetable',
         platform: 'GitHub',
+        tags: [
+            'tables'
+        ],
         type: 'repository'
     },
     tests: {

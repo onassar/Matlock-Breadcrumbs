178e84476a925740d0f17f822e8ffb01db16a833
onassar <onassar@gmail.com>
2019-01-04 17:55:28 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -331,6 +331,35 @@
     }
 }
 ,
+// src/gitHub/Chevereto/Chevereto-Free.js
+{
+    properties: {
+        examples: [
+            'https://demo.chevereto.com/'
+        ],
+        link: 'https://github.com/Chevereto/Chevereto-Free',
+        name: 'Chevereto Free',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        shortcuts: [
+            {
+                key: 'query.meta.generator',
+                generator: 'Chevereto'
+            }
+        ]
+    },
+    versions: {
+        shortcuts: [
+            {
+                key: 'query.meta.generator',
+                generator: 'Chevereto'
+            }
+        ]
+    }
+}
+,
 // src/gitHub/CodeSeven/toastr.js
 {
     properties: {
---
+++ src/gitHub/Chevereto/Chevereto-Free.js
@@ -0,0 +1,27 @@
+{
+    properties: {
+        examples: [
+            'https://demo.chevereto.com/'
+        ],
+        link: 'https://github.com/Chevereto/Chevereto-Free',
+        name: 'Chevereto Free',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        shortcuts: [
+            {
+                key: 'query.meta.generator',
+                generator: 'Chevereto'
+            }
+        ]
+    },
+    versions: {
+        shortcuts: [
+            {
+                key: 'query.meta.generator',
+                generator: 'Chevereto'
+            }
+        ]
+    }
+}

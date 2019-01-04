258e670cfd4457e2d6fa985b42f5426e239a6fb5
onassar <onassar@gmail.com>
2019-01-04 17:15:50 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -6294,6 +6294,35 @@
     }
 }
 ,
+// src/gitHub/eldy/awstats.js
+{
+    properties: {
+        examples: [
+            'https://www.nltechno.com/awstats/awstats.pl?config=destailleur.fr'
+        ],
+        link: 'https://github.com/eldy/awstats',
+        name: 'AWStats',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        shortcuts: [
+            {
+                key: 'query.meta.generator',
+                generator: 'AWStats'
+            }
+        ]
+    },
+    versions: {
+        shortcuts: [
+            {
+                key: 'query.meta.generator',
+                generator: 'AWStats'
+            }
+        ]
+    }
+}
+,
 // src/gitHub/eligrey/FileSaver.js
 {
     properties: {
---
+++ src/gitHub/eldy/awstats.js
@@ -0,0 +1,27 @@
+{
+    properties: {
+        examples: [
+            'https://www.nltechno.com/awstats/awstats.pl?config=destailleur.fr'
+        ],
+        link: 'https://github.com/eldy/awstats',
+        name: 'AWStats',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        shortcuts: [
+            {
+                key: 'query.meta.generator',
+                generator: 'AWStats'
+            }
+        ]
+    },
+    versions: {
+        shortcuts: [
+            {
+                key: 'query.meta.generator',
+                generator: 'AWStats'
+            }
+        ]
+    }
+}

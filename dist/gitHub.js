544e3b49f166f52a953a15311e2fe579acbdb129
onassar <onassar@gmail.com>
2019-01-04 02:10:32 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -15645,6 +15645,30 @@
     }
 }
 ,
+// src/gitHub/simsalabim/sisyphus.js
+{
+    properties: {
+        examples: [
+            'http://sisyphus-js.herokuapp.com/'
+        ],
+        link: 'https://github.com/simsalabim/sisyphus',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.Sisyphus'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.Sisyphus.version'
+        ]
+    }
+}
+,
 // src/gitHub/sindresorhus/query-string.js
 {
     properties: {
---
+++ src/gitHub/simsalabim/sisyphus.js
@@ -0,0 +1,22 @@
+{
+    properties: {
+        examples: [
+            'http://sisyphus-js.herokuapp.com/'
+        ],
+        link: 'https://github.com/simsalabim/sisyphus',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.Sisyphus'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.Sisyphus.version'
+        ]
+    }
+}

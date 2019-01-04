bbd7b4617c6a55fe80f51bd6130d0d38c2f43eb1
onassar <onassar@gmail.com>
2019-01-03 23:27:58 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -2762,6 +2762,28 @@
     }
 }
 ,
+// src/gitHub/amsul/pickadate.js.js
+{
+    properties: {
+        examples: [
+            'http://amsul.ca/pickadate.js/'
+        ],
+        link: 'https://github.com/amsul/pickadate.js',
+        platform: 'GitHub',
+        tags: [
+            'datetime-picker'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.fn.pickadate'
+            ]
+        ]
+    }
+}
+,
 // src/gitHub/andchir/jquery-video-extend.js
 {
     properties: {
---
+++ src/gitHub/amsul/pickadate.js.js
@@ -0,0 +1,20 @@
+{
+    properties: {
+        examples: [
+            'http://amsul.ca/pickadate.js/'
+        ],
+        link: 'https://github.com/amsul/pickadate.js',
+        platform: 'GitHub',
+        tags: [
+            'datetime-picker'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.fn.pickadate'
+            ]
+        ]
+    }
+}

a7d517b65d18420b9e84613e2f43ce60f553f708
onassar <onassar@gmail.com>
2019-01-04 18:18:20 -0500

    Breadcrumb update (auto)

--- src/gitHub/jonthornton/jquery-timepicker.js
+++ src/gitHub/jonthornton/jquery-timepicker.js
@@ -1,9 +1,11 @@
 {
     properties: {
         examples: [
-            'http://jonthornton.github.io/jquery-timepicker/'
+            'http://jonthornton.github.io/jquery-timepicker/',
+            'https://demo.matomo.org/index.php?module=CoreHome&action=index&idSite=62&period=day&date=yesterday#?idSite=62&period=day&date=yesterday&category=Dashboard_Dashboard&subcategory=1',
         ],
         link: 'https://github.com/jonthornton/jquery-timepicker',
+        name: 'jQuery Timepicker Plugin',
         platform: 'GitHub',
         type: 'repository'
     },

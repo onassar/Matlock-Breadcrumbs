33dc63ffa28ae0107858c0408561d6095563fe03
onassar <onassar@gmail.com>
2019-01-04 00:04:01 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -926,6 +926,26 @@
     }
 }
 ,
+// src/gitHub/LiranCohen/stickUp.js
+{
+    properties: {
+        examples: [
+            'http://lirancohen.github.io/stickUp/'
+        ],
+        link: 'https://github.com/LiranCohen/stickUp',
+        name: 'jQuery stickUp',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.fn.stickUp'
+            ]
+        ]
+    }
+}
+,
 // src/gitHub/Mathachew/jquery-autotab.js
 {
     properties: {
---
+++ src/gitHub/LiranCohen/stickUp.js
@@ -0,0 +1,18 @@
+{
+    properties: {
+        examples: [
+            'http://lirancohen.github.io/stickUp/'
+        ],
+        link: 'https://github.com/LiranCohen/stickUp',
+        name: 'jQuery stickUp',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.fn.stickUp'
+            ]
+        ]
+    }
+}

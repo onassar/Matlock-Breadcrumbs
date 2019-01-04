28408a0ece9bf086c6fa3105e6e5f342c8d6b9da
onassar <onassar@gmail.com>
2019-01-04 02:17:51 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -18236,6 +18236,31 @@
     }
 }
 ,
+// src/gitHub/xing/wysihtml5.js
+{
+    properties: {
+        examples: [
+            'http://vitalets.github.io/x-editable/demo.html'
+        ],
+        link: 'https://github.com/xing/wysihtml5',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.wysihtml5',
+                'window.wysihtml5.commands'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.wysihtml5.version'
+        ]
+    }
+}
+,
 // src/gitHub/xoxco/jQuery-Tags-Input.js
 {
     properties: {
---
+++ src/gitHub/xing/wysihtml5.js
@@ -0,0 +1,23 @@
+{
+    properties: {
+        examples: [
+            'http://vitalets.github.io/x-editable/demo.html'
+        ],
+        link: 'https://github.com/xing/wysihtml5',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.wysihtml5',
+                'window.wysihtml5.commands'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.wysihtml5.version'
+        ]
+    }
+}

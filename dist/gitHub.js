2922c9155d53a4c29f61436ba3874d18d4b4c0d6
onassar <onassar@gmail.com>
2019-01-04 01:21:33 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -4100,6 +4100,28 @@
     }
 }
 ,
+// src/gitHub/bgrins/bindWithDelay.js
+{
+    properties: {
+        examples: [
+            'https://bgrins.github.io/bindWithDelay/#example'
+        ],
+        link: 'https://github.com/bgrins/bindWithDelay',
+        platform: 'GitHub',
+        tags: [
+            'events'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.fn.bindWithDelay'
+            ]
+        ]
+    }
+}
+,
 // src/gitHub/bgrins/spectrum.js
 {
     properties: {
---
+++ src/gitHub/bgrins/bindWithDelay.js
@@ -0,0 +1,20 @@
+{
+    properties: {
+        examples: [
+            'https://bgrins.github.io/bindWithDelay/#example'
+        ],
+        link: 'https://github.com/bgrins/bindWithDelay',
+        platform: 'GitHub',
+        tags: [
+            'events'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.fn.bindWithDelay'
+            ]
+        ]
+    }
+}

368212cb0e0f9c956a7e9a1588d64413c1ef0e23
onassar <onassar@gmail.com>
2019-01-04 17:27:07 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -3500,6 +3500,35 @@
     }
 }
 ,
+// src/gitHub/asciidoc/asciidoc.js
+{
+    properties: {
+        examples: [
+            'http://asciidoc.org/'
+        ],
+        link: 'https://github.com/asciidoc/asciidoc',
+        name: 'AsciiDoc',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        shortcuts: [
+            {
+                key: 'query.meta.generator',
+                generator: 'AsciiDoc'
+            }
+        ]
+    },
+    versions: {
+        shortcuts: [
+            {
+                key: 'query.meta.generator',
+                generator: 'AsciiDoc'
+            }
+        ]
+    }
+}
+,
 // src/gitHub/asual/jquery-address.js
 {
     properties: {
---
+++ src/gitHub/asciidoc/asciidoc.js
@@ -0,0 +1,27 @@
+{
+    properties: {
+        examples: [
+            'http://asciidoc.org/'
+        ],
+        link: 'https://github.com/asciidoc/asciidoc',
+        name: 'AsciiDoc',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        shortcuts: [
+            {
+                key: 'query.meta.generator',
+                generator: 'AsciiDoc'
+            }
+        ]
+    },
+    versions: {
+        shortcuts: [
+            {
+                key: 'query.meta.generator',
+                generator: 'AsciiDoc'
+            }
+        ]
+    }
+}

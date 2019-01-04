694c358eac2489e4177dc2006cf55e89362666e4
onassar <onassar@gmail.com>
2019-01-04 01:30:14 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -15003,6 +15003,26 @@
     }
 }
 ,
+// src/gitHub/rstaib/jquery-bootgrid.js
+{
+    properties: {
+        examples: [
+            'http://www.jquery-bootgrid.com/Examples'
+        ],
+        link: 'https://github.com/rstaib/jquery-bootgrid',
+        name: 'jQuery Bootgrid Plugin',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.fn.bootgrid'
+            ]
+        ]
+    }
+}
+,
 // src/gitHub/ruby/ruby.js
 {
     properties: {
---
+++ src/gitHub/rstaib/jquery-bootgrid.js
@@ -0,0 +1,18 @@
+{
+    properties: {
+        examples: [
+            'http://www.jquery-bootgrid.com/Examples'
+        ],
+        link: 'https://github.com/rstaib/jquery-bootgrid',
+        name: 'jQuery Bootgrid Plugin',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.fn.bootgrid'
+            ]
+        ]
+    }
+}

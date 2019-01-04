744a04e0e0d7edb2888c64a11d614c95eb2e97ce
onassar <onassar@gmail.com>
2019-01-04 18:01:21 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -5356,6 +5356,35 @@
     }
 }
 ,
+// src/gitHub/concrete5/concrete5.js
+{
+    properties: {
+        examples: [
+            'https://www.concrete5.org/'
+        ],
+        link: 'https://github.com/concrete5/concrete5',
+        name: 'Concrete5',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        shortcuts: [
+            {
+                key: 'query.meta.generator',
+                generator: 'Concrete5'
+            }
+        ]
+    },
+    versions: {
+        shortcuts: [
+            {
+                key: 'query.meta.generator',
+                generator: 'Concrete5'
+            }
+        ]
+    }
+}
+,
 // src/gitHub/corbanbrook/dsp.js.js
 {
     properties: {
---
+++ src/gitHub/concrete5/concrete5.js
@@ -0,0 +1,27 @@
+{
+    properties: {
+        examples: [
+            'https://www.concrete5.org/'
+        ],
+        link: 'https://github.com/concrete5/concrete5',
+        name: 'Concrete5',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        shortcuts: [
+            {
+                key: 'query.meta.generator',
+                generator: 'Concrete5'
+            }
+        ]
+    },
+    versions: {
+        shortcuts: [
+            {
+                key: 'query.meta.generator',
+                generator: 'Concrete5'
+            }
+        ]
+    }
+}

5c3e3fdee4ffc9e9a313ef2545d2c26c6b4abf8d
onassar <onassar@gmail.com>
2019-01-04 00:39:40 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -17212,6 +17212,29 @@
     }
 }
 ,
+// src/gitHub/walmik/timer.jquery.js
+{
+    properties: {
+        examples: [
+            'https://jquerytimer.com/'
+        ],
+        link: 'https://github.com/walmik/timer.jquery',
+        name: 'jQuery Timer plugin',
+        platform: 'GitHub',
+        tags: [
+            'datetime'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.fn.timer'
+            ]
+        ]
+    }
+}
+,
 // src/gitHub/weareoutman/clockpicker.js
 {
     properties: {
---
+++ src/gitHub/walmik/timer.jquery.js
@@ -0,0 +1,21 @@
+{
+    properties: {
+        examples: [
+            'https://jquerytimer.com/'
+        ],
+        link: 'https://github.com/walmik/timer.jquery',
+        name: 'jQuery Timer plugin',
+        platform: 'GitHub',
+        tags: [
+            'datetime'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.fn.timer'
+            ]
+        ]
+    }
+}

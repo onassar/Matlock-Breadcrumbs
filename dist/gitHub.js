df49818396abc37a61e9f2ec38743dc2ecf48638
onassar <onassar@gmail.com>
2019-01-03 22:24:29 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -11292,6 +11292,26 @@
     }
 }
 ,
+// src/gitHub/markgoodyear/scrollup.js
+{
+    properties: {
+        examples: [
+            'https://markgoodyear.com/labs/scrollup/'
+        ],
+        link: 'https://github.com/markgoodyear/scrollup',
+        name: 'ScrollUp',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.fn.scrollUp'
+            ]
+        ]
+    }
+}
+,
 // src/gitHub/marklagendijk/jquery.tabbable.js
 {
     properties: {
---
+++ src/gitHub/markgoodyear/scrollup.js
@@ -0,0 +1,18 @@
+{
+    properties: {
+        examples: [
+            'https://markgoodyear.com/labs/scrollup/'
+        ],
+        link: 'https://github.com/markgoodyear/scrollup',
+        name: 'ScrollUp',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.fn.scrollUp'
+            ]
+        ]
+    }
+}

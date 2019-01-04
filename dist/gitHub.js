4da9ca1f8e8954c90e1f6ebd22ef93f0e2af1c60
onassar <onassar@gmail.com>
2019-01-04 16:46:53 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -614,6 +614,38 @@
     }
 }
 ,
+// src/gitHub/Fantomas42/django-blog-zinnia.js
+{
+    dependencies: [
+        'https://github.com/django/django'
+    ],
+    properties: {
+        examples: [
+            'https://demo.django-blog-zinnia.com/blog/'
+        ],
+        link: 'https://github.com/Fantomas42/django-blog-zinnia',
+        name: 'Django Blog Zinnia',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        shortcuts: [
+            {
+                key: 'query.meta.generator',
+                generator: 'Zinnia'
+            }
+        ]
+    },
+    versions: {
+        shortcuts: [
+            {
+                key: 'query.meta.generator',
+                generator: 'Zinnia'
+            }
+        ]
+    }
+}
+,
 // src/gitHub/FezVrasta/popper.js.js
 {
     properties: {
---
+++ src/gitHub/Fantomas42/django-blog-zinnia.js
@@ -0,0 +1,30 @@
+{
+    dependencies: [
+        'https://github.com/django/django'
+    ],
+    properties: {
+        examples: [
+            'https://demo.django-blog-zinnia.com/blog/'
+        ],
+        link: 'https://github.com/Fantomas42/django-blog-zinnia',
+        name: 'Django Blog Zinnia',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        shortcuts: [
+            {
+                key: 'query.meta.generator',
+                generator: 'Zinnia'
+            }
+        ]
+    },
+    versions: {
+        shortcuts: [
+            {
+                key: 'query.meta.generator',
+                generator: 'Zinnia'
+            }
+        ]
+    }
+}

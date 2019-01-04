94df99309e694b86f02730d99736eb91f81f80c2
onassar <onassar@gmail.com>
2019-01-04 02:24:42 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -9337,6 +9337,26 @@
     }
 }
 ,
+// src/gitHub/jakerella/jquery-mockjax.js
+{
+    properties: {
+        examples: [
+            'http://vitalets.github.io/x-editable/demo.html'
+        ],
+        link: 'https://github.com/jakerella/jquery-mockjax',
+        name: 'jQuery MockJax',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.mockjax'
+            ]
+        ]
+    }
+}
+,
 // src/gitHub/jakiestfu/Medium.js.js
 {
     properties: {

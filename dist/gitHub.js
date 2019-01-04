e885c3752b69ced9df7be8b9b2f861018ebbaab3
onassar <onassar@gmail.com>
2019-01-04 00:54:28 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -8342,6 +8342,33 @@
     }
 }
 ,
+// src/gitHub/highchartTable/jquery-highchartTable-plugin.js
+{
+    properties: {
+        examples: [
+            'http://www.guriddo.net/demo/guriddojs/loading_data/rest_million/index.html'
+        ],
+        link: 'https://github.com/tonytomov/jqGrid',
+        platform: 'GitHub',
+        tags: [
+            'tables'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQueray.fn.jqGrid'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.jQuery.jgrid.version'
+        ]
+    }
+}
+,
 // src/gitHub/highcharts/highcharts.js
 {
     properties: {
---
+++ src/gitHub/highchartTable/jquery-highchartTable-plugin.js
@@ -0,0 +1,25 @@
+{
+    properties: {
+        examples: [
+            'http://www.guriddo.net/demo/guriddojs/loading_data/rest_million/index.html'
+        ],
+        link: 'https://github.com/tonytomov/jqGrid',
+        platform: 'GitHub',
+        tags: [
+            'tables'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQueray.fn.jqGrid'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.jQuery.jgrid.version'
+        ]
+    }
+}

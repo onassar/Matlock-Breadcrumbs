49e3ae5fd1c540d73b478197891799869cc2d394
onassar <onassar@gmail.com>
2019-01-04 01:15:56 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -12529,6 +12529,30 @@
     }
 }
 ,
+// src/gitHub/mezzoblue/isotope.js
+{
+    properties: {
+        examples: [
+            'https://isotope.metafizzy.co/'
+        ],
+        link: 'https://github.com/metafizzy/isotope',
+        name: 'Isotope',
+        platform: 'GitHub',
+        tags: [
+            'filtering',
+            'layouts'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.fn.isotope'
+            ]
+        ]
+    }
+}
+,
 // src/gitHub/mholt/PapaParse.js
 {
     properties: {
---
+++ src/gitHub/mezzoblue/isotope.js
@@ -0,0 +1,22 @@
+{
+    properties: {
+        examples: [
+            'https://isotope.metafizzy.co/'
+        ],
+        link: 'https://github.com/metafizzy/isotope',
+        name: 'Isotope',
+        platform: 'GitHub',
+        tags: [
+            'filtering',
+            'layouts'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.fn.isotope'
+            ]
+        ]
+    }
+}

240d0abd6dd136a8b950294b4232beda83530372
onassar <onassar@gmail.com>
2019-01-04 23:15:14 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -5068,6 +5068,28 @@
     }
 }
 ,
+// src/gitHub/cferdinandi/tabby.js
+{
+    properties: {
+        examples: [
+            'http://cferdinandi.github.io/tabby/',
+            'https://www.smashingmagazine.com/jobs/'
+        ],
+        link: 'https://github.com/cferdinandi/tabby',
+        name: 'Tabby',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.tabby',
+                'window.tabby.toggleTab'
+            ]
+        ]
+    }
+}
+,
 // src/gitHub/chaijs/chai.js
 {
     properties: {
---
+++ src/gitHub/cferdinandi/tabby.js
@@ -0,0 +1,20 @@
+{
+    properties: {
+        examples: [
+            'http://cferdinandi.github.io/tabby/',
+            'https://www.smashingmagazine.com/jobs/'
+        ],
+        link: 'https://github.com/cferdinandi/tabby',
+        name: 'Tabby',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.tabby',
+                'window.tabby.toggleTab'
+            ]
+        ]
+    }
+}

9ea2669ecea47195d7104083ae8744392cf7daa3
onassar <onassar@gmail.com>
2019-01-05 11:32:07 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -12531,6 +12531,31 @@
     }
 }
 ,
+// src/gitHub/madrobby/prototype.js
+{
+    properties: {
+        examples: [
+            'http://pieroxy.net/blog/2012/10/22/index_of_the_media_queries_articles.html'
+        ],
+        link: 'https://github.com/prototypejs/prototype',
+        name: 'Prototype',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.Prototype'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.Prototype.Version'
+        ]
+    }
+}
+,
 // src/gitHub/madrobby/scriptaculous.js
 {
     properties: {
---
+++ src/gitHub/madrobby/prototype.js
@@ -0,0 +1,23 @@
+{
+    properties: {
+        examples: [
+            'http://pieroxy.net/blog/2012/10/22/index_of_the_media_queries_articles.html'
+        ],
+        link: 'https://github.com/prototypejs/prototype',
+        name: 'Prototype',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.Prototype'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.Prototype.Version'
+        ]
+    }
+}

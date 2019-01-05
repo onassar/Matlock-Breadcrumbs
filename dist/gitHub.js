cec600ff8667fe3bfd99f2f179c7601ea8b0d1eb
onassar <onassar@gmail.com>
2019-01-05 11:30:47 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -12531,6 +12531,28 @@
     }
 }
 ,
+// src/gitHub/madrobby/scriptaculous.js
+{
+    properties: {
+        link: 'https://github.com/madrobby/scriptaculous',
+        name: 'script.aculo.us',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.Scriptaculous'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.Scriptaculous.version'
+        ]
+    }
+}
+,
 // src/gitHub/madrobby/zepto.js
 {
     properties: {
---
+++ src/gitHub/madrobby/scriptaculous.js
@@ -0,0 +1,20 @@
+{
+    properties: {
+        link: 'https://github.com/madrobby/scriptaculous',
+        name: 'script.aculo.us',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.Scriptaculous'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.Scriptaculous.version'
+        ]
+    }
+}

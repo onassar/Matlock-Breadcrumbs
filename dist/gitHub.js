930edeaad21756b5cb6cb7d3717b359e1775bad9
onassar <onassar@gmail.com>
2019-01-07 16:08:58 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -6180,6 +6180,41 @@
     }
 }
 ,
+// src/gitHub/designmodo/Flat-UI.js
+{
+    properties: {
+        examples: [
+            'https://designmodo.github.io/Flat-UI/'
+        ],
+        link: 'https://github.com/designmodo/Flat-UI',
+        name: 'Flat UI',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        shortcuts: [
+            {
+                key: 'query.stylesheets',
+                fileroots: [
+                    'flatui',
+                    'flat-ui'
+                ]
+            }
+        ]
+    },
+    versions: {
+        shortcuts: [
+            {
+                key: 'query.stylesheets',
+                fileroots: [
+                    'flatui',
+                    'flat-ui'
+                ]
+            }
+        ]
+    }
+}
+,
 // src/gitHub/devbridge/jQuery-Autocomplete.js
 {
     properties: {
---
+++ src/gitHub/designmodo/Flat-UI.js
@@ -0,0 +1,33 @@
+{
+    properties: {
+        examples: [
+            'https://designmodo.github.io/Flat-UI/'
+        ],
+        link: 'https://github.com/designmodo/Flat-UI',
+        name: 'Flat UI',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        shortcuts: [
+            {
+                key: 'query.stylesheets',
+                fileroots: [
+                    'flatui',
+                    'flat-ui'
+                ]
+            }
+        ]
+    },
+    versions: {
+        shortcuts: [
+            {
+                key: 'query.stylesheets',
+                fileroots: [
+                    'flatui',
+                    'flat-ui'
+                ]
+            }
+        ]
+    }
+}

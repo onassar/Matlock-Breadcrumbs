7921cddb5004043668fe4061203979f26d7a6889
onassar <onassar@gmail.com>
2019-01-07 03:16:32 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -12617,7 +12617,6 @@
 ,
 // src/gitHub/madrobby/scriptaculous.js
 {
-    isolate: true,
     properties: {
         examples: [
             'https://script.aculo.us/'
--- src/gitHub/madrobby/scriptaculous.js
+++ src/gitHub/madrobby/scriptaculous.js
@@ -1,5 +1,4 @@
 {
-    isolate: true,
     properties: {
         examples: [
             'https://script.aculo.us/'

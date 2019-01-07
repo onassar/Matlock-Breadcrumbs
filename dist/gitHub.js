8fa90f0593ce63272884610bd2b137fb9e0d199c
onassar <onassar@gmail.com>
2019-01-07 16:28:48 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -15300,6 +15300,31 @@
     }
 }
 ,
+// src/gitHub/philogb/jit.js
+{
+    properties: {
+        examples: [
+            'https://philogb.github.io/jit/demos.html'
+        ],
+        link: 'https://github.com/philogb/jit',
+        name: 'JavaScript InfoVis Toolkit',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.$jit'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.$jit.version'
+        ]
+    }
+}
+,
 // src/gitHub/php/php-src.js
 {
     properties: {
---
+++ src/gitHub/philogb/jit.js
@@ -0,0 +1,23 @@
+{
+    properties: {
+        examples: [
+            'https://philogb.github.io/jit/demos.html'
+        ],
+        link: 'https://github.com/philogb/jit',
+        name: 'JavaScript InfoVis Toolkit',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.$jit'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.$jit.version'
+        ]
+    }
+}

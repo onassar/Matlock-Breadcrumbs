a7b93c7310ec8fd9d30b86c1d270e045ac12398c
onassar <onassar@gmail.com>
2019-01-07 15:59:02 -0500

    Breadcrumb update (auto)

---
+++ src/gitHub/ankane/ahoy.js
@@ -0,0 +1,31 @@
+{
+    properties: {
+        examples: [
+            'https://www.instacart.com/opensource'
+        ],
+        link: 'https://github.com/ankane/ahoy',
+        name: 'Ahoy Analytics',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        patterns: {
+            cookies: [
+                [
+                    /ahoy_track/i
+                ],
+                [
+                    /ahoy_visit/i
+                ],
+                [
+                    /ahoy_visitor/i
+                ]
+            ]
+        },
+        references: [
+            [
+                'window.ahoy'
+            ]
+        ]
+    }
+}

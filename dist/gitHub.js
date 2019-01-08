ff6a9837a7be011e9f08ba938bb49e272fc94408
onassar <onassar@gmail.com>
2019-01-08 15:16:00 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -15459,6 +15459,42 @@
     }
 }
 ,
+// src/gitHub/pimcore/pimcore.js
+{
+    dependencies: [
+        'https://github.com/php/php-src'
+    ],
+    properties: {
+        examples: [
+            'https://www.meowmix.com/'
+        ],
+        link: 'https://github.com/pimcore/pimcore',
+        platform: 'GitHub',
+        name: 'Pimcore CMS',
+        type: 'repository',
+        wikiPedia: {
+            load: true,
+            title: 'Pimcore'
+        }
+    },
+    tests: {
+        shortcuts: [
+            {
+                key: 'header.server',
+                name: 'Pimcore'
+            }
+        ]
+    },
+    versions: {
+        shortcuts: [
+            {
+                key: 'header.server',
+                name: 'Pimcore'
+            }
+        ]
+    }
+}
+,
 // src/gitHub/pixelcog/parallax.js.js
 {
     properties: {
---
+++ src/gitHub/pimcore/pimcore.js
@@ -0,0 +1,34 @@
+{
+    dependencies: [
+        'https://github.com/php/php-src'
+    ],
+    properties: {
+        examples: [
+            'https://www.meowmix.com/'
+        ],
+        link: 'https://github.com/pimcore/pimcore',
+        platform: 'GitHub',
+        name: 'Pimcore CMS',
+        type: 'repository',
+        wikiPedia: {
+            load: true,
+            title: 'Pimcore'
+        }
+    },
+    tests: {
+        shortcuts: [
+            {
+                key: 'header.server',
+                name: 'Pimcore'
+            }
+        ]
+    },
+    versions: {
+        shortcuts: [
+            {
+                key: 'header.server',
+                name: 'Pimcore'
+            }
+        ]
+    }
+}

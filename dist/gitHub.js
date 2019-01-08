c758944e1730c7f23ea5983496f105ea0b94b57b
onassar <onassar@gmail.com>
2019-01-08 16:19:42 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -13176,6 +13176,26 @@
     }
 }
 ,
+// src/gitHub/marionettejs/backbone.wreqr.js
+{
+    properties: {
+        examples: [
+            'https://www.thebay.com/'
+        ],
+        link: 'https://github.com/marionettejs/backbone.wreqr',
+        name: 'Backbone.Wreqr',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.Backbone.Wreqr'
+            ]
+        ]
+    }
+}
+,
 // src/gitHub/markedjs/marked.js
 {
     properties: {
---
+++ src/gitHub/marionettejs/backbone.wreqr.js
@@ -0,0 +1,18 @@
+{
+    properties: {
+        examples: [
+            'https://www.thebay.com/'
+        ],
+        link: 'https://github.com/marionettejs/backbone.wreqr',
+        name: 'Backbone.Wreqr',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.Backbone.Wreqr'
+            ]
+        ]
+    }
+}

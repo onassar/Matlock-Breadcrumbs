1c87d34df3af790f860f746c384d1e7bdd949f79
onassar <onassar@gmail.com>
2019-01-08 18:34:23 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -1401,6 +1401,26 @@
     }
 }
 ,
+// src/gitHub/Pikaday/Pikaday.js
+{
+    properties: {
+        examples: [
+            'https://pikaday.com/'
+        ],
+        link: 'https://github.com/Pikaday/Pikaday',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.Pikaday',
+                'window.Pikaday.prototype.clear'
+            ]
+        ]
+    }
+}
+,
 // src/gitHub/Pixabay/jQuery-autoComplete.js
 {
     properties: {
---
+++ src/gitHub/Pikaday/Pikaday.js
@@ -0,0 +1,18 @@
+{
+    properties: {
+        examples: [
+            'https://pikaday.com/'
+        ],
+        link: 'https://github.com/Pikaday/Pikaday',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.Pikaday',
+                'window.Pikaday.prototype.clear'
+            ]
+        ]
+    }
+}

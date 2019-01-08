94faed2b95ffbf61774accf555d4780669215a68
onassar <onassar@gmail.com>
2019-01-08 15:31:19 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -2095,6 +2095,31 @@
     }
 }
 ,
+// src/gitHub/Valve/fingerprintjs.js
+{
+    properties: {
+        examples: [
+            'https://support.logmeininc.com/gotomeeting/help/how-to-log-in-g2m010001'
+        ],
+        link: 'https://github.com/Valve/fingerprintjs',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.Fingerprint',
+                'window.Fingerprint.prototype.isCanvasSupported'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.Fingerprint.VERSION'
+        ]
+    }
+}
+,
 // src/gitHub/Valve/fingerprintjs2.js
 {
     properties: {
---
+++ src/gitHub/Valve/fingerprintjs.js
@@ -0,0 +1,23 @@
+{
+    properties: {
+        examples: [
+            'https://support.logmeininc.com/gotomeeting/help/how-to-log-in-g2m010001'
+        ],
+        link: 'https://github.com/Valve/fingerprintjs',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.Fingerprint',
+                'window.Fingerprint.prototype.isCanvasSupported'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.Fingerprint.VERSION'
+        ]
+    }
+}

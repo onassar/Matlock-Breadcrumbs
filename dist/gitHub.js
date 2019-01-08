b8c6841a632b5c577ad6a4707966358609e56d63
onassar <onassar@gmail.com>
2019-01-08 18:16:09 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -13130,6 +13130,38 @@
     }
 }
 ,
+// src/gitHub/marceljuenemann/angular-drag-and-drop-lists.js
+{
+    properties: {
+        examples: [
+            'https://analytics.google.com/analytics/web/#/report-home/a35893957w63834918p65499463'
+        ],
+        link: 'https://github.com/marceljuenemann/angular-drag-and-drop-lists',
+        platform: 'GitHub',
+        tags: [
+            'angular',
+            'drag-and-drop'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        shortcuts: [
+            {
+                key: 'statement.angular.module',
+                name: 'dndLists'
+            }
+        ]
+    },
+    versions: {
+        shortcuts: [
+            {
+                key: 'statement.angular.module',
+                name: 'dndLists'
+            }
+        ]
+    }
+}
+,
 // src/gitHub/marcj/css-element-queries.js
 {
     properties: {

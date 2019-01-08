e135743c85763075c6d58f88ad042b42d30179bd
onassar <onassar@gmail.com>
2019-01-08 14:43:13 -0500

    Breadcrumb update (auto)

--- src/gitHub/ampproject/amphtml.js
+++ src/gitHub/ampproject/amphtml.js
@@ -1,12 +1,17 @@
 {
     properties: {
         examples: [
+            'https://www.nbcnews.com/news/amp/ncna955931',
             'https://www.independent.co.uk/news/world/americas/us-politics/trump-james-mattis-defense-secretary-white-house-tweet-replace-retire-latest-a8693756.html'
         ],
         link: 'https://github.com/ampproject/amphtml',
         name: 'AMP HTML Source',
         platform: 'GitHub',
-        type: 'repository'
+        type: 'repository',
+        wikiPedia: {
+            load: true,
+            title: 'Accelerated_Mobile_Pages'
+        }
     },
     tests: {
         references: [

962f050e04ea8fd228a40d588895547e229d7a1a
onassar <onassar@gmail.com>
2019-01-08 18:14:43 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -10826,6 +10826,37 @@
     }
 }
 ,
+// src/gitHub/jmdobry/angular-cache.js
+{
+    properties: {
+        examples: [
+            'https://analytics.google.com/analytics/web/#/report-home/a35893957w63834918p65499463'
+        ],
+        link: 'https://github.com/jmdobry/angular-cache',
+        platform: 'GitHub',
+        tags: [
+            'angular'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        shortcuts: [
+            {
+                key: 'statement.angular.module',
+                name: 'jmdobry.angular-cache'
+            }
+        ]
+    },
+    versions: {
+        shortcuts: [
+            {
+                key: 'statement.angular.module',
+                name: 'jmdobry.angular-cache'
+            }
+        ]
+    }
+}
+,
 // src/gitHub/jmosbech/StickyTableHeaders.js
 {
     properties: {
---
+++ src/gitHub/jmdobry/angular-cache.js
@@ -0,0 +1,29 @@
+{
+    properties: {
+        examples: [
+            'https://analytics.google.com/analytics/web/#/report-home/a35893957w63834918p65499463'
+        ],
+        link: 'https://github.com/jmdobry/angular-cache',
+        platform: 'GitHub',
+        tags: [
+            'angular'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        shortcuts: [
+            {
+                key: 'statement.angular.module',
+                name: 'jmdobry.angular-cache'
+            }
+        ]
+    },
+    versions: {
+        shortcuts: [
+            {
+                key: 'statement.angular.module',
+                name: 'jmdobry.angular-cache'
+            }
+        ]
+    }
+}

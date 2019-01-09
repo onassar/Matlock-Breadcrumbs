207014a305617940fa007f5281f72575bf71e9cb
onassar <onassar@gmail.com>
2019-01-09 13:34:29 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -1989,6 +1989,43 @@
     }
 }
 ,
+// src/gitHub/TYPO3/TYPO3.CMS.js
+{
+    properties: {
+        examples: [
+            'http://www.italia.it/en/home.html'
+        ],
+        link: 'https://github.com/TYPO3/TYPO3.CMS',
+        name: 'TYPO3 CMS',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        queries: [
+            [
+                {
+                    selectors: [
+                        'link[rel="stylesheet" i][href*="typo3confg" i]',
+                        'link[rel="stylesheet" i][href*="typo3temp" i]',
+                        'img[src*="typo3confg" i]',
+                        'img[src*="typo3temp" i]'
+                    ],
+                    length: {
+                        operator: '>',
+                        value: 0
+                    }
+                }
+            ]
+        ],
+        shortcuts: [
+            {
+                key: 'query.meta.generator',
+                generator: 'TYPO3 CMS'
+            }
+        ]
+    }
+}
+,
 // src/gitHub/TalAter/SpeechKITT.js
 {
     properties: {
---
+++ src/gitHub/TYPO3/TYPO3.CMS.js
@@ -0,0 +1,35 @@
+{
+    properties: {
+        examples: [
+            'http://www.italia.it/en/home.html'
+        ],
+        link: 'https://github.com/TYPO3/TYPO3.CMS',
+        name: 'TYPO3 CMS',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        queries: [
+            [
+                {
+                    selectors: [
+                        'link[rel="stylesheet" i][href*="typo3confg" i]',
+                        'link[rel="stylesheet" i][href*="typo3temp" i]',
+                        'img[src*="typo3confg" i]',
+                        'img[src*="typo3temp" i]'
+                    ],
+                    length: {
+                        operator: '>',
+                        value: 0
+                    }
+                }
+            ]
+        ],
+        shortcuts: [
+            {
+                key: 'query.meta.generator',
+                generator: 'TYPO3 CMS'
+            }
+        ]
+    }
+}

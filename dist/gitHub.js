3b92ea766bccf5af18a25415921a5682755fc84b
onassar <onassar@gmail.com>
2019-01-09 13:20:27 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -18490,6 +18490,30 @@
     }
 }
 ,
+// src/gitHub/strues/retinajs.js
+{
+    properties: {
+        examples: [
+            'https://morphext.fyianlai.com/'
+        ],
+        link: 'https://github.com/strues/retinajs',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.Retina',
+                'window.Retina.isRetina'
+            ],
+            [
+                'window.Retina',
+                'window.Retina.isRetina'
+            ]
+        ]
+    }
+}
+,
 // src/gitHub/stutrek/scrollMonitor.js
 {
     properties: {
---
+++ src/gitHub/strues/retinajs.js
@@ -0,0 +1,22 @@
+{
+    properties: {
+        examples: [
+            'https://morphext.fyianlai.com/'
+        ],
+        link: 'https://github.com/strues/retinajs',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.Retina',
+                'window.Retina.isRetina'
+            ],
+            [
+                'window.Retina',
+                'window.Retina.isRetina'
+            ]
+        ]
+    }
+}

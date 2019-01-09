8cff908a1197b2989d9bca8c049c810ff2f0870e
onassar <onassar@gmail.com>
2019-01-08 19:27:19 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -18254,6 +18254,27 @@
     }
 }
 ,
+// src/gitHub/tildeio/rsvp.js.js
+{
+    properties: {
+        examples: [
+            'https://addyosmani.com/basket.js/'
+        ],
+        link: 'https://github.com/tildeio/rsvp.js',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.RSVP',
+                'window.RSVP.filter',
+                'window.RSVP.race'
+            ]
+        ]
+    }
+}
+,
 // src/gitHub/timdown/rangy.js
 {
     properties: {
---
+++ src/gitHub/tildeio/rsvp.js.js
@@ -0,0 +1,19 @@
+{
+    properties: {
+        examples: [
+            'https://addyosmani.com/basket.js/'
+        ],
+        link: 'https://github.com/tildeio/rsvp.js',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.RSVP',
+                'window.RSVP.filter',
+                'window.RSVP.race'
+            ]
+        ]
+    }
+}

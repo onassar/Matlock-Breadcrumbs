68b8d89bf15109894433648904bc716c50ea8969
onassar <onassar@gmail.com>
2019-01-09 01:00:24 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -6405,6 +6405,37 @@
     }
 }
 ,
+// src/gitHub/dbtek/angular-aside.js
+{
+    properties: {
+        examples: [
+            'https://oscar.go.com/'
+        ],
+        link: 'https://github.com/dbtek/angular-aside',
+        platform: 'GitHub',
+        tags: [
+            'angular'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        shortcuts: [
+            {
+                key: 'statement.angular.module',
+                name: 'ngAside'
+            }
+        ]
+    },
+    versions: {
+        shortcuts: [
+            {
+                key: 'statement.angular.module',
+                name: 'ngAside'
+            }
+        ]
+    }
+}
+,
 // src/gitHub/dbushell/Nestable.js
 {
     properties: {
---
+++ src/gitHub/dbtek/angular-aside.js
@@ -0,0 +1,29 @@
+{
+    properties: {
+        examples: [
+            'https://oscar.go.com/'
+        ],
+        link: 'https://github.com/dbtek/angular-aside',
+        platform: 'GitHub',
+        tags: [
+            'angular'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        shortcuts: [
+            {
+                key: 'statement.angular.module',
+                name: 'ngAside'
+            }
+        ]
+    },
+    versions: {
+        shortcuts: [
+            {
+                key: 'statement.angular.module',
+                name: 'ngAside'
+            }
+        ]
+    }
+}

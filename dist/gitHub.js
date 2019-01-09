1cd100a27471c3cce0b043ac28520e926cd03198
onassar <onassar@gmail.com>
2019-01-09 05:16:14 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -20666,6 +20666,34 @@
     }
 }
 ,
+// src/gitHub/xdamman/selection-sharer.js
+{
+    properties: {
+        examples: [
+            'https://www.thebeaverton.com/'
+        ],
+        link: 'https://github.com/xdamman/selection-sharer',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.SelectionSharer'
+            ],
+            [
+                'window.jQuery.fn.SelectionSharer'
+            ],
+            [
+                'window.thbSelectionSharer'
+            ],
+            [
+                'window.jQuery.fn.thbSelectionSharer'
+            ]
+        ]
+    }
+}
+,
 // src/gitHub/xdan/jodit.js
 {
     properties: {
---
+++ src/gitHub/xdamman/selection-sharer.js
@@ -0,0 +1,26 @@
+{
+    properties: {
+        examples: [
+            'https://www.thebeaverton.com/'
+        ],
+        link: 'https://github.com/xdamman/selection-sharer',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.SelectionSharer'
+            ],
+            [
+                'window.jQuery.fn.SelectionSharer'
+            ],
+            [
+                'window.thbSelectionSharer'
+            ],
+            [
+                'window.jQuery.fn.thbSelectionSharer'
+            ]
+        ]
+    }
+}

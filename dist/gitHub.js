93d5f60cf0bcac7507084288a15bafa10588e4e1
onassar <onassar@gmail.com>
2019-01-08 19:54:31 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -20027,6 +20027,34 @@
     }
 }
 ,
+// src/gitHub/yui/yuidoc.js
+{
+    properties: {
+        examples: [
+            'https://yuilibrary.com/'
+        ],
+        link: 'https://github.com/yui/yuidoc',
+        name: 'YUIDoc',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        queries: [
+            [
+                {
+                    selectors: [
+                        'body.yui3-skin-sam'
+                    ],
+                    length: {
+                        operator: '>',
+                        value: 0
+                    }
+                }
+            ]
+        ]
+    }
+}
+,
 // src/gitHub/zachleat/BixText.js
 {
     properties: {

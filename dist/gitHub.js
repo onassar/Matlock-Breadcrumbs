6ce5e50110b29571a1d5cc30943419216488486b
onassar <onassar@gmail.com>
2019-01-09 05:09:52 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -17956,6 +17956,26 @@
     }
 }
 ,
+// src/gitHub/smurfy/cloud-zoom.js
+{
+    properties: {
+        examples: [
+            'https://twentyonetoys.ca/'
+        ],
+        link: 'https://github.com/smurfy/cloud-zoom',
+        name: 'CloudZoom',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.fn.CloudZoom'
+            ]
+        ]
+    }
+}
+,
 // src/gitHub/snapappointments/bootstrap-select.js
 {
     properties: {
---
+++ src/gitHub/smurfy/cloud-zoom.js
@@ -0,0 +1,18 @@
+{
+    properties: {
+        examples: [
+            'https://twentyonetoys.ca/'
+        ],
+        link: 'https://github.com/smurfy/cloud-zoom',
+        name: 'CloudZoom',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.fn.CloudZoom'
+            ]
+        ]
+    }
+}

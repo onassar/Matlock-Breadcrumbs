cda50a16d205a90722783af289bda2fa836767f6
onassar <onassar@gmail.com>
2019-01-09 02:01:02 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -8342,6 +8342,37 @@
     }
 }
 ,
+// src/gitHub/firebase/angularfire.js
+{
+    properties: {
+        examples: [
+            'https://angularjs.org/'
+        ],
+        link: 'https://github.com/firebase/angularfire',
+        platform: 'GitHub',
+        tags: [
+            'angular'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        shortcuts: [
+            {
+                key: 'statement.angular.module',
+                name: 'firebase'
+            }
+        ]
+    },
+    versions: {
+        shortcuts: [
+            {
+                key: 'statement.angular.module',
+                name: 'firebase'
+            }
+        ]
+    }
+}
+,
 // src/gitHub/flatlogic/bootstrap-tabcollapse.js
 {
     properties: {
---
+++ src/gitHub/firebase/angularfire.js
@@ -0,0 +1,29 @@
+{
+    properties: {
+        examples: [
+            'https://angularjs.org/'
+        ],
+        link: 'https://github.com/firebase/angularfire',
+        platform: 'GitHub',
+        tags: [
+            'angular'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        shortcuts: [
+            {
+                key: 'statement.angular.module',
+                name: 'firebase'
+            }
+        ]
+    },
+    versions: {
+        shortcuts: [
+            {
+                key: 'statement.angular.module',
+                name: 'firebase'
+            }
+        ]
+    }
+}

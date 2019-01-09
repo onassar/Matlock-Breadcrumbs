f26e5f03241f01a2766074e0bf53655b150e811e
onassar <onassar@gmail.com>
2019-01-09 01:13:20 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -9708,6 +9708,31 @@
     }
 }
 ,
+// src/gitHub/hay/stapes.js
+{
+    properties: {
+        examples: [
+            'http://hay.github.io/stapes/'
+        ],
+        link: 'https://github.com/hay/stapes',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.Stapes',
+                'window.Stapes.mixinEvents'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.Stapes.version'
+        ]
+    }
+}
+,
 // src/gitHub/headjs/headjs.js
 {
     properties: {
---
+++ src/gitHub/hay/stapes.js
@@ -0,0 +1,23 @@
+{
+    properties: {
+        examples: [
+            'http://hay.github.io/stapes/'
+        ],
+        link: 'https://github.com/hay/stapes',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.Stapes',
+                'window.Stapes.mixinEvents'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.Stapes.version'
+        ]
+    }
+}

c3c1e0047bbc911db96c6e0107a93d95d087b185
onassar <onassar@gmail.com>
2019-01-08 23:42:37 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -7977,6 +7977,26 @@
     }
 }
 ,
+// src/gitHub/fgnass/spin.js.js
+{
+    properties: {
+        examples: [
+            'https://siftery.com/'
+        ],
+        link: 'https://github.com/fgnass/spin.js',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.Spinner',
+                'window.Spinner.prototype.start'
+            ]
+        ]
+    }
+}
+,
 // src/gitHub/fians/Waves.js
 {
     properties: {
---
+++ src/gitHub/fgnass/spin.js.js
@@ -0,0 +1,18 @@
+{
+    properties: {
+        examples: [
+            'https://siftery.com/'
+        ],
+        link: 'https://github.com/fgnass/spin.js',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.Spinner',
+                'window.Spinner.prototype.start'
+            ]
+        ]
+    }
+}

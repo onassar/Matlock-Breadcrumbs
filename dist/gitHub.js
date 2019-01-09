26f6bc3125158dd55d9a2a02833acbc4b7476ee8
onassar <onassar@gmail.com>
2019-01-09 00:14:56 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -1225,6 +1225,27 @@
     }
 }
 ,
+// src/gitHub/MohammadYounes/jquery-scrollLock.js
+{
+    properties: {
+        examples: [
+            'https://embedjs.readme.io/docs'
+        ],
+        link: 'https://github.com/MohammadYounes/jquery-scrollLock',
+        name: 'jQuery ScrollLock',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.fn.scrollLock',
+                'window.jQuery.fn.scrollLock.defaults'
+            ]
+        ]
+    }
+}
+,
 // src/gitHub/Mottie/tablesorter.js
 {
     properties: {
---
+++ src/gitHub/MohammadYounes/jquery-scrollLock.js
@@ -0,0 +1,19 @@
+{
+    properties: {
+        examples: [
+            'https://embedjs.readme.io/docs'
+        ],
+        link: 'https://github.com/MohammadYounes/jquery-scrollLock',
+        name: 'jQuery ScrollLock',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.fn.scrollLock',
+                'window.jQuery.fn.scrollLock.defaults'
+            ]
+        ]
+    }
+}

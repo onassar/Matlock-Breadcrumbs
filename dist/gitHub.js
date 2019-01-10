313afdd1c90d57cb8e9b839c09ae086fcf74f6d6
onassar <onassar@gmail.com>
2019-01-10 01:29:10 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -12665,6 +12665,33 @@
     }
 }
 ,
+// src/gitHub/karacas/imgLiquid.js
+{
+    properties: {
+        examples: [
+            'http://www.lse.ac.uk/iga/initiatives/global-migration'
+        ],
+        link: 'https://github.com/karacas/imgLiquid',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.imgLiquid'
+            ],
+            [
+                'window.jQuery.fn.imgLiquid'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.imgLiquid.VER'
+        ]
+    }
+}
+,
 // src/gitHub/karpathy/convnetjs.js
 {
     properties: {
---
+++ src/gitHub/karacas/imgLiquid.js
@@ -0,0 +1,25 @@
+{
+    properties: {
+        examples: [
+            'http://www.lse.ac.uk/iga/initiatives/global-migration'
+        ],
+        link: 'https://github.com/karacas/imgLiquid',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.imgLiquid'
+            ],
+            [
+                'window.jQuery.fn.imgLiquid'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.imgLiquid.VER'
+        ]
+    }
+}

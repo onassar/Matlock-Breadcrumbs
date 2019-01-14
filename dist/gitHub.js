09bcf05f83605ab482dff4e5cac6b6379808c025
onassar <onassar@gmail.com>
2019-01-14 00:34:23 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -3381,20 +3381,22 @@
 // src/gitHub/alizahid/slinky.js
 {
     properties: {
-        link: 'https://github.com/alizahid/slinky',
-        platform: 'GitHub',
         examples: [
             'https://alizahid.github.io/slinky/'
         ],
+        link: 'https://github.com/alizahid/slinky',
+        platform: 'GitHub',
         type: 'repository'
     },
     tests: {
+        references: [
+            [
+                'window.jQuery.fn.slinky'
+            ]
+        ],
         statements: [
             [
                 'new window.Slinky().jump'
-            ],
-            [
-                'window.jQuery.fn.slinky'
             ]
         ]
     }
--- src/gitHub/alizahid/slinky.js
+++ src/gitHub/alizahid/slinky.js
@@ -1,19 +1,21 @@
 {
     properties: {
-        link: 'https://github.com/alizahid/slinky',
-        platform: 'GitHub',
         examples: [
             'https://alizahid.github.io/slinky/'
         ],
+        link: 'https://github.com/alizahid/slinky',
+        platform: 'GitHub',
         type: 'repository'
     },
     tests: {
+        references: [
+            [
+                'window.jQuery.fn.slinky'
+            ]
+        ],
         statements: [
             [
                 'new window.Slinky().jump'
-            ],
-            [
-                'window.jQuery.fn.slinky'
             ]
         ]
     }

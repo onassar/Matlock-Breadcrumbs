13bf5672ab5c4e4dc10de8bff621f9ab59efb8c1
onassar <onassar@gmail.com>
2019-01-13 23:45:29 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -16941,6 +16941,31 @@
     }
 }
 ,
+// src/gitHub/plaid/link.js
+{
+    properties: {
+        examples: [
+            'https://my.wealthsimple.com/app/withdrawals'
+        ],
+        link: 'https://github.com/plaid/link',
+        name: 'Plaid',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.Plaid'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.Plaid.version'
+        ]
+    }
+}
+,
 // src/gitHub/pmsi-alignalytics/dimple.js
 {
     properties: {
---
+++ src/gitHub/plaid/link.js
@@ -0,0 +1,23 @@
+{
+    properties: {
+        examples: [
+            'https://my.wealthsimple.com/app/withdrawals'
+        ],
+        link: 'https://github.com/plaid/link',
+        name: 'Plaid',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.Plaid'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.Plaid.version'
+        ]
+    }
+}

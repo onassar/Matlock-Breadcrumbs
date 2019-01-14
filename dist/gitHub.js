ec4dee714d5e9ed21684abf7456b63509d64ccd0
onassar <onassar@gmail.com>
2019-01-13 23:51:16 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -3705,6 +3705,38 @@
     }
 }
 ,
+// src/gitHub/angular-ui/ui-validate.js
+{
+    properties: {
+        examples: [
+            'https://my.wealthsimple.com/app/withdrawals'
+        ],
+        link: 'https://github.com/angular-ui/ui-validate',
+        name: 'AngularJS ui-validate',
+        platform: 'GitHub',
+        tags: [
+            'angular'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        shortcuts: [
+            {
+                key: 'statement.angular.module',
+                name: 'ui.validate'
+            }
+        ]
+    },
+    versions: {
+        shortcuts: [
+            {
+                key: 'statement.angular.module',
+                name: 'ui.validate'
+            }
+        ]
+    }
+}
+,
 // src/gitHub/angular/angular.js
 {
     properties: {
---
+++ src/gitHub/angular-ui/ui-validate.js
@@ -0,0 +1,30 @@
+{
+    properties: {
+        examples: [
+            'https://my.wealthsimple.com/app/withdrawals'
+        ],
+        link: 'https://github.com/angular-ui/ui-validate',
+        name: 'AngularJS ui-validate',
+        platform: 'GitHub',
+        tags: [
+            'angular'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        shortcuts: [
+            {
+                key: 'statement.angular.module',
+                name: 'ui.validate'
+            }
+        ]
+    },
+    versions: {
+        shortcuts: [
+            {
+                key: 'statement.angular.module',
+                name: 'ui.validate'
+            }
+        ]
+    }
+}

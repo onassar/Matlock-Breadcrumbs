a230ade1f47b14ff20b521383a2d3b8b0acc5057
onassar <onassar@gmail.com>
2019-01-15 16:10:22 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -12004,6 +12004,27 @@
     }
 }
 ,
+// src/gitHub/jonathantneal/svg4everybody.js
+{
+    properties: {
+        alternatives: [
+            'https://jonathantneal.github.io/svg4everybody/',
+            'https://www.prospects.ac.uk/universities/soas-university-of-london-3869/department-of-development-studies-10080/courses/violence-conflict-and-development-palestine-pathway-116816'
+        ],
+        link: 'https://github.com/jonathantneal/svg4everybody',
+        name: 'SVG for Everybody',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.svg4everybody'
+            ]
+        ]
+    }
+}
+,
 // src/gitHub/jondavidjohn/payform.js
 {
     properties: {

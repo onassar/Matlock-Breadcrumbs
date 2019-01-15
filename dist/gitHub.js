e8f798364b7dce3c7c16b152d820a4231b155057
onassar <onassar@gmail.com>
2019-01-15 15:34:15 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -5668,6 +5668,27 @@
     }
 }
 ,
+// src/gitHub/cbeyls/slimbox.js
+{
+    properties: {
+        examples: [
+            'http://www.digitalia.be/software/slimbox2/',
+            'https://www.soas.ac.uk/courseunits/15PDSC006.html'
+        ],
+        link: 'https://github.com/cbeyls/slimbox',
+        name: 'jQuery Slimbox2',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.fn.slimbox'
+            ]
+        ]
+    }
+}
+,
 // src/gitHub/cbopp-art/lightcase.js
 {
     properties: {
---
+++ src/gitHub/cbeyls/slimbox.js
@@ -0,0 +1,19 @@
+{
+    properties: {
+        examples: [
+            'http://www.digitalia.be/software/slimbox2/',
+            'https://www.soas.ac.uk/courseunits/15PDSC006.html'
+        ],
+        link: 'https://github.com/cbeyls/slimbox',
+        name: 'jQuery Slimbox2',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.fn.slimbox'
+            ]
+        ]
+    }
+}

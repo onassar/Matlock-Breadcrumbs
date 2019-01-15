0e5e906eff1edef08ecb99d2a76f8165817bf591
onassar <onassar@gmail.com>
2019-01-15 12:31:02 -0500

    Breadcrumb update (auto)

---
+++ src/gitHub/dubrox/Multiple-Dates-Picker-for-jQuery-UI.js
@@ -0,0 +1,24 @@
+{
+    properties: {
+        examples: [
+            'https://dubrox.github.io/Multiple-Dates-Picker-for-jQuery-UI/',
+            'https://www.rover.com/ca/'
+        ],
+        link: 'https://github.com/dubrox/Multiple-Dates-Picker-for-jQuery-UI',
+        name: 'jQuery UI MultiDatesPicker',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.multiDatesPicker'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.jQuery.multiDatesPicker.version'
+        ]
+    }
+}

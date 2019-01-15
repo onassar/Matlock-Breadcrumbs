a791f3a7172d7bce11550ba30ff6d6d4f3652817
onassar <onassar@gmail.com>
2019-01-15 16:14:44 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -740,6 +740,35 @@
     }
 }
 ,
+// src/gitHub/Financial-Times/polyfill-service.js
+{
+    properties: {
+        examples: [
+            ''
+        ],
+        link: 'https://github.com/Financial-Times/polyfill-service',
+        name: 'Polyfill Service (by Financial Times)'
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        queries: [
+            [
+                {
+                    selectors: [
+                        'script[src*="polyfill.io/v2/polyfill.js" i]',
+                        'script[src*="polyfill.io/v3/polyfill.js" i]'
+                    ],
+                    length: {
+                        operator: '>',
+                        value: 0
+                    }
+                }
+            ]
+        ]
+    }
+}
+,
 // src/gitHub/FortAwesome/Font-Awesome.js
 {
     properties: {
---
+++ src/gitHub/Financial-Times/polyfill-service.js
@@ -0,0 +1,27 @@
+{
+    properties: {
+        examples: [
+            ''
+        ],
+        link: 'https://github.com/Financial-Times/polyfill-service',
+        name: 'Polyfill Service (by Financial Times)'
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        queries: [
+            [
+                {
+                    selectors: [
+                        'script[src*="polyfill.io/v2/polyfill.js" i]',
+                        'script[src*="polyfill.io/v3/polyfill.js" i]'
+                    ],
+                    length: {
+                        operator: '>',
+                        value: 0
+                    }
+                }
+            ]
+        ]
+    }
+}

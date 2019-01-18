6f6378d23372d8503c7173dffbba6fdb8d39fc82
onassar <onassar@gmail.com>
2019-01-18 18:04:21 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -4565,6 +4565,29 @@
     }
 }
 ,
+// src/gitHub/backbone-paginator/backbone.paginator.js
+{
+    properties: {
+        examples: [
+            'http://backbone-paginator.github.io/backbone.paginator/examples/client-mode.html'
+        ],
+        link: 'https://github.com/backbone-paginator/backbone.paginator',
+        name: 'backbone.paginator',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.Backbone.Paginator'
+            ],
+            [
+                'window.Backbone.PageableCollection'
+            ]
+        ]
+    }
+}
+,
 // src/gitHub/backdrop/backdrop.js
 {
     properties: {
---
+++ src/gitHub/backbone-paginator/backbone.paginator.js
@@ -0,0 +1,21 @@
+{
+    properties: {
+        examples: [
+            'http://backbone-paginator.github.io/backbone.paginator/examples/client-mode.html'
+        ],
+        link: 'https://github.com/backbone-paginator/backbone.paginator',
+        name: 'backbone.paginator',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.Backbone.Paginator'
+            ],
+            [
+                'window.Backbone.PageableCollection'
+            ]
+        ]
+    }
+}

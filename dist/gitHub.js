a5f2917f37555500f8e5251e681af017151b7eaa
onassar <onassar@gmail.com>
2019-01-20 02:37:21 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -17532,6 +17532,35 @@
     }
 }
 ,
+// src/gitHub/plone/Plone.js
+{
+    properties: {
+        examples: [
+            'https://www.imi-n.org/publications/money-matters-the-role-of-funding-in-migration-governance'
+        ],
+        link: 'https://github.com/plone/Plone',
+        name: 'Plone CMS',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        shortcuts: [
+            {
+                key: 'query.meta.generator',
+                generator: 'Plone'
+            }
+        ]
+    },
+    versions: {
+        shortcuts: [
+            {
+                key: 'query.meta.generator',
+                generator: 'Plone'
+            }
+        ]
+    }
+}
+,
 // src/gitHub/pmsi-alignalytics/dimple.js
 {
     properties: {
---
+++ src/gitHub/plone/Plone.js
@@ -0,0 +1,27 @@
+{
+    properties: {
+        examples: [
+            'https://www.imi-n.org/publications/money-matters-the-role-of-funding-in-migration-governance'
+        ],
+        link: 'https://github.com/plone/Plone',
+        name: 'Plone CMS',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        shortcuts: [
+            {
+                key: 'query.meta.generator',
+                generator: 'Plone'
+            }
+        ]
+    },
+    versions: {
+        shortcuts: [
+            {
+                key: 'query.meta.generator',
+                generator: 'Plone'
+            }
+        ]
+    }
+}

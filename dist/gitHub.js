b87d3e6507d29cddfa2c5edfba144a1d109da82f
onassar <onassar@gmail.com>
2019-01-20 02:42:59 -0500

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -17760,6 +17760,41 @@
     }
 }
 ,
+// src/gitHub/python/cpython.js
+{
+    properties: {
+        donate: {
+            link: 'https://www.python.org/psf/donations/'
+        },
+        examples: [
+            'https://www.imi-n.org/publications/money-matters-the-role-of-funding-in-migration-governance'
+        ],
+        link: 'https://github.com/python/cpython',
+        platform: 'GitHub',
+        type: 'repository',
+        wikiPedia: {
+            load: true,
+            title: 'Python_(programming_language)'
+        }
+    },
+    tests: {
+        shortcuts: [
+            {
+                key: 'header.server',
+                name: 'python'
+            }
+        ]
+    },
+    versions: {
+        shortcuts: [
+            {
+                key: 'header.server',
+                name: 'python'
+            }
+        ]
+    }
+}
+,
 // src/gitHub/qTip2/qTip2.js
 {
     properties: {
---
+++ src/gitHub/python/cpython.js
@@ -0,0 +1,33 @@
+{
+    properties: {
+        donate: {
+            link: 'https://www.python.org/psf/donations/'
+        },
+        examples: [
+            'https://www.imi-n.org/publications/money-matters-the-role-of-funding-in-migration-governance'
+        ],
+        link: 'https://github.com/python/cpython',
+        platform: 'GitHub',
+        type: 'repository',
+        wikiPedia: {
+            load: true,
+            title: 'Python_(programming_language)'
+        }
+    },
+    tests: {
+        shortcuts: [
+            {
+                key: 'header.server',
+                name: 'python'
+            }
+        ]
+    },
+    versions: {
+        shortcuts: [
+            {
+                key: 'header.server',
+                name: 'python'
+            }
+        ]
+    }
+}

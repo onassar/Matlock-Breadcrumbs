086048e30fa8c098531bf952d04728d548e3f5a0
onassar <onassar@gmail.com>
2019-01-24 17:36:50 +0000

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -11801,6 +11801,27 @@
     }
 }
 ,
+// src/gitHub/jasonmayes/Twitter-Post-Fetcher.js
+{
+    properties: {
+        examples: [
+            'https://www.grantedclothing.com/collections/cbc?ls=en'
+        ],
+        link: 'https://github.com/jasonmayes/Twitter-Post-Fetcher',
+        name: 'Twitter-Post-Fetcher',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.twitterFetcher',
+                'window.twitterFetcher.fetch'
+            ]
+        ]
+    }
+}
+,
 // src/gitHub/javve/list.js.js
 {
     properties: {
---
+++ src/gitHub/jasonmayes/Twitter-Post-Fetcher.js
@@ -0,0 +1,19 @@
+{
+    properties: {
+        examples: [
+            'https://www.grantedclothing.com/collections/cbc?ls=en'
+        ],
+        link: 'https://github.com/jasonmayes/Twitter-Post-Fetcher',
+        name: 'Twitter-Post-Fetcher',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.twitterFetcher',
+                'window.twitterFetcher.fetch'
+            ]
+        ]
+    }
+}

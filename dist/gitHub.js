8dbf5f2d82eb420dca6f33ec712b4d38caf6034e
onassar <onassar@gmail.com>
2019-01-27 23:53:21 +0000

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -11928,6 +11928,29 @@
     }
 }
 ,
+// src/gitHub/jcubic/jquery.terminal.js
+{
+    properties: {
+        examples: [
+            'https://terminal.jcubic.pl/',
+            'http://codebyte.re/'
+        ],
+        link: 'https://github.com/jcubic/jquery.terminal',
+        name: 'jQuery Terminal Emulator',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.terminal',
+                'window.jQuery.terminal.color_names',
+                'window.jQuery.terminal.escape_brackets'
+            ]
+        ]
+    }
+}
+,
 // src/gitHub/jedfoster/Readmore.js.js
 {
     properties: {
---
+++ src/gitHub/jcubic/jquery.terminal.js
@@ -0,0 +1,21 @@
+{
+    properties: {
+        examples: [
+            'https://terminal.jcubic.pl/',
+            'http://codebyte.re/'
+        ],
+        link: 'https://github.com/jcubic/jquery.terminal',
+        name: 'jQuery Terminal Emulator',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.terminal',
+                'window.jQuery.terminal.color_names',
+                'window.jQuery.terminal.escape_brackets'
+            ]
+        ]
+    }
+}

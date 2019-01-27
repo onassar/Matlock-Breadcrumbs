fbb81aa8178c6a99033a3767362c15f76b35aefc
onassar <onassar@gmail.com>
2019-01-27 23:56:41 +0000

    Breadcrumb update (auto)

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -12526,6 +12526,33 @@
     }
 }
 ,
+// src/gitHub/josecebe/twbs-pagination.js
+{
+    properties: {
+        examples: [
+            'http://josecebe.github.io/twbs-pagination/',
+            'https://terminal.jcubic.pl/'
+        ],
+        link: 'https://github.com/josecebe/twbs-pagination',
+        name: 'jQuery Pagination plugin',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.fn.twbsPagination',
+                'window.jQuery.fn.twbsPagination.defaults'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.jQuery.fn.twbsPagination.version'
+        ]
+    }
+}
+,
 // src/gitHub/jpillora/notifyjs.js
 {
     properties: {
---
+++ src/gitHub/josecebe/twbs-pagination.js
@@ -0,0 +1,25 @@
+{
+    properties: {
+        examples: [
+            'http://josecebe.github.io/twbs-pagination/',
+            'https://terminal.jcubic.pl/'
+        ],
+        link: 'https://github.com/josecebe/twbs-pagination',
+        name: 'jQuery Pagination plugin',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.jQuery.fn.twbsPagination',
+                'window.jQuery.fn.twbsPagination.defaults'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.jQuery.fn.twbsPagination.version'
+        ]
+    }
+}

318772f85d96e9672d79ac063592042346e352bd
onassar <onassar@gmail.com>
2019-02-11 18:14:22 +0000

    Wikipedia article is gone

--- src/gitHub/aFarkas/html5shiv.js
+++ src/gitHub/aFarkas/html5shiv.js
@@ -8,11 +8,7 @@
             'https://i.imgur.com/VaUooX0.png'
         ],
         platform: 'GitHub',
-        type: 'repository',
-        wikiPedia: {
-            load: true,
-            title: 'HTML5_Shiv'
-        }
+        type: 'repository'
     },
     tests: {
         patterns: {

6458f2563b3da0c95825956311ba2478247520d2
onassar <onassar@gmail.com>
2019-07-28 01:18:14 +0200

    New lib

---
+++ src/gitHub/apollographql/apollo-client.js
@@ -0,0 +1,22 @@
+{
+    properties: {
+        examples: [
+            'https://qz.com/455109/entrepreneurs-dont-have-a-special-gene-for-risk-they-come-from-families-with-money/'
+        ],
+        link: 'https://github.com/apollographql/apollo-client',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.__APOLLO_CLIENT__'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.__APOLLO_CLIENT__.version'
+        ]
+    }
+}

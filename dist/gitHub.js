5021d767d8c94d2a736819a331a049a04d5e7430
onassar <onassar@gmail.com>
2019-07-28 01:18:37 +0200

    new apollo lib

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -4155,6 +4155,30 @@
     }
 }
 ,
+// src/gitHub/apollographql/apollo-client.js
+{
+    properties: {
+        examples: [
+            'https://qz.com/455109/entrepreneurs-dont-have-a-special-gene-for-risk-they-come-from-families-with-money/'
+        ],
+        link: 'https://github.com/apollographql/apollo-client',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.__APOLLO_CLIENT__'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.__APOLLO_CLIENT__.version'
+        ]
+    }
+}
+,
 // src/gitHub/appleple/SmartPhoto.js
 {
     properties: {

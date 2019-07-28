648156b42262f10db41604f26f90fa1bb5474b01
onassar <onassar@gmail.com>
2019-07-28 14:20:25 +0200

    New one

---
+++ src/gitHub/willmcpo/body-scroll-lock.js
@@ -0,0 +1,19 @@
+{
+    properties: {
+        examples: [
+            'https://www.airoav.com/'
+        ],
+        link: 'https://github.com/willmcpo/body-scroll-lock',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.bodyScrollLock',
+                'window.bodyScrollLock.disableBodyScroll',
+                'window.bodyScrollLock.enableBodyScroll'
+            ]
+        ]
+    }
+}

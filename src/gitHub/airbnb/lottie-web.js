1bb4a1ebc65ed11ba8937afafe2db591ece069d2
onassar <onassar@gmail.com>
2019-07-28 14:11:13 +0200

    new libs

---
+++ src/gitHub/airbnb/lottie-web.js
@@ -0,0 +1,23 @@
+{
+    properties: {
+        link: 'https://github.com/airbnb/lottie-web',
+        platform: 'GitHub',
+        examples: [
+            'https://www.airoav.com/'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.bodymovin',
+                'window.bodymovin.pause'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.bodymovin.version'
+        ]
+    }
+}
---
+++ src/gitHub/docsifyjs/docsify.js
@@ -0,0 +1,23 @@
+{
+    properties: {
+        link: 'https://github.com/docsifyjs/docsify',
+        platform: 'GitHub',
+        examples: [
+            'http://airbnb.io/lottie/'
+        ],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.Docsify',
+                'window.Docsify.util'
+            ]
+        ]
+    },
+    versions: {
+        references: [
+            'window.Docsify.version'
+        ]
+    }
+}

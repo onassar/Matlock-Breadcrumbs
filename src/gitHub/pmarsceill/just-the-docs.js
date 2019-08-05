f79c708d76de6ebcca551056082bb5954fdb4c28
onassar <onassar@gmail.com>
2019-08-05 13:58:40 +0300

    new one

---
+++ src/gitHub/pmarsceill/just-the-docs.js
@@ -0,0 +1,27 @@
+{
+    properties: {
+        examples: [
+            'https://endoflife.date/php'
+        ],
+        link: 'https://github.com/pmarsceill/just-the-docs',
+        platform: 'GitHub',
+        tags: [],
+        type: 'repository'
+    },
+    tests: {
+        shortcuts: [
+            {
+                key: 'query.script',
+                fileroots: [
+                    'just-the-docs'
+                ]
+            },
+            {
+                key: 'query.stylesheets',
+                fileroots: [
+                    'just-the-docs'
+                ]
+            }
+        ]
+    }
+}

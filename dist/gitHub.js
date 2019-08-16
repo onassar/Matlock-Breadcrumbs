7a0322744c799a1f06f25f414d9944b54c455614
onassar <onassar@gmail.com>
2019-08-16 11:08:50 -0400

    new lib

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -9397,6 +9397,28 @@
     }
 }
 ,
+// src/gitHub/ganlanyuan/tiny-slider.js
+{
+    properties: {
+        examples: [
+            'http://ganlanyuan.github.io/tiny-slider/demo/'
+        ],
+        link: 'https://github.com/ganlanyuan/tiny-slider',
+        name: 'tiny-slider',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.tns',
+                'window.tns.prototype',
+                'window.tns.prototype.constructor'
+            ]
+        ]
+    }
+}
+,
 // src/gitHub/gatsbyjs/gatsby.js
 {
     dependencies: [
---
+++ src/gitHub/ganlanyuan/tiny-slider.js
@@ -0,0 +1,20 @@
+{
+    properties: {
+        examples: [
+            'http://ganlanyuan.github.io/tiny-slider/demo/'
+        ],
+        link: 'https://github.com/ganlanyuan/tiny-slider',
+        name: 'tiny-slider',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.tns',
+                'window.tns.prototype',
+                'window.tns.prototype.constructor'
+            ]
+        ]
+    }
+}

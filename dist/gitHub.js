a5e9eea2d0121cd0c7bc94e6b019972d71d018fb
onassar <onassar@gmail.com>
2019-08-23 23:04:56 -0400

    new lib

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -9996,6 +9996,26 @@
     }
 }
 ,
+// src/gitHub/googlemaps/js-marker-clusterer.js
+{
+    properties: {
+        examples: [
+            'https://central.wordcamp.org'
+        ],
+        link: 'https://github.com/googlemaps/js-marker-clusterer',
+        name: 'Marker Clusterer',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.MarkerClusterer'
+            ]
+        ]
+    }
+}
+,
 // src/gitHub/gopalraju/gridtab.js
 {
     properties: {
---
+++ src/gitHub/googlemaps/js-marker-clusterer.js
@@ -0,0 +1,18 @@
+{
+    properties: {
+        examples: [
+            'https://central.wordcamp.org'
+        ],
+        link: 'https://github.com/googlemaps/js-marker-clusterer',
+        name: 'Marker Clusterer',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.MarkerClusterer'
+            ]
+        ]
+    }
+}

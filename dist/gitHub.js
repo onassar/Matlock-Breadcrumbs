25c9b4eba3ebe65f108b52563745762e1a0563d3
onassar <onassar@gmail.com>
2019-08-23 23:39:18 -0400

    Dist

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -11377,7 +11377,20 @@
     versions: {
         statements: [
             function() {
-                var version = '123';
+                var selector = 'script[src*="//instant.page"][type="module"]',
+                    elements = document.querySelectorAll(selector);
+                elements = Array.prototype.slice.call(elements);
+                if (elements.length === 0) {
+                    return null;
+                }
+                var element = elements[0],
+                    src = element.getAttribute('src'),
+                    pattern = /instant\.page\/([0-9\.]+)/i,
+                    matches = src.match(pattern);
+                if (matches === null) {
+                    return null;
+                }
+                var version = matches[1];
                 return version;
             }
         ]

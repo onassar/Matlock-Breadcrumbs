7720075588a7e820a9ea6f61b63e3d95472d7063
onassar <onassar@gmail.com>
2019-08-25 11:22:30 -0400

    New lib

---
+++ src/gitHub/glidejs/glide.js
@@ -0,0 +1,18 @@
+{
+    properties: {
+        link: 'https://github.com/glidejs/glide',
+        name: 'Glide.js',
+        platform: 'GitHub',
+        tags: [],
+        type: 'repository'
+    },
+    tests: {
+        references: [
+            [
+                'window.Glide',
+                'window.Glide.prototype',
+                'window.Glide.prototype.constructor'
+            ]
+        ]
+    }
+}

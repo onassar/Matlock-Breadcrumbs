e15bed91ca9331327d9a20446fe6917b540d8287
onassar <onassar@gmail.com>
2019-08-27 15:49:27 -0400

    New CMS

---
+++ src/gitHub/getgrav/grav.js
@@ -0,0 +1,20 @@
+{
+    properties: {
+        examples: [
+            'https://getgrav.org',
+            'https://kevq.uk/please-add-rss-support-to-your-site'
+        ],
+        link: 'https://github.com/getgrav/grav',
+        name: 'Grav',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        shortcuts: [
+            {
+                key: 'query.meta.generator',
+                generator: 'GravCMS'
+            }
+        ]
+    }
+}

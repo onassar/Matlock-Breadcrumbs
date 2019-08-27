9aaebf34da8acee3aa94ae8feb5fd667cda9b529
onassar <onassar@gmail.com>
2019-08-27 15:50:23 -0400

    New crumb

--- dist/gitHub.js
+++ dist/gitHub.js
@@ -9512,6 +9512,28 @@
     }
 }
 ,
+// src/gitHub/getgrav/grav.js
+{
+    properties: {
+        examples: [
+            'https://getgrav.org',
+            'https://kevq.uk/please-add-rss-support-to-your-site'
+        ],
+        link: 'https://github.com/getgrav/grav',
+        name: 'Grav',
+        platform: 'GitHub',
+        type: 'repository'
+    },
+    tests: {
+        shortcuts: [
+            {
+                key: 'query.meta.generator',
+                generator: 'GravCMS'
+            }
+        ]
+    }
+}
+,
 // src/gitHub/getpelican/pelican.js
 {
     properties: {
